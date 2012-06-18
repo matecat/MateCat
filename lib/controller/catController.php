ee64e651784def8fbab4fd7a02c405c5925b41d1
antonio <antonio@translated.net>
2012-06-18 19:36:13 +0200

    first release of catController

--- lib/controller/catController.php
+++ lib/controller/catController.php
@@ -1,10 +1,5 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
+include_once INIT::$MODEL_ROOT."/queries.php";
 /**
  * Description of catController
  *
@@ -13,6 +8,9 @@
 class catController extends viewcontroller {
     //put your code here
     private $id_file="";
+    private $segments_data=array();
+    
+    
     public function __construct() {
 	echo ".........\n";
         parent::__construct();
@@ -28,12 +26,29 @@ class catController extends viewcontroller {
         if (empty($this->id_file)){
             $this->postback("File not specified");
         }
-
         
+        $data=getCurrentFormalOffer($this->id_file);
+        foreach ($data as $seg){
+            $id_file=$seg['id_file'];
+            unset ($seg['id_file']);
+            if (!isset ($this->segments_data["$id_file"])){
+                $this->segments_data["$id_file"]=array();
+            }
+            $this->segments_data["$id_file"][]=$seg;                     
+        }
+//        echo "<pre>";
+//        print_r ($this->segments_data);
+//        exit;
+                      
     }
     
-    public function setTemplateVars() {
-        ;
+    public function setTemplateVars() {        
+        $this->template->segments_data=$this->segments_data;
+        
+//        echo "<pre>";
+//        print_r ($this->template);
+//        exit;
+//        ;
     }
 }
 
