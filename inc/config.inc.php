8c69fa6cf3777c5eecceaf8aa1c9975397b44aa5
antonio <antonio@translated.net>
2012-06-19 13:04:28 +0200

    new config management (static class)

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -1,36 +1,92 @@
 <?php
 
 //print_r ($_SERVER['HTTP_HOST']);
-if (!defined('CONST')) {
-    define('CONST', 1);
 
-    define('DB_DATABASE', "translated"); //database name
+class INIT {
+    private static $instance;
 
-    define('DB_DATABASE_SANDBOX', "matecat_sandbox"); //database name
+    public static $ROOT;
+    public static $DEBUG;
+    public static $DB_SERVER;
+    public static $DB_DATABASE;
+    public static $DB_USER;
+    public static $DB_PASS;
+    public static $LOG_REPOSITORY;
+    public static $TEMPLATE_ROOT;
+    public static $MODEL_ROOT;
+    public static $CONTROLLER_ROOT;
+    public static $UTILS_ROOT;
 
-    if (!defined('ROOT')) {
-       // $root = dirname(getcwd());
+    public static function obtain() {        
+        if (!self::$instance) {
+            self::$instance = new INIT();
+        }
+        return self::$instance;
+    }
+
+    private function __construct() {
         $root = getcwd();
-        define('ROOT', $root);
-        // echo ROOT; exit;
+        self::$ROOT = $root;
         switch ($_SERVER['HTTP_HOST']) {
             case ('matecat.pi'):
             case ('matecat.local'):
-                define('ROOT_URL', '/index.php');
-                //define('DB_SERVER', "192.168.1.53"); //database server
-                 define('DB_SERVER', "10.30.1.241");
-                define('DB_USER', "translated"); //database login name
-                define('DB_PASS', "azerty1"); //database login password
+                self::$DEBUG = true;
+                self::$DB_SERVER = "localhost";
+                self::$DB_DATABASE = "matecat_sandbox"; //database name
+                self::$DB_USER = "root"; //database login name
+                self::$DB_PASS = "root"; //database login password
                 break;
             case 'cattooldemo.matecat.com':
-	    case 'matecat.translated.home':
+            case 'matecat.translated.home':
             case 'matecat.com':
             case 'www.matecat.com':
-                define('DB_SERVER', "10.30.1.241"); //database server
-                define('DB_USER', "matecat"); //database login name
+                self::$DEBUG = false;
+                self::$DB_SERVER = "213.215.131.241"; //database server
+                self::$DB_DATABASE = "matecat"; //database name
+                self::$DB_USER = "translated"; //database login name
+                self::$DB_PASS = "azerty1"; //database login password
+                break;
+            default :
+                die(__FILE__ . " " . __LINE__ . " : unrecognided domain. Unable to set ROOT");
+        }
+
+        self::$LOG_REPOSITORY = self::$ROOT . "/log_archive";
+        self::$TEMPLATE_ROOT = self::$ROOT . "/lib/view";
+        self::$MODEL_ROOT = self::$ROOT . '/lib/model';
+        self::$CONTROLLER_ROOT = self::$ROOT . '/lib/controller';
+        self::$UTILS_ROOT = self::$ROOT . '/lib/utils';
+    }
+
+}
+
+
+/*
+if (!defined('CONST')) {
+    define('CONST', 1);
+    define('ROOT_URL', '/index.php');
+
+    if (!defined('ROOT')) {
+        // $root = dirname(getcwd());
+        $root = getcwd();
+        define('ROOT', $root);
+        switch ($_SERVER['HTTP_HOST']) {
+            case ('matecat.pi'):
+            case ('matecat.local'):
+                define('DEBUG', true);
+                define('DB_SERVER', "localhost");
+                define('DB_DATABASE', "matecat_sandbox"); //database name
+                define('DB_USER', "root"); //database login name
+                define('DB_PASS', "root"); //database login password
+                break;
+            case 'cattooldemo.matecat.com':
+            case 'matecat.translated.home':
+            case 'matecat.com':
+            case 'www.matecat.com':
+                define('DEBUG', false);
+                define('DB_SERVER', "213.215.131.241"); //database server
+                define('DB_DATABASE', "matecat"); //database name
+                define('DB_USER', "translated"); //database login name
                 define('DB_PASS', "azerty1"); //database login password
-
-
                 break;
             default :
                 die(__FILE__ . " " . __LINE__ . " : unrecognided domain. Unable to set ROOT");
@@ -45,9 +101,8 @@ if (!defined('CONST')) {
     define('MODEL_ROOT', ROOT . '/lib/model');
     define('CONTROLLER_ROOT', ROOT . '/lib/controller');
     define('UTILS_ROOT', ROOT . '/lib/utils');
-
-
-
-    //define('PROJECTS_ROOT', "/home/translated/projects");
 }
+ * 
+ */
+
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 error_reporting (E_ALL);
 /*
 print_r ($_REQUEST);
@@ -11,29 +10,29 @@ echo "</pre>";
 exit;
 */
 require_once 'inc/config.inc.php';
+
+INIT::obtain();
 //require_once UTILS_ROOT.'/utils.class.php';
 
-require_once UTILS_ROOT.'/log.class.php';
-require_once CONTROLLER_ROOT.'/frontController.php';
-require_once MODEL_ROOT.'/Database.class.php';
+require_once INIT::$UTILS_ROOT.'/log.class.php';
+require_once INIT::$CONTROLLER_ROOT.'/frontController.php';
+require_once INIT::$MODEL_ROOT.'/Database.class.php';
 //echo DB_DATABASE;exit;
-$db=Database::obtain(DB_SERVER, DB_USER, DB_PASS, DB_DATABASE);
+$db=Database::obtain(INIT::$DB_SERVER, INIT::$DB_USER, INIT::$DB_PASS, INIT::$DB_DATABASE);
 //var_dump($db);
 //exit;
-
+$db->debug=INIT::$DEBUG;
 $db->connect();
 
 $dispatcher= controllerDispatcher::obtain();
 //var_dump ($dispatcher);
 $controller=$dispatcher->getController();
 //var_dump ($controller);
-
-$controllerRet=$controller->doAction();
+$controller->doAction();
 
 
 if (get_parent_class($controller)=='ajaxcontroller'){	
 	$controller->echoJSONResult();
-	
 }else{	
 	$controller->executeTemplate();
 }
