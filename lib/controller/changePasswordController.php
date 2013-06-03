e79e85f7f8bb8116b34c7691e0860d4ed39300df
Antonio Farina <antonio@translated.net>
2013-06-03 09:38:37 +0200

    projects list implementation intermediate - added untracked files

---
+++ lib/controller/changePasswordController.php
@@ -0,0 +1,24 @@
+<?php
+include_once INIT::$MODEL_ROOT . "/queries.php";
+include_once INIT::$UTILS_ROOT . "/cat.class.php";
+
+class changePasswordController extends ajaxcontroller {
+      private $res_type;
+      private $res_id;
+	  
+      public function __construct() {
+            parent::__construct();
+            $this->res_type = $this->get_from_get_post('res');
+            $this->res_id = $this->get_from_get_post('id');
+      }
+
+      public function doAction() {
+        	$password = CatUtils::generate_password();
+
+        	$changePass = changePassword($this->res_type, $this->res_id,$password);
+            $this->result['password'] = $password;
+      }
+
+}
+
+?>
