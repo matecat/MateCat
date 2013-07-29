694e366ecc6d4e8ae90341ef3aad343270028b18
andreamartines <andreamartines@10.3.15.44>
2013-07-29 11:33:15 +0200

    - added first job segment data from the controller

--- lib/controller/catController.php
+++ lib/controller/catController.php
@@ -97,6 +97,7 @@ class catController extends viewcontroller {
 			$this->job_not_found = true;
 		}
 
+		log::doLog('DATA: ' , $data);
 
 		$first_not_translated_found = false;
 
@@ -111,6 +112,10 @@ class catController extends viewcontroller {
 				$this->last_opened_segment = $seg['last_opened_segment'];
 			}
 
+			if (empty($this->first_job_segment)) {
+				$this->first_job_segment = $seg['id_segment_start'];
+			}
+            
 			if (empty($this->cid)) {
 				$this->cid = $seg['cid'];
 			}
@@ -225,6 +230,7 @@ class catController extends viewcontroller {
 		$this->template->source_code = $this->source_code;
 		$this->template->target_code = $this->target_code;
 
+		$this->template->first_job_segment = $this->first_job_segment;
 		$this->template->last_opened_segment = $this->last_opened_segment;
 		$this->template->data = $this->data;
 
