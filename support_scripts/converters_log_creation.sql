3a79e78daa2782f679f5e903b549bfb08682bb5d
domenico <domenico@translated.net>
2013-11-18 10:30:50 +0100

    Removed trash in converters_log_creation.sql

--- support_scripts/converters_log_creation.sql
+++ support_scripts/converters_log_creation.sql
@@ -1,27 +1,6 @@
--- MySQL dump 10.13  Distrib 5.5.34, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: matecat_local
--- ------------------------------------------------------
--- Server version	5.5.34-0ubuntu0.13.10.1
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `converters`
---
 
 DROP TABLE IF EXISTS `converters`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `converters` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ip_converter` varchar(45) NOT NULL,
@@ -42,25 +21,21 @@ CREATE TABLE `converters` (
   KEY `status_offline` (`status_offline`),
   KEY `status_reboot` (`status_reboot`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `converters`
---
 
 LOCK TABLES `converters` WRITE;
 /*!40000 ALTER TABLE `converters` DISABLE KEYS */;
-INSERT INTO `converters` VALUES (1,'10.11.0.10','10.11.0.11','10.30.1.250','root','marcofancydandy','TradosAPI-2','2013-11-15 12:26:28',0,1,0),(2,'10.11.0.18','10.11.0.19','10.30.1.251','root','marcofancydandy','TradosAPI-3','2013-11-15 14:14:08',1,0,0),(3,'10.11.0.26','10.11.0.27','10.30.1.251','root','marcofancydandy','TradosAPI-4','2013-11-15 14:14:13',1,0,0),(4,'10.11.0.34','10.11.0.35','10.30.1.232','root','marcofancydandy','TradosAPI-5','2013-11-15 14:14:16',1,0,0),(5,'10.11.0.42','10.11.0.43','10.30.1.232','root','marcofancydandy','TradosAPI-6','2013-11-15 14:14:20',1,0,0);
+INSERT INTO `converters` VALUES
+(1,'10.11.0.10','10.11.0.11','10.30.1.250','root','marcofancydandy','TradosAPI-2','2013-11-15 12:26:28',0,1,0),
+(2,'10.11.0.18','10.11.0.19','10.30.1.251','root','marcofancydandy','TradosAPI-3','2013-11-15 14:14:08',1,0,0),
+(3,'10.11.0.26','10.11.0.27','10.30.1.251','root','marcofancydandy','TradosAPI-4','2013-11-15 14:14:13',1,0,0),
+(4,'10.11.0.34','10.11.0.35','10.30.1.232','root','marcofancydandy','TradosAPI-5','2013-11-15 14:14:16',1,0,0),
+(5,'10.11.0.42','10.11.0.43','10.30.1.232','root','marcofancydandy','TradosAPI-6','2013-11-15 14:14:20',1,0,0);
 /*!40000 ALTER TABLE `converters` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `converters_log`
---
 
 DROP TABLE IF EXISTS `converters_log`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `converters_log` (
   `id_log` int(11) NOT NULL AUTO_INCREMENT,
   `id_converter` int(11) NOT NULL,
@@ -71,24 +46,5 @@ CREATE TABLE `converters_log` (
   KEY `outcome_idx` (`test_passed`),
   KEY `id_converter_idx` (`id_converter`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `converters_log`
---
 
-LOCK TABLES `converters_log` WRITE;
-/*!40000 ALTER TABLE `converters_log` DISABLE KEYS */;
-/*!40000 ALTER TABLE `converters_log` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2013-11-15 19:44:34
