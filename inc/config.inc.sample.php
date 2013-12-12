04ee510797d345c1e847c2d2efbc4d57bbc131d8
domenico <domenico@translated.net>
2013-12-12 17:18:38 +0100

    Master Release Version 0.3.3.6
    Removed Glossary Tab.

--- inc/config.inc.sample.php
+++ inc/config.inc.sample.php
@@ -178,7 +178,7 @@ class INIT {
 		self::$ENABLED_BROWSERS = array('chrome', 'safari');
 		self::$CONVERSION_ENABLED = true;
 		self::$ANALYSIS_WORDS_PER_DAYS = 3000;
-		self::$BUILD_NUMBER = '0.3.3.5';
+		self::$BUILD_NUMBER = '0.3.3.6';
 		self::$VOLUME_ANALYSIS_ENABLED = true;
 
 		self::$WARNING_POLLING_INTERVAL = 10; //seconds
