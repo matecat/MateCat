10220449b8c9cb139e9fb7b5867ad03694810e98
root <root@translated.net>
2013-12-16 11:07:52 +0100

    Fixed bug in Project Manager
    Added set time limit to 0 in project creation

--- lib/utils/ProjectManager.php
+++ lib/utils/ProjectManager.php
@@ -682,7 +682,7 @@ class ProjectManager {
 
                                         //add an empty string to avoid casting to int: 0001 -> 1
                                         //useful for idiom internal xliff id
-                                        $this->projectStructure['translations']->offsetSet( "" . $xliff_trans_unit[ 'attr' ][ 'id' ] , $target );
+                                        $this->projectStructure['translations']->offsetSet( "" . $xliff_trans_unit[ 'attr' ][ 'id' ] , new ArrayObject( array( 2 => $target ) ) );
 
                                         //seg-source and target translation can have different mrk id
                                         //override the seg-source surrounding mrk-id with them of target
