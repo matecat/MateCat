b5b7f37968c35ad9e0e2b5a6ed63e3f20478c41a
Antonio Farina <antonio@translated.net>
2013-12-17 17:08:34 +0100

    added getGlossary after successfull  setGlossary

--- lib/controller/glossaryController.php
+++ lib/controller/glossaryController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: domenico
@@ -26,46 +27,44 @@ class glossaryController extends ajaxcontroller {
         parent::__construct();
 
         $filterArgs = array(
-            'exec'          => array( 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH ),
-            'id_job'        => array( 'filter' => FILTER_SANITIZE_NUMBER_INT ),
-            'password'      => array( 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH ),
-            'segment'       => array( 'filter' => FILTER_UNSAFE_RAW ),
-            'translation'   => array( 'filter' => FILTER_UNSAFE_RAW ),
+            'exec' => array('filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH),
+            'id_job' => array('filter' => FILTER_SANITIZE_NUMBER_INT),
+            'password' => array('filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH),
+            'segment' => array('filter' => FILTER_UNSAFE_RAW),
+            'translation' => array('filter' => FILTER_UNSAFE_RAW),
         );
 
         //$__postInput = filter_input_array( INPUT_POST, $filterArgs );
-
         //NOTE: This is for debug purpose only,
         //NOTE: Global $_POST Overriding from CLI
-        $__postInput = filter_var_array( $_POST, $filterArgs );
+        $__postInput = filter_var_array($_POST, $filterArgs);
 
-        $this->exec          = $__postInput[ 'exec' ];
-        $this->id_job        = $__postInput[ 'id_job' ];
-        $this->password      = $__postInput[ 'password' ];
-        $this->segment       = $__postInput[ 'segment' ];
-        $this->translation   = $__postInput[ 'translation' ];
-
+        $this->exec = $__postInput['exec'];
+        $this->id_job = $__postInput['id_job'];
+        $this->password = $__postInput['password'];
+        $this->segment = $__postInput['segment'];
+        $this->translation = $__postInput['translation'];
     }
 
     public function doAction() {
 
-        $st = getJobData( $this->id_job, $this->password );
+        $st = getJobData($this->id_job, $this->password);
 
         try {
 
-            if ( empty( $st['id_translator'] ) ) {
-                throw new Exception( "No Private Glossary Key provided for Job.", -1 );
+            if (empty($st['id_translator'])) {
+                throw new Exception("No Private Glossary Key provided for Job.", -1);
             }
 
             $config = TMS::getConfigStruct();
 
-            $config[ 'segment' ]       = $this->segment;
-            $config[ 'translation' ]   = $this->translation;
-            $config[ 'source_lang' ]   = $st['source'];
-            $config[ 'target_lang' ]   = $st['target'];
-            $config[ 'email' ]         = "demo@matecat.com";
-            $config[ 'id_user' ]       = $st['id_translator'];
-            $config[ 'isGlossary' ]    = true;
+            $config['segment'] = $this->segment;
+            $config['translation'] = $this->translation;
+            $config['source_lang'] = $st['source'];
+            $config['target_lang'] = $st['target'];
+            $config['email'] = "demo@matecat.com";
+            $config['id_user'] = $st['id_translator'];
+            $config['isGlossary'] = true;
 
             /**
              * For future reminder
@@ -73,31 +72,32 @@ class glossaryController extends ajaxcontroller {
              * MyMemory should not be the only Glossary provider
              *
              */
-            $_TMS = new TMS( 1 /* MyMemory */ );
+            $_TMS = new TMS(1 /* MyMemory */);
 
-            switch ( $this->exec ) {
+            switch ($this->exec) {
 
                 case 'get':
-                    $TMS_RESULT = $_TMS->get( $config )->get_glossary_matches_as_array();
-                    $this->result[ 'data' ][ 'matches' ] = $TMS_RESULT;
+                    $TMS_RESULT = $_TMS->get($config)->get_glossary_matches_as_array();
+                    $this->result['data']['matches'] = $TMS_RESULT;
                     break;
                 case 'set':
-                    $TMS_RESULT = $_TMS->set( $config );
-                    $this->result[ 'code' ] = $TMS_RESULT;
-                    $this->result[ 'data' ] = ( $TMS_RESULT ? 'OK' : null );
+                    $TMS_RESULT = $_TMS->set($config);
+                    $this->result['code'] = $TMS_RESULT;
+                    $this->result['data'] = ( $TMS_RESULT ? 'OK' : null );
+                    if ($this->result['code']) {
+                        $TMS_RESULT = $_TMS->get($config)->get_glossary_matches_as_array();
+                        $this->result['data']['matches'] = $TMS_RESULT;
+                    }
                     break;
                 case 'delete':
-                    $TMS_RESULT = $_TMS->delete( $config );
-                    $this->result[ 'code' ] = $TMS_RESULT;
-                    $this->result[ 'data' ] = ( $TMS_RESULT ? 'OK' : null );
+                    $TMS_RESULT = $_TMS->delete($config);
+                    $this->result['code'] = $TMS_RESULT;
+                    $this->result['data'] = ( $TMS_RESULT ? 'OK' : null );
                     break;
-
             }
-
-        } catch ( Exception $e ) {
-            $this->result[ 'errors' ][ ] = array( "code" => $e->getCode(), "message" => $e->getMessage() );
+        } catch (Exception $e) {
+            $this->result['errors'][] = array("code" => $e->getCode(), "message" => $e->getMessage());
         }
-
     }
 
 }
