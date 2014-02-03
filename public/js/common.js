52ab9b66743e1b233cfc55144fe11ea3b8e463ab
Andrea Martines <andrea.martines@gmail.com>
2014-02-03 15:26:20 +0100

    - Applied patch from Domenico for postprocessing function. - Fixed fitText function

--- public/js/common.js
+++ public/js/common.js
@@ -181,21 +181,23 @@ APP = {
 //            $('.popup:not(.hide), .popup-outer:not(.hide)').remove();
     },
     fitText: function(container,child,limitHeight) {
-        if(container.height() < (limitHeight+1)) return;
-        txt = child.text();
-        var name = txt;
-        var ext = '';
-        if(txt.split('.').length > 1) {
-            var extension = txt.split('.')[txt.split('.').length-1];
-            name = txt.replace('.'+extension,'');
-            ext = '.' + extension;
-        }
-        firstHalf = name.substr(0 , Math.ceil(name.length/2));
-        secondHalf = name.replace(firstHalf,'');
-        child.text(firstHalf.substr(0,firstHalf.length-1)+'[...]'+secondHalf.substr(1)+ext);
-        while (container.height() > limitHeight) {
-            child.text(child.text().replace(/(.)\[\.\.\.\](.)/,'[...]'));
-        }
+		if(container.height() < (limitHeight+1)) return;
+		txt = child.text();
+		var name = txt;
+		var ext = '';
+		if(txt.split('.').length > 1) {
+			var extension = txt.split('.')[txt.split('.').length-1];
+			name = txt.replace('.'+extension,'');
+			ext = '.' + extension;
+		}
+		firstHalf = name.substr(0 , Math.ceil(name.length/2));
+		secondHalf = name.replace(firstHalf,'');
+		child.text(firstHalf.substr(0,firstHalf.length-1)+'[...]'+secondHalf.substr(1)+ext);
+		while (container.height() > limitHeight) {
+			num = child.text().length;
+			child.text(child.text().replace(/(.)\[\.\.\.\](.)/,'[...]'));
+			if(num == child.text().length) break;
+		}
     },
     objectSize: function(obj) {
         var size = 0, key;
