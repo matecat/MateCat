f8832e94f34a692141ecbe005faca7672f548bf8
domenico <domenico@translated.net>
2014-02-11 11:27:53 +0100

    Adjusted Api status accordingly to documentation

--- lib/controller/API/StatusController.php
+++ lib/controller/API/StatusController.php
@@ -22,13 +22,13 @@ class StatusController extends getVolumeAnalysisController {
 
         $filterArgs = array(
                 'id_project' => array( 'filter' => FILTER_SANITIZE_NUMBER_INT ),
-                'ppassword'  => array( 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH ),
+                'project_pass'  => array( 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH ),
         );
 
         $__postInput = filter_input_array( INPUT_POST, $filterArgs );
 
         $this->id_project = $__postInput[ 'id_project' ];
-        $this->ppassword  = $__postInput[ 'ppassword' ];
+        $this->ppassword  = $__postInput[ 'project_pass' ];
 
     }
 
