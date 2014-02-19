558aa6a89ff7d256c6066babb7eb5a769c07cb3c
domenico <domenico@translated.net>
2014-02-19 16:25:41 +0100

    Minor Fixes

--- lib/utils/API/Upload.php
+++ lib/utils/API/Upload.php
@@ -95,17 +95,29 @@ class Upload  {
         if ( !empty ($fileError) ) {
 
             switch ($fileError) {
-                case 1 :
+                case 1 : //UPLOAD_ERR_INI_SIZE
                     throw new Exception ( __METHOD__ . " -> The file '$fileName' is bigger than this PHP installation allows.");
                     break;
-                case 2 :
+                case 2 : //UPLOAD_ERR_FORM_SIZE
                     throw new Exception ( __METHOD__ . " -> The file '$fileName' is bigger than this form allows.");
                     break;
-                case 3 :
+                case 3 : //UPLOAD_ERR_PARTIAL
                     throw new Exception ( __METHOD__ . " -> Only part of the file '$fileName'  was uploaded.");
                     break;
-                case 4 :
+                case 4 : //UPLOAD_ERR_NO_FILE
                     throw new Exception ( __METHOD__ . " -> No file was uploaded.");
+                    break;
+                case 6 : //UPLOAD_ERR_NO_TMP_DIR
+                    throw new Exception ( __METHOD__ . " -> Missing a temporary folder. ");
+                    break;
+                case 7 : //UPLOAD_ERR_CANT_WRITE
+                    throw new Exception ( __METHOD__ . " -> Failed to write file to disk.");
+                    break;
+                case 8 : //UPLOAD_ERR_EXTENSION
+                    throw new Exception ( __METHOD__ . " -> A PHP extension stopped the file upload. PHP does not provide a way to ascertain which extension caused the file upload to stop; examining the list of loaded extensions with phpinfo() may help.");
+                    break;
+                default:
+                    throw new Exception ( __METHOD__ . " -> Unknown Error.");
                     break;
             }
 
