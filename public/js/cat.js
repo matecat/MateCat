4003794813f07cfab30324c03f923f5a12cc9314
Andrea Martines <andrea.martines@gmail.com>
2014-02-20 10:49:00 +0100

    survey modified

--- public/js/cat.js
+++ public/js/cat.js
@@ -231,7 +231,7 @@ UI = {
 				'	<div class="popup-outer"></div>' +
 				'	<div class="popup">' +
 				'		<a href="#" class="x-popup"></a>' +
-				'		<h1>Survey</h1>' +
+				'		<h1>Translation Completed - Take a Survey</h1>' +
 				'		<div class="popup-box">' +
 				'			<iframe src="' + s + '" width="100%" height="400" frameborder="0" marginheight="0" marginwidth="0">Loading ...</iframe>' +
 				'		</div>' +
