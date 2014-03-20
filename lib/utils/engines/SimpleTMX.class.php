4244ee8e31fb3f6e5bd33fc30a767614d9de662f
Alberto Massidda <alberto@translated.net>
2014-03-20 13:26:34 +0100

    added support for public TMX importing and reporting added more error codes to manage different cases (wrong key)

--- lib/utils/engines/SimpleTMX.class.php
+++ lib/utils/engines/SimpleTMX.class.php
@@ -6,24 +6,29 @@ class SimpleTMX extends Engine{
 
 
 	public function __construct($id){
-        parent::__construct($id);
+		parent::__construct($id);
 	}
 
-	public function import($file,$key,$name=false){
+	public function import($file,$key=false,$name=false){
 
 		$postfields=array(
-				'key'=>$key,
 				'tmx'=>"@".realpath($file),
 				'name'=>$name
 				);
+		if(!empty($key)){
+			$postfields['key']=trim($key);
+		}
 		//query db
 		$this->doQuery('tmx_import', $postfields,true);
 		return $this->raw_result;
 	}
 
-	public function getStatus($key,$name=false){
+	public function getStatus($key=false,$name=false){
 
-		$parameters=array('key'=>$key);
+		$parameters=array();
+		if(!empty($key)){
+			$parameters['key']=trim($key);
+		}
 
 		//if provided, add name parameter
 		if($name) $parameters['name']=$name;
