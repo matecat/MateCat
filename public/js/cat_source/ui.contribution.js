828b3f8f827cd189f651556305737aef2f5bd834
Andrea Martines <andrea.martines@gmail.com>
2014-04-28 16:35:36 +0200

    draft 2 of propagation and translation alternatives

--- public/js/cat_source/ui.contribution.js
+++ public/js/cat_source/ui.contribution.js
@@ -3,6 +3,7 @@
  */
 $.extend(UI, {
 	chooseSuggestion: function(w) {console.log('chooseSuggestion');
+//		console.log($('.editor ul[data-item=' + w + '] li.b .translation'));
 		this.copySuggestionInEditarea(this.currentSegment, $('.editor ul[data-item=' + w + '] li.b .translation').text(), $('.editor .editarea'), $('.editor ul[data-item=' + w + '] ul.graysmall-details .percent').text(), false, false, w);
 		this.lockTags(this.editarea);
 		this.setChosenSuggestion(w);
