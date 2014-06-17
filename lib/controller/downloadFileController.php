4db264c7e7d880f7e5f8e3160dc2f75cbae9f059
domenico <domenico@translated.net>
2014-06-17 13:41:37 +0200

    Patch Fix for the download bug when upload failed to store original file in database ( Temporary )

--- lib/controller/downloadFileController.php
+++ lib/controller/downloadFileController.php
@@ -164,7 +164,7 @@ class downloadFileController extends downloadController {
                 // we already have an sdlxliff or an accepted file
                 $file['original_file'] = @gzinflate( $file['original_file'] );
 
-                if( !INIT::$CONVERSION_ENABLED || empty( $file['original_file'] ) ){
+                if( !INIT::$CONVERSION_ENABLED || ( empty( $file['original_file'] ) && $mime_type == 'sdlxliff' ) ){
                     $convertBackToOriginal = false;
                     Log::doLog( "SDLXLIFF: {$file['filename']} --- " . var_export( $convertBackToOriginal , true ) );
                 }
@@ -217,10 +217,10 @@ class downloadFileController extends downloadController {
 
         $debug[ 'total' ][ ] = time();
 
-        unlink( $path );
-        unlink( $path . '.out.sdlxliff' );
-        rmdir( INIT::$TMP_DOWNLOAD . '/' . $this->id_job . '/' . $id_file . '/' );
-        rmdir( INIT::$TMP_DOWNLOAD . '/' . $this->id_job . '/' );
+//        unlink( $path );
+//        unlink( $path . '.out.sdlxliff' );
+//        rmdir( INIT::$TMP_DOWNLOAD . '/' . $this->id_job . '/' . $id_file . '/' );
+//        rmdir( INIT::$TMP_DOWNLOAD . '/' . $this->id_job . '/' );
 
     }
 
