50f3bd3dd2cfe8c710b806e7631575912b02b7ad
Andrea Martines <andrea.martines@gmail.com>
2014-07-03 12:29:34 +0200

    temp commit

--- public/js/cat.js
+++ public/js/cat.js
@@ -2403,11 +2403,42 @@ UI = {
 			this.setProgress(d.stats);
 			//check status of global warnings
 			this.checkWarnings(false);
+            this.beforePropagateTranslation(segment, status);
+        }
+    },
+    beforePropagateTranslation: function(segment, status) {
+        console.log('before propagate');
+        checkBefore = false;
+        if(checkBefore) {
+            APP.popup({
+                name: 'confirmPropagation',
+                title: 'Warning',
+                buttons: [
+                    {
+                        type: 'ok',
+                        text: 'Yes',
+                        callback: 'doPropagate',
+                        params: 'true',
+                        closeOnClick: 'true'
+                    },
+                    {
+                        type: 'cancel',
+                        text: 'No, thanks',
+                        callback: 'doPropagate',
+                        params: 'false',
+                        closeOnClick: 'true'
+                    }
+                ],
+                content: "Dou you want to extend the autopropagation of this translation even to already translated segments?"
+            });
+        } else {
             this.propagateTranslation(segment, status);
         }
     },
+
     propagateTranslation: function(segment, status) {
 //        console.log($(segment).attr('data-hash'));
+
 
         if( status == 'translated' ){
             //unset actual segment as autoPropagated because now it is translated
@@ -2422,8 +2453,11 @@ UI = {
         });
         $('section[data-hash=' + $(segment).attr('data-hash') + ']');
     },
+    doPropagate: function (trans) {
+        console.log('trans: ', trans);
+    },
 
-	setWaypoints: function() {
+    setWaypoints: function() {
 		this.firstSegment.waypoint('remove');
 		this.lastSegment.waypoint('remove');
 		this.detectFirstLast();
