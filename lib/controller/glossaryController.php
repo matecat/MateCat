68825d24b6bcdb107aac8c800df45e1fa4a6a0ca
domenico <domenico@translated.net>
2014-09-17 19:21:31 +0200

    Fix update/delete glossary, delete Contribution, set Translation

--- lib/controller/glossaryController.php
+++ lib/controller/glossaryController.php
@@ -21,6 +21,8 @@ class glossaryController extends ajaxController {
     private $translation;
     private $comment;
     private $automatic;
+    private $_TMS;
+    private $job_info;
 
     public function __construct() {
 
@@ -52,7 +54,15 @@ class glossaryController extends ajaxController {
 
     public function doAction() {
 
-        $st = getJobData( $this->id_job, $this->password );
+        $this->job_info = getJobData( $this->id_job, $this->password );
+
+        /**
+         * For future reminder
+         *
+         * MyMemory should not be the only Glossary provider
+         *
+         */
+        $this->_TMS = new TMS( 1 /* MyMemory */ );
 
         try {
 
@@ -61,167 +71,237 @@ class glossaryController extends ajaxController {
             $config[ 'segment' ]     = $this->segment;
             $config[ 'translation' ] = $this->translation;
             $config[ 'tnote' ]       = $this->comment;
-            $config[ 'source_lang' ] = $st[ 'source' ];
-            $config[ 'target_lang' ] = $st[ 'target' ];
+            $config[ 'source_lang' ] = $this->job_info[ 'source' ];
+            $config[ 'target_lang' ] = $this->job_info[ 'target' ];
             $config[ 'email' ]       = "demo@matecat.com";
-            $config[ 'id_user' ]     = $st[ 'id_translator' ];
+            $config[ 'id_user' ]     = $this->job_info[ 'id_translator' ];
             $config[ 'isGlossary' ]  = true;
             $config[ 'get_mt' ]      = null;
             $config[ 'num_result' ]  = 100; //do not want limit the results from glossary: set as a big number
 
-            /**
-             * For future reminder
-             *
-             * MyMemory should not be the only Glossary provider
-             *
-             */
-            $_TMS = new TMS( 1 /* MyMemory */ );
-
             switch ( $this->exec ) {
 
                 case 'get':
-                    //get TM keys with read grants
-                    $tm_keys = TmKeyManagement_TmKeyManagement::getJobTmKeys( $st[ 'tm_keys' ], 'r', 'glossary' );
-
-                    if ( count( $tm_keys ) ) {
-                        $config[ 'id_user' ] = array();
-                        /**
-                         * @var $tm_key TmKeyManagement_TmKeyStruct
-                         */
-                        foreach ( $tm_keys as $tm_key ) {
-                            $config[ 'id_user' ][ ] = $tm_key->key;
-                        }
-                    }
-
-                    $TMS_RESULT = $_TMS->get( $config )
-                                       ->get_glossary_matches_as_array();
-
-                    /**
-                     * Return only exact matches in glossary when a search is executed over the entire segment
-                     * Reordered by positional status of matches in source
-                     *
-                     * Example:
-                     * Segment: On average, Members of the House of Commons have 4,2 support staff.
-                     *
-                     * Glossary terms found: House of Commons, House of Lords
-                     *
-                     * Return: House of Commons
-                     *
-                     */
-                    if ( $this->automatic ) {
-                        $tmp_Result = array();
-                        foreach ( $TMS_RESULT as $k => $val ) {
-                            if ( ( $res = mb_stripos( $this->segment, preg_replace( '/[ \t\n\r\0\x0A\xA0]+$/u', '', $k ) ) ) === false ) {
-                                unset( $TMS_RESULT[ $k ] );
-                            } else {
-                                $tmp_Result[ $res ] = $k;
-                            }
-                        }
-                        ksort( $tmp_Result ); //sort by position in source
-                        $ordered_Result = array();
-                        foreach ( $tmp_Result as $glossary_matches ) {
-                            $ordered_Result[ $glossary_matches ] = $TMS_RESULT[ $glossary_matches ];
-                        }
-                        $TMS_RESULT = $ordered_Result;
-                    }
-                    $this->result[ 'data' ][ 'matches' ] = $TMS_RESULT;
-
+                    $this->_get( $config );
                     break;
                 case 'set':
-
-                    if ( $st[ 'id_tms' ] == 0 ) {
+                    /**
+                     * For future reminder
+                     *
+                     * MyMemory should not be the only Glossary provider
+                     *
+                     */
+                    if ( $this->job_info[ 'id_tms' ] == 0 ) {
                         throw new Exception( "Glossary is not available when the TM feature is disabled", -11 );
                     }
+                    $this->_set( $config );
+                    break;
+                case 'update':
+                    $this->_update( $config );
+                    break;
+                case 'delete':
+                    $this->_delete( $config );
+                    break;
 
-                    //get tm keys with write grants
-                    $tm_keys = TmKeyManagement_TmKeyManagement::getJobTmKeys( $st[ 'tm_keys' ], 'w', 'glossary' );
+            }
 
-                    if ( empty( $tm_keys ) ) {
+        } catch ( Exception $e ) {
+            $this->result[ 'errors' ][ ] = array( "code" => $e->getCode(), "message" => $e->getMessage() );
+        }
+        
+    }
 
-                        $APIKeySrv = TMSServiceFactory::getAPIKeyService();
-                        $newUser   = $APIKeySrv->createMyMemoryKey(); //throws exception
+    protected function _get( $config ){
 
-                        //TODO Replace with User Key Management
-                        updateTranslatorJob( $this->id_job, $newUser );
-                        $config[ 'id_user' ] = $newUser->id;
+        //get TM keys with read grants
+        $tm_keys = TmKeyManagement_TmKeyManagement::getJobTmKeys( $this->job_info[ 'tm_keys' ], 'r', 'glossary' );
 
-                        $new_key        = TmKeyManagement_TmKeyManagement::getTmKeyStructure();
-                        $new_key->tm    = 1;
-                        $new_key->glos  = 1;
-                        $new_key->key   = $newUser->key;
-                        $new_key->owner = 0;
-                        $new_key->r     = 1;
-                        $new_key->w     = 1;
+        if ( count( $tm_keys ) ) {
+            $config[ 'id_user' ] = array();
+            /**
+             * @var $tm_key TmKeyManagement_TmKeyStruct
+             */
+            foreach ( $tm_keys as $tm_key ) {
+                $config[ 'id_user' ][ ] = $tm_key->key;
+            }
+        }
 
-                        //create an empty array
-                        $tm_keys   = array();
-                        //append new key
-                        $tm_keys[] = $new_key;
+        $TMS_RESULT = $this->_TMS->get( $config )->get_glossary_matches_as_array();
 
-                        TmKeyManagement_TmKeyManagement::setJobTmKeys( $this->id_job, $this->password, $tm_keys );
+        /**
+         * Return only exact matches in glossary when a search is executed over the entire segment
+         * Reordered by positional status of matches in source
+         *
+         * Example:
+         * Segment: On average, Members of the House of Commons have 4,2 support staff.
+         *
+         * Glossary terms found: House of Commons, House of Lords
+         *
+         * Return: House of Commons
+         *
+         */
+        if ( $this->automatic ) {
+            $tmp_Result = array();
+            foreach ( $TMS_RESULT as $k => $val ) {
+                if ( ( $res = mb_stripos( $this->segment, preg_replace( '/[ \t\n\r\0\x0A\xA0]+$/u', '', $k ) ) ) === false ) {
+                    unset( $TMS_RESULT[ $k ] );
+                } else {
+                    $tmp_Result[ $res ] = $k;
+                }
+            }
+            ksort( $tmp_Result ); //sort by position in source
+            $ordered_Result = array();
+            foreach ( $tmp_Result as $glossary_matches ) {
+                $ordered_Result[ $glossary_matches ] = $TMS_RESULT[ $glossary_matches ];
+            }
+            $TMS_RESULT = $ordered_Result;
+        }
+        $this->result[ 'data' ][ 'matches' ] = $TMS_RESULT;
 
-                    }
+    }
 
-                    //prepare the error report
-                    $set_code = array();
-                    //set the glossary entry for each key with write grants
-                    if ( count( $tm_keys ) ) {
+    protected function _set( $config ){
 
-                        /**
-                         * @var $tm_key TmKeyManagement_TmKeyStruct
-                         */
-                        foreach ( $tm_keys as $tm_key ) {
-                            $config[ 'id_user' ] = $tm_key->key;
+        //get tm keys with write grants
+        $tm_keys = TmKeyManagement_TmKeyManagement::getJobTmKeys( $this->job_info[ 'tm_keys' ], 'w', 'glossary' );
 
-                            $TMS_RESULT = $_TMS->set( $config );
+        if ( empty( $tm_keys ) ) {
 
-                            $set_code[ ] = $TMS_RESULT;
-                        }
+            $APIKeySrv = TMSServiceFactory::getAPIKeyService();
+            $newUser   = $APIKeySrv->createMyMemoryKey(); //throws exception
 
-                    }
+            //TODO Replace with User Key Management
+            updateTranslatorJob( $this->id_job, $newUser );
+            $config[ 'id_user' ] = $newUser->id;
 
-                    $set_successful = true;
-                    if( array_search( false, $set_code, true ) ){
-                        //There's an error, for now skip, let's assume that are not errors
-//                        $set_successful = false;
-                    }
+            $new_key        = TmKeyManagement_TmKeyManagement::getTmKeyStructure();
+            $new_key->tm    = 1;
+            $new_key->glos  = 1;
+            $new_key->key   = $newUser->key;
+            $new_key->owner = 0;
+            $new_key->r     = 1;
+            $new_key->w     = 1;
 
-                    if ( $set_successful ) {
-//                      Often the get method after a set is not in real time, so return the same values ( FAKE )
-//                      $TMS_GET_RESULT = $_TMS->get($config)->get_glossary_matches_as_array();
-//                      $this->result['data']['matches'] = $TMS_GET_RESULT;
-                        $this->result[ 'data' ][ 'matches' ] = array(
-                            $config[ 'segment' ] => array(
-                                array(
+            //create an empty array
+            $tm_keys   = array();
+            //append new key
+            $tm_keys[] = $new_key;
+
+            TmKeyManagement_TmKeyManagement::setJobTmKeys( $this->id_job, $this->password, $tm_keys );
+
+        }
+
+        //prepare the error report
+        $set_code = array();
+        //set the glossary entry for each key with write grants
+        if ( count( $tm_keys ) ) {
+
+            /**
+             * @var $tm_keys TmKeyManagement_TmKeyStruct[]
+             */
+            foreach ( $tm_keys as $tm_key ) {
+                $config[ 'id_user' ] = $tm_key->key;
+                $TMS_RESULT = $this->_TMS->set( $config );
+                $set_code[ ] = $TMS_RESULT;
+            }
+
+        }
+
+        $set_successful = true;
+        if( array_search( false, $set_code, true ) ){
+            //There's an error, for now skip, let's assume that are not errors
+//          $set_successful = false;
+        }
+
+        if ( $set_successful ) {
+//          Often the get method after a set is not in real time, so return the same values ( FAKE )
+//          $TMS_GET_RESULT = $this->_TMS->get($config)->get_glossary_matches_as_array();
+//          $this->result['data']['matches'] = $TMS_GET_RESULT;
+            $this->result[ 'data' ][ 'matches' ] = array(
+                    $config[ 'segment' ] => array(
+                            array(
                                     'segment'          => $config[ 'segment' ],
                                     'translation'      => $config[ 'translation' ],
                                     'last_update_date' => date_create()->format( 'Y-m-d H:i:m' ),
                                     'last_updated_by'  => "Matecat user",
                                     'created_by'       => "Matecat user",
                                     'target_note'      => $config[ 'tnote' ],
-                                )
                             )
-                        );
-                    }
+                    )
+            );
 
-                    break;
-                case 'update':
-                    $TMS_RESULT = $_TMS->update( $config );
-                    $set_code   = $TMS_RESULT;
-                    if ( $set_code ) {
-                        $TMS_GET_RESULT                      = $_TMS->get( $config )
-                                                                    ->get_glossary_matches_as_array();
-                        $this->result[ 'data' ][ 'matches' ] = $TMS_GET_RESULT;
-                    }
-                    break;
-                case 'delete':
-                    $TMS_RESULT             = $_TMS->delete( $config );
-                    $this->result[ 'code' ] = $TMS_RESULT;
-                    $this->result[ 'data' ] = ( $TMS_RESULT ? 'OK' : null );
-                    break;
-            }
-        } catch ( Exception $e ) {
-            $this->result[ 'errors' ][ ] = array( "code" => $e->getCode(), "message" => $e->getMessage() );
         }
+
     }
+
+    protected function _update( $config ){
+
+        //get tm keys with write grants
+        $tm_keys = TmKeyManagement_TmKeyManagement::getJobTmKeys( $this->job_info[ 'tm_keys' ], 'w', 'glossary' );
+
+        //prepare the error report
+        $set_code = array();
+        //set the glossary entry for each key with write grants
+        if ( count( $tm_keys ) ) {
+
+            /**
+             * @var $tm_key TmKeyManagement_TmKeyStruct
+             */
+            foreach ( $tm_keys as $tm_key ) {
+                $config[ 'id_user' ] = $tm_key->key;
+                $TMS_RESULT = $this->_TMS->update( $config );
+                $set_code[ ] = $TMS_RESULT;
+            }
+
+        }
+
+        $set_successful = true;
+        if( array_search( false, $set_code, true ) ){
+            $set_successful = false;
+        }
+
+        //reset key list
+        $config[ 'id_user' ] = array();
+        foreach ( $tm_keys as $tm_key ) {
+            $config[ 'id_user' ][ ] = $tm_key->key;
+        }
+
+        if ( $set_successful ) {
+            $TMS_GET_RESULT = $this->_TMS->get( $config )->get_glossary_matches_as_array();
+            $this->result[ 'data' ][ 'matches' ] = $TMS_GET_RESULT;
+        }
+
+    }
+
+    protected function _delete( $config ){
+
+        //get tm keys with write grants
+        $tm_keys = TmKeyManagement_TmKeyManagement::getJobTmKeys( $this->job_info[ 'tm_keys' ], 'w', 'glossary' );
+
+        //prepare the error report
+        $set_code = array();
+        //set the glossary entry for each key with write grants
+        if ( count( $tm_keys ) ) {
+
+            /**
+             * @var $tm_key TmKeyManagement_TmKeyStruct
+             */
+            foreach ( $tm_keys as $tm_key ) {
+                $config[ 'id_user' ] = $tm_key->key;
+                $TMS_RESULT = $this->_TMS->delete( $config );
+                $set_code[ ] = $TMS_RESULT;
+            }
+
+        }
+
+        $set_successful = true;
+        if( array_search( false, $set_code, true ) ){
+            $set_successful = false;
+        }
+
+        $this->result[ 'code' ] = $set_successful;
+        $this->result[ 'data' ] = ( $set_successful ? 'OK' : null );
+
+    }
+
 }
