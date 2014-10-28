232eaf19dd25846004445265e9a920ea6a0bfe0d
domenico <domenico@translated.net>
2014-10-28 11:16:33 +0100

    Fixed bug in loadTMX ( no error when no files uploaded )

--- lib/utils/TMLoader.php
+++ lib/utils/TMLoader.php
@@ -116,11 +116,9 @@ class TMLoader {
     public function uploadFile() {
         try {
 
-            if ( empty($_FILES) ) throw new Exception ( "No files uploaded." );
-
             $uploadManager = new Upload();
             $uploadedFiles = $uploadManager->uploadFiles( $_FILES ); Log::doLog( $_FILES );
-            
+
         } catch ( Exception $e ) {
             Log::doLog( $e->getMessage() );
             throw new Exception( $e->getMessage(), -8);
