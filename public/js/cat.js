286497c245b78025e947ca72eaffb46dbf6cd672
domenico <domenico@translated.net>
2014-11-11 11:27:22 +0100

    Increased version Number

--- public/js/cat.js
+++ public/js/cat.js
@@ -3102,7 +3102,7 @@ $(window).resize(function() {
 $.extend(UI, {
 	init: function() {
 		this.initStart = new Date();
-		this.version = "0.4.1.1";
+		this.version = "0.4.1.1b";
 		if (this.debug)
 			console.log('Render time: ' + (this.initStart - renderStart));
 		this.numContributionMatchesResults = 3;
