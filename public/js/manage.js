b10a9387724e600c154fd768efb27c3d2aba6a33
Daniele Giliberto <daniele@translated.net>
2014-12-10 16:26:42 +0100

    export sdlxliff button

--- public/js/manage.js
+++ public/js/manage.js
@@ -703,6 +703,7 @@ UI = {
 		            '            <a class="resume" href="#" title="Resume Job">Resume</a>'+
 		            '            <a class="unarchive" href="#" title="Unarchive Job">Unarchive</a>'+
 		            '            <a class="sdlxliff" target="_blank" href="/?action=downloadFile&id_job=' + this.id + '&password=' + this.password + '&forceXliff=1" title="Bilingual Trados file. Ideal to run QA or finish the work offline"></a>'+
+		            '            <a href="#" class="pull-left btn-ok download-manage"><span>Export SDLXLIFF</span></a>'+
 		            '        </td>'+
 		            '    </tr>';
 
