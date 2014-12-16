14d68045542daca67da5a8c5cd6eaf09f2bb8c55
domenico <domenico@translated.net>
2014-12-16 13:30:59 +0100

    Fixed: Se tolgo TMX, non prosegue con l'analisi

--- lib/controller/createProjectController.php
+++ lib/controller/createProjectController.php
@@ -239,7 +239,8 @@ class createProjectController extends ajaxController {
             //FIXME THIS IS A WORKAROUND, AN WARNING LEVEL MESSAGE SHOULD BE RAISED INSTEAD OF ERROR
             $delete_session = true;
             foreach( $this->result[ 'errors' ] as $err ){
-                if( $err['code'] == -17 ){
+                //these are error for tmx load error because of languages, skip these
+                if( $err['code'] == -17 || $err['code'] == -16 ){
                     $delete_session = false;
                 }
             }
