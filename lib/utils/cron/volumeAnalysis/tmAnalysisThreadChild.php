c2745d65cc75f09a2c930d318e499b4770779d98
domenico <domenico@translated.net>
2015-03-18 12:48:41 +0100

    Fixed debug error

--- lib/utils/cron/volumeAnalysis/tmAnalysisThreadChild.php
+++ lib/utils/cron/volumeAnalysis/tmAnalysisThreadChild.php
@@ -40,12 +40,12 @@ echo "--- (child $my_pid) : parent pid is $parent_pid\n";
 $memcacheHandler = MemcacheHandler::getInstance();
 
 while ( 1 ) {
-    if ( 0 && !processFileExists( $my_pid ) ) {
+    if ( !processFileExists( $my_pid ) ) {
         die( "(child $my_pid) :  EXITING!  my file does not exists anymore\n" );
     }
 
     // control if parent is still running
-    if ( 0 && !isRunningProcess( $parent_pid ) ) {
+    if ( !isRunningProcess( $parent_pid ) ) {
         echo "--- (child $my_pid) : EXITING : parent seems to be died.\n";
         exit ( -1 );
     }
