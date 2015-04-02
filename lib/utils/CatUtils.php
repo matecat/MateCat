317b5e7f3fa4dcc832b1bf00899083b72379a853
domenico <domenico@translated.net>
2015-04-02 17:53:39 +0200

    Improved segment split parsing

--- lib/utils/CatUtils.php
+++ lib/utils/CatUtils.php
@@ -444,7 +444,7 @@ class CatUtils {
                 $separator     = $separateWithChar;
 
                 //if the last char of the last chunk AND the first of the next are spaces, don't add another one
-                if( substr( $chunk, -1 ) == " " || @substr( $split_chunks[ $pos + 1 ], 0, 1 ) == " " ){
+                if( substr( $chunk, -1 ) == $separateWithChar || @substr( $split_chunks[ $pos + 1 ], 0, 1 ) == $separateWithChar ){
                     $separator_len = 0;
                     $separator = '';
                 }
