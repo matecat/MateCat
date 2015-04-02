22179f97ad4d7eac3a9a568310268bad87bbe5d4
Antonio Farina <antonio@translated.net>
2015-04-02 18:40:40 +0200

    changer back filter_var from get to post

--- lib/controller/editlogDownloadController.php
+++ lib/controller/editlogDownloadController.php
@@ -21,7 +21,7 @@ class editlogDownloadController extends downloadController {
                 'password' => array( 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH ),
         );
 
-        $__postInput = filter_input_array( INPUT_GET, $filterArgs );
+        $__postInput = filter_input_array( INPUT_POST, $filterArgs );
 
         $this->jid      = $__postInput[ "jid" ];
         $this->password = $__postInput[ "password" ];
