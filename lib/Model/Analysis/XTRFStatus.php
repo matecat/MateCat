c3dc317af077da91a8e53a25bbbf1a261ef47c14
Domenico Lupinetti <ostico@gmail.com>
2015-05-11 23:42:44 +0200

    Added empty Model

---
+++ lib/Model/Analysis/XTRFStatus.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * @author domenico domenico@translated.net / ostico@gmail.com
+ * Date: 04/05/15
+ * Time: 13.37
+ *
+ */
+class Analysis_XTRFStatus extends Analysis_APIStatus {
+
+    public function getResult(){
+
+
+
+
+    }
+
+}
