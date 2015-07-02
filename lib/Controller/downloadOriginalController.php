5d655ebaf56f059e9e9ede2452b00da20cd69e62
domenico <domenico@translated.net>
2015-07-02 21:11:25 +0200

    Original Zip files in storage phase 2 ( Download Original )

--- lib/Controller/downloadOriginalController.php
+++ lib/Controller/downloadOriginalController.php
@@ -9,6 +9,8 @@ class downloadOriginalController extends downloadController {
     private $fname;
     private $download_type;
     private $id_file;
+    private $id_project;
+    private $project_date;
 
 
     public function __construct() {
@@ -48,163 +50,77 @@ class downloadOriginalController extends downloadController {
         $fs        = new FilesStorage();
         $files_job = $fs->getOriginalFilesForJob( $this->id_job, $this->id_file, $this->password );
 
-        $output_content  = array();
-        $thereIsAZipFile = false;
+        //take the project ID and creation date, array index zero is good, all id are equals
+        $this->id_project   = $files_job[0]['id_project'];
+        $this->project_date = $files_job[0]['create_date'];
 
+        $output_content  = array();
 
         foreach ( $files_job as $file ) {
+
             $id_file = $file[ 'id_file' ];
 
             $zipPathInfo = ZipArchiveExtended::zipPathInfo( $file[ 'filename' ] );
+
             if ( is_array( $zipPathInfo ) ) {
-                $thereIsAZipFile                                 = true;
-                $output_content[ $id_file ][ 'zipfilename' ]     = $zipPathInfo[ 'zipfilename' ];
-                $output_content[ $id_file ][ 'zipinternalPath' ] = $zipPathInfo[ 'dirname' ];
-                $output_content[ $id_file ][ 'filename' ]        = $zipPathInfo[ 'basename' ];
+                $output_content[ $id_file ][ 'output_filename' ] = $zipPathInfo[ 'zipfilename' ];
+                $output_content[ $id_file ][ 'input_filename' ]  = $fs->getOriginalZipPath( $this->project_date, $this->id_project, $zipPathInfo[ 'zipfilename' ] );
             } else {
-                $output_content[ $id_file ][ 'filename' ] = $file[ 'filename' ];
+                $output_content[ $id_file ][ 'output_filename' ] = $file[ 'filename' ];
+                $output_content[ $id_file ][ 'input_filename' ]  = $file[ 'originalFilePath' ];
             }
 
-            $output_content[ $id_file ][ 'contentPath' ] = $file[ 'originalFilePath' ];
         }
 
+        /*
+         * get Unique file zip because there are more than one file in the zip
+         * array_unique compares items using a string comparison.
+         *
+         * From the docs:
+         * Note: Two elements are considered equal if and only if (string) $elem1 === (string) $elem2.
+         * In words: when the string representation is the same. The first element will be used.
+         */
+        $output_content = array_map( 'unserialize', array_unique( array_map( 'serialize', $output_content ) ) );
+
+        foreach ( $output_content as $key => $iFile ) {
+            $output_content[ $key ] = new ZipContentObject( $iFile );
+        }
 
         if ( $this->download_type == 'all' ) {
-            if ( $thereIsAZipFile ) {
-                $output_content = $this->getOutputContentsWithZipFiles( $output_content );
-            }
 
             if ( count( $output_content ) > 1 ) {
 
-                foreach ( $output_content as $key => $iFile ) {
-                    $output_content[ $key ] = new ZipContentObject( $iFile );
-                }
-
                 $this->_filename = $this->fname;
-                $pathinfo        = pathinfo( $this->fname );
+                $pathInfo        = pathinfo( $this->fname );
 
-                if ( $pathinfo[ 'extension' ] != 'zip' ) {
-                    $this->_filename = $pathinfo[ 'basename' ] . ".zip";
+                if ( $pathInfo[ 'extension' ] != 'zip' ) {
+                    $this->_filename = $pathInfo[ 'basename' ] . ".zip";
                 }
 
                 $this->content = self::composeZip( $output_content ); //add zip archive content here;
+
             } elseif ( count( $output_content ) == 1 ) {
                 $this->setContent( $output_content );
             }
+
         } else {
+
             $this->setContent( $output_content );
+
         }
+
     }
 
     /**
      * There is a foreach, but this should be always one element
      *
-     * @param $output_content
+     * @param $output_content ZipContentObject[]
      */
     private function setContent( $output_content ) {
         foreach ( $output_content as $oc ) {
-            $this->_filename = $oc[ 'filename' ];
-            if(isset($oc[ 'document_content' ]) && !empty($oc[ 'document_content' ])) {
-                $this->content = $oc[ 'document_content' ];
-            }
-            else {
-                $this->content = file_get_contents( $oc[ 'contentPath' ] );
-            }
+            $this->_filename = $oc->output_filename;
+            $this->content   = $oc->getContent();
         }
-    }
-
-
-
-//    private function getOutputContentsWithZipFiles( $output_content ) {
-//        $zipFiles         = array();
-//        $newOutputContent = array();
-//
-//        //group files by zip archive
-//        foreach ( $output_content as $idFile => $fileInformations ) {
-//            //If this file comes from a ZIP, add it to $zipFiles
-//            if ( isset( $fileInformations[ 'zipfilename' ] ) ) {
-//                $zipFiles[ $fileInformations[ 'zipfilename' ] ][ ] = $fileInformations;
-//                unset( $output_content[ $idFile ] );
-//            }
-//        }
-//        unset( $idFile );
-//        unset( $fileInformations );
-//
-//        //for each zip file index, compose zip again, save it to a temporary location and add it into output_content
-//        foreach ( $zipFiles as $zipFileName => $internalFile ) {
-//            foreach ( $internalFile as $__idx => $fileInformations ) {
-//                $internalFile[ $__idx ][ 'filename' ] = $fileInformations[ 'zipinternalPath' ] . DIRECTORY_SEPARATOR . $fileInformations[ 'filename' ];
-//            }
-//
-//            $zip      = $this->composeZip( $internalFile );
-//            $savedZip = tempnam( "/tmp", "matecatzip" );
-//
-//            file_put_contents( $savedZip, $zip );
-//
-//            $newOutputContent[ ] = array(
-//                    'filename'    => $zipFileName,
-//                    'contentPath' => $savedZip
-//            );
-//        }
-//
-//        $newOutputContent = array_merge( $newOutputContent, $output_content );
-//
-//        return $newOutputContent;
-//    }
-
-    private static function getOutputContentsWithZipFiles( $output_content ) {
-        $zipFiles         = array();
-        $newOutputContent = array();
-
-        //group files by zip archive
-        foreach ( $output_content as $idFile => $fileInformations ) {
-            //If this file comes from a ZIP, add it to $zipFiles
-            if ( isset( $fileInformations[ 'zipfilename' ] ) ) {
-                $zipFileName = $fileInformations[ 'zipfilename' ];
-
-                $zipFiles[ $zipFileName ][ ] = $fileInformations;
-                unset( $output_content[ $idFile ] );
-            }
-        }
-        unset( $idFile );
-        unset( $fileInformations );
-
-        //for each zip file index, compose zip again, save it to a temporary location and add it into output_content
-        foreach ( $zipFiles as $zipFileName => $internalFile ) {
-            foreach ( $internalFile as $__idx => $fileInformations ) {
-                $zipFiles[ $zipFileName ][ $__idx ][ 'output_filename' ] = $fileInformations[ 'zipinternalPath' ] . DIRECTORY_SEPARATOR . $fileInformations[ 'filename' ];
-
-                $zipFiles[ $zipFileName ][ $__idx ][ 'document_content' ] = $internalFile[ $__idx ][ 'documentContent' ];
-
-                unset( $zipFiles[ $zipFileName ][ $__idx ][ 'documentContent' ] );
-                unset( $zipFiles[ $zipFileName ][ $__idx ][ 'filename' ] );
-                unset( $zipFiles[ $zipFileName ][ $__idx ][ 'zipinternalPath' ] );
-                unset( $zipFiles[ $zipFileName ][ $__idx ][ 'zipfilename' ] );
-                unset( $zipFiles[ $zipFileName ][ $__idx ][ 'source' ] );
-                unset( $zipFiles[ $zipFileName ][ $__idx ][ 'target' ] );
-                unset( $zipFiles[ $zipFileName ][ $__idx ][ 'out_xliff_name' ] );
-            }
-
-            $internalFile = $zipFiles[ $zipFileName ];
-            $internalFile = self::getOutputContentsWithZipFiles( $internalFile );
-
-            foreach ( $internalFile as $key => $iFile ) {
-                $iFile[ 'input_filename' ] = $iFile[ 'contentPath' ];
-                unset ( $iFile[ 'contentPath' ] );
-                $internalFile[ $key ] = new ZipContentObject( $iFile );
-            }
-
-            $zip = self::composeZip( $internalFile );
-
-            $newOutputContent[ ] = array(
-                    'output_filename'  => $zipFileName,
-                    'document_content' => $zip
-            );
-        }
-
-        $newOutputContent = array_merge( $newOutputContent, $output_content );
-
-        return $newOutputContent;
     }
 
 }
