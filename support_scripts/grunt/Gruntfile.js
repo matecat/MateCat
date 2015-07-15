2b1e90110022c491f9548316261bef5bda174a96
Fabrizio Regini <freegenie@gmail.com>
2015-07-15 23:20:09 +0200

    removed tabs from Gruntfile

--- support_scripts/grunt/Gruntfile.js
+++ support_scripts/grunt/Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
-	var basePath = '../../public/js/';
+    var basePath = '../../public/js/';
     var buildPath = '../../public/js/build/';
-	var incPath = '../../inc/';
+    var incPath = '../../inc/';
 
     var cssFiles = [
         basePath + '../css/common.css',
@@ -14,23 +14,23 @@ module.exports = function(grunt) {
     grunt.log.ok( 'Matecat Version: ' + version );
 
     // Configuration goes here
-	grunt.initConfig({
-		concat: {
-			components: {
-				src: [
-					basePath + 'cat_source/ui.core.js',
-					basePath + 'cat_source/ui.scrollsegment.js',
-					basePath + 'cat_source/ui.classes.js',
-					basePath + 'cat_source/ui.init.js',
-					basePath + 'cat_source/ui.render.js',
-					basePath + 'cat_source/ui.events.js',
-					basePath + 'cat_source/ui.contribution.js',
-					basePath + 'cat_source/ui.tags.js',
-					basePath + 'cat_source/ui.concordance.js',
-					basePath + 'cat_source/ui.glossary.js',
-					basePath + 'cat_source/ui.search.js',
-					basePath + 'cat_source/functions.js',
-					basePath + 'cat_source/ui.customization.js',
+    grunt.initConfig({
+        concat: {
+            components: {
+                src: [
+                    basePath + 'cat_source/ui.core.js',
+                    basePath + 'cat_source/ui.scrollsegment.js',
+                    basePath + 'cat_source/ui.classes.js',
+                    basePath + 'cat_source/ui.init.js',
+                    basePath + 'cat_source/ui.render.js',
+                    basePath + 'cat_source/ui.events.js',
+                    basePath + 'cat_source/ui.contribution.js',
+                    basePath + 'cat_source/ui.tags.js',
+                    basePath + 'cat_source/ui.concordance.js',
+                    basePath + 'cat_source/ui.glossary.js',
+                    basePath + 'cat_source/ui.search.js',
+                    basePath + 'cat_source/functions.js',
+                    basePath + 'cat_source/ui.customization.js',
                     basePath + 'cat_source/ui.review.js',
                     basePath + 'cat_source/ui.offline.js',
                     basePath + 'cat_source/ui.split.js',
@@ -38,60 +38,60 @@ module.exports = function(grunt) {
                     basePath + 'cat_source/mbc.main.js',
                     basePath + 'cat_source/mbc.templates.js',
                     basePath + 'tm.js'
-				],
-				dest: buildPath + 'cat.js'
-			},
-			libraries: {
-				src: [
-					basePath + 'lib/jquery-1.11.0.min.js',
+                ],
+                dest: buildPath + 'cat.js'
+            },
+            libraries: {
+                src: [
+                    basePath + 'lib/jquery-1.11.0.min.js',
                     basePath + 'lib/waypoints.min.js',
                     basePath + 'lib/jquery-ui.js',
                     basePath + 'lib/jquery.hotkeys.min.js',
                     basePath + 'lib/jquery.cookie.js',
                     basePath + 'lib/jquery.tablesorter-fork-mottie.js',
-					basePath + 'lib/diff_match_patch.js',
-					basePath + 'lib/rangy-core.js',
-					basePath + 'lib/rangy-selectionsaverestore.js'
-				],
-				dest: buildPath + 'libs.js'
-			},
-			app: {
-				src: [
-					basePath + 'common.js',
-					buildPath + 'cat.js',
-					basePath + 'logout.js'
-				],
-				dest: buildPath + 'app.js'
-			},
-			styles: {
-				src: [
-					basePath + '../css/common.css',
-					basePath + '../css/style.css',
-					basePath + '../css/mbc-style.css'
-				],
-				dest: basePath + '../css/app.css'
-			}
-		},
-		watch: {
-			scripts: {
+                    basePath + 'lib/diff_match_patch.js',
+                    basePath + 'lib/rangy-core.js',
+                    basePath + 'lib/rangy-selectionsaverestore.js'
+                ],
+                dest: buildPath + 'libs.js'
+            },
+            app: {
+                src: [
+                    basePath + 'common.js',
+                    buildPath + 'cat.js',
+                    basePath + 'logout.js'
+                ],
+                dest: buildPath + 'app.js'
+            },
+            styles: {
+                src: [
+                    basePath + '../css/common.css',
+                    basePath + '../css/style.css',
+                    basePath + '../css/mbc-style.css'
+                ],
+                dest: basePath + '../css/app.css'
+            }
+        },
+        watch: {
+            scripts: {
                 files: [
                     basePath + 'cat_source/*.js',
                     basePath + 'tm.js',
                 ].concat( cssFiles ) ,
 
-				tasks: ['development'],
-				options: {
-					interrupt: true
-				}
-			}
-		},
-		jshint: {
-			options: {
-			  force: true,
-			  smarttabs: true
-			},
-			all: [basePath + 'cat_source/*.js'] // TODO: expand to other js files
-		},
+                tasks: ['development'],
+                options: {
+                    interrupt: true
+                }
+            }
+        },
+        jshint: {
+            options: {
+              force: true,
+              smarttabs: true
+            },
+            all: [basePath + 'cat_source/*.js'] // TODO: expand to other js files
+        },
         strip : {
             app : {
                 src : buildPath + 'app.js',
@@ -111,22 +111,22 @@ module.exports = function(grunt) {
                 src: [ buildPath + 'app.js' ] // files to remove comments from
             },
         },
-		replace: {
-		  version: {
-			src: [buildPath + 'cat.js'],             // source files array (supports minimatch)
-			dest: buildPath + 'cat.js',             // destination directory or file
-			replacements: [{
-			  from: /this\.version \= \"(.*?)\"/gi,      // regex replacement ('Fooo' to 'Mooo')
-			  to: 'this.version = "' + version + '"'
-			}]
-		  }
-		}
-	});
+        replace: {
+          version: {
+            src: [buildPath + 'cat.js'],             // source files array (supports minimatch)
+            dest: buildPath + 'cat.js',             // destination directory or file
+            replacements: [{
+              from: /this\.version \= \"(.*?)\"/gi,      // regex replacement ('Fooo' to 'Mooo')
+              to: 'this.version = "' + version + '"'
+            }]
+          }
+        }
+    });
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-text-replace');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-text-replace');
     grunt.loadNpmTasks('grunt-strip');
     grunt.loadNpmTasks('grunt-stripcomments');
 
