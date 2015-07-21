f02d36d09e29afd3684c374c42320820e46ada1a
Álvaro Reneses <alvaroreneses@gmail.com>
2015-07-21 10:39:47 +0200

    PDO Database tested

--- lib/Model/Database.php
+++ lib/Model/Database.php
@@ -1,26 +1,29 @@
 <?php
 
+/**
+ * Class which implements a database using PDO
+ *
+ * The used test script can be found at: https://gist.github.com/reneses/3108444332d4e56c0b73
+ */
 class Database implements IDatabase {
 
     /**
-     * @var IDatabase   $instance
-     * @var string      $server
-     * @var string      $user
-     * @var string      $password
-     * @var string      $database
-     * @var PDO         $connection
+     * Unique instance of the class (singleton design pattern)
+     * @var Database $instance
      */
-    // Store the single instance of Database
-	private static $instance;
+    private static $instance;
 
-	// Connection variables
-	private $server = ""; //database server
-	private $user = ""; //database login name
-	private $password = ""; //database login password
-	private $database = ""; //database name
+    /**
+     * Established connection
+     * @var PDO $connection
+     */
+    private $connection;
 
-	// Connection
-	private $connection;
+    // Connection variables
+    private $server = ""; //database server
+    private $user = ""; //database login name
+    private $password = ""; //database login password
+    private $database = ""; //database name
 
 
     /**
@@ -30,102 +33,101 @@ class Database implements IDatabase {
      * @param string $password
      * @param string $database
      */
-	private function __construct($server=null, $user=null, $password=null, $database=null) {
+    private function __construct($server=null, $user=null, $password=null, $database=null) {
 
-		// Check that the variables are not empty
-		if ($server == null || $user == null || $database == null) { // TODO: pass?
-			throw new InvalidArgumentException("Database information must be passed in when the object is first created.");
-		}
+        // Check that the variables are not empty
+        if ($server == null || $user == null || $database == null) {
+            throw new InvalidArgumentException("Database information must be passed in when the object is first created.");
+        }
 
-		// Set fields
-		$this->server = $server;
-		$this->user = $user;
-		$this->password = $password;
-		$this->database = $database;
-	}
-
-
-    /**
-     * @Override
-     * {@inheritdoc}
-     */
-	public static function obtain($server=null, $user=null, $password=null, $database=null) {
-		if (!self::$instance) {
-			self::$instance = new PDODatabase($server, $user, $password, $database);
-		}
-		return self::$instance;
-	}
+        // Set fields
+        $this->server = $server;
+        $this->user = $user;
+        $this->password = $password;
+        $this->database = $database;
+    }
 
 
     /**
      * @Override
      * {@inheritdoc}
      */
-	public function connect() {
+    public static function obtain($server=null, $user=null, $password=null, $database=null) {
+        if (!self::$instance  ||  $server != null  &&  $user != null  &&  $password != null  &&  $database != null) {
+            self::$instance = new Database($server, $user, $password, $database);
+        }
+        return self::$instance;
+    }
+
+
+    /**
+     * @Override
+     * {@inheritdoc}
+     */
+    public function connect() {
         $this->connection = new PDO(
-			"mysql:host={$this->server};dbname={$this->database};charset=UTF8",
-			$this->user,
-			$this->password,
+            "mysql:host={$this->server};dbname={$this->database};charset=UTF8",
+            $this->user,
+            $this->password,
             array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION // Raise exceptions on errors
             ));
-	}
+    }
 
 
     /**
      * @Override
      * {@inheritdoc}
      */
-	public function close() {
-		$this->connection = null;
-	}
+    public function close() {
+        $this->connection = null;
+    }
 
 
     /**
      * @Override
      * {@inheritdoc}
      */
-	public function useDb($name){
-		$preparedStatement = $this->connection->prepare("USE :databaseName");
-        $preparedStatement->execute(array(':databaseName'=>$name));
-		$this->database = $name;
-	}
+    public function useDb($name){
+        $this->connection->query("USE ".$name);
+        $this->database = $name;
+    }
 
 
     /**
      * @Override
      * {@inheritdoc}
      */
-	public function begin() {
-		$this->connection->beginTransaction();
-	}
+    public function begin() {
+        $this->connection->beginTransaction();
+    }
 
 
     /**
      * @Override
      * {@inheritdoc}
      */
-	public function commit() {
-		$this->connection->commit();
-	}
+    public function commit() {
+        $this->connection->commit();
+    }
 
 
     /**
      * @Override
      * {@inheritdoc}
      */
-	public function rollback() {
-		$this->connection->rollBack();
-	}
+    public function rollback() {
+        $this->connection->rollBack();
+    }
 
 
     /**
      * @Override
      * {@inheritdoc}
      */
-	public function query($sql) {
-		return $this->connection->query($sql);
-	}
+    public function query($sql) {
+        return $this->connection->query($sql);
+    }
 
 
     /**
