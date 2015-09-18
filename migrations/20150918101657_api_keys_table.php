b13076797b8a590ebafe72086d691e40aff9309e
Fabrizio Regini <freegenie@gmail.com>
2015-09-18 16:30:46 +0200

    adding phinx for easy migrations

---
+++ migrations/20150918101657_api_keys_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class ApiKeysTable extends AbstractMigration {
+    public function up() {
+
+        $table = $this->table('api_keys');
+
+        $table->addColumn('uid', 'biginteger', array('null' => false))
+            ->addColumn('api_key', 'string', array(
+                'limit' => '45', 'null' => false
+            ))
+            ->addColumn('api_secret', 'string', array(
+                'limit' => '45', 'null' => false
+            ))
+            ->addColumn('creation_date', 'datetime', array('null' => false))
+            ->addColumn('last_update', 'datetime', array('null' => false))
+            ->addColumn('enabled', 'boolean', array(
+                'null' => false, 'default' => true
+            ))
+            ->save();
+    }
+
+    public function down() {
+        $this->dropTable('api_keys');
+    }
+}
