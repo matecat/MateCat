08706ba316d51eb40cf75672727df628bb0c1bf1
Giuseppe Silvano <giuseppe.silvano@gmail.com>
2015-10-30 12:44:09 +0100

    Detect converter version from XLIFF and use it to choose the proper converter

--- lib/Controller/downloadFileController.php
+++ lib/Controller/downloadFileController.php
@@ -95,45 +95,25 @@ class downloadFileController extends downloadController {
            5)the temporary file is deleted
          */
 
-        // This array will contain all the files of $files_job split by the
-        // proper converter to use for each one.
-        // At index 0 there will be all the files that need new converters.
-        // At index 1 all the files that need legacy converters
-        $files_job_by_converter_type = array(array(), array());
+        // This array will contain all the files of $files_job split by
+        // converter version.
+        $files_job_by_converter_version = array();
 
-        // Detect the type of converter to use for each file, then store
+        // Detect the converter's version to use for each file, then store
         // file infos accordingly.
         foreach ( $files_job as $file ) {
             $fileType = DetectProprietaryXliff::getInfo($file['xliffFilePath']);
-            if ($fileType[ 'proprietary_short_name' ] == 'matecat_converter') {
-                // Use new converters
-                $converter_type = 0;
-            } else {
-                // Use legacy converters in case of SDLXLIFF and GlobalSight
-                $converter_type = 1;
-            }
-            $files_job_by_converter_type[$converter_type][] = $file;
+            $files_job_by_converter_version[$fileType[ 'converter_version' ]][] = $file;
         }
 
-        // Process files according to the converters needed, one converter
+        // Process files according to the converters' versions, one version
         // at a time
-        foreach ($files_job_by_converter_type as $converter_type => $files_job) {
-            switch ($converter_type) {
-                case 0:
-                    $useLegacyConverters = false;
-                    break;
-                case 1:
-                    $useLegacyConverters = true;
-                    break;
-                default:
-                    throw new Exception("Unexpected converter type: " . $converter_type);
-            }
-
+        foreach ($files_job_by_converter_version as $converter_version => $files_job) {
             //file array is chuncked. Each chunk will be used for a parallel conversion request.
             $files_job = array_chunk( $files_job, self::FILES_CHUNK_SIZE );
             foreach ( $files_job as $chunk ) {
 
-                $converter = new FileFormatConverter($useLegacyConverters);
+                $converter = new FileFormatConverter($converter_version);
 
                 $files_to_be_converted = array();
 
--- lib/Utils/FileFormatConverter.php
+++ lib/Utils/FileFormatConverter.php
@@ -14,7 +14,7 @@ class FileFormatConverter {
     private $lang_handler; //object that exposes language utilities
     private $storage_lookup_map;
 
-    private $useLegacyConverters;
+    private $converterVersion;
 
     private $conversionObject;
 
@@ -36,14 +36,17 @@ class FileFormatConverter {
     /**
      * Create a new FileFormatConverter, using old or new converters.
      * This function looks for converters in the 'converters' table in the db.
-     * If $useLegacyConverters is false, this function will filter only the
-     * row of the table with the column 'conversion_api_version' starting with
-     * 'open '.
-     *
-     * @param bool $useLegacyConverters
+     * $converterVersion can be "legacy", "latest", or something like "1.0.0".
+     * In the first case legacy converters will be used; in the second case,
+     * the latest version of new converters will be used; in the third case,
+     * this function will look for the converters with the provided version,
+     * and if not found will use the converters with higher but closest version.
+     * Version check is done on the conversion_api_version field of the
+     * converters db table; new converters are expected to have a value like
+     * "open 1.0.0".
      */
-    public function __construct( $useLegacyConverters = true ) {
-        $this->useLegacyConverters = $useLegacyConverters;
+    public function __construct($converterVersion = null) {
+        $this->converterVersion = $converterVersion;
 
         if ( !class_exists( "INIT" ) ) {
             include_once( "../../inc/config.inc.php" );
@@ -68,12 +71,36 @@ class FileFormatConverter {
         ), ArrayObject::ARRAY_AS_PROPS );
 
         // Get converters instances list from database,
-        // according to the $useLegacyConverters parameter.
         $db         = Database::obtain();
-        $query = 'SELECT ip_converter, cpu_weight, ip_storage, segmentation_rule'
-                . ' FROM converters'
-                . ' WHERE status_active = 1 AND status_offline = 0'
-                . ' AND conversion_api_version ' . ($this->useLegacyConverters ? 'NOT LIKE' : 'LIKE') . ' "open %"';
+        // The base query to obtain the converters
+        $baseQuery =
+            'SELECT ip_converter, cpu_weight, ip_storage, segmentation_rule'
+            . ' FROM converters'
+            . ' WHERE status_active = 1 AND status_offline = 0';
+
+        // Complete the $baseQuery according to the converter's version
+        if ($this->converterVersion == "legacy") {
+            // Retrieve only old converters
+            $query = $baseQuery . ' AND conversion_api_version NOT LIKE "open %"';
+
+        } else {
+            // Here we use new converters
+
+            if ($this->converterVersion == "latest") {
+                // Get the converters with the latest version
+                $query = $baseQuery . ' AND conversion_api_version = ('
+                    . 'SELECT MAX(conversion_api_version)'
+                    . ' FROM converters'
+                    . ' WHERE conversion_api_version LIKE "open %"'
+                    . ' AND status_active = 1 AND status_offline = 0'
+                    . ')';
+
+            } else {
+                $closest_conversion_api_version = self::getClosestConversionApiVersion($this->converterVersion);
+                $query = $baseQuery . ' AND conversion_api_version = "' . $closest_conversion_api_version . '"';
+            }
+        }
+
         $converters = $db->fetch_array( $query );
 
         // SUUUPER ugly, those variables should not be static at all! No way!
@@ -96,6 +123,49 @@ class FileFormatConverter {
 
         $this->storage_lookup_map = self::$Storage_Lookup_IP_Map;
 
+    }
+
+    /**
+     * Returns the value of conversion_api_version for the provided version.
+     * If no converters in db match the provided version, use the higher closest
+     * converter version.
+     */
+    private static function getClosestConversionApiVersion($version) {
+        $db = Database::obtain();
+
+        // First, obtain a list of all the versions sorted from oldest
+        // to newest
+        $query = 'SELECT DISTINCT conversion_api_version'
+            . ' FROM converters'
+            . ' WHERE conversion_api_version LIKE "open %"'
+            . ' AND status_active = 1 AND status_offline = 0'
+            . ' ORDER BY conversion_api_version ASC';
+        $db_list = $db->fetch_array( $query );
+
+        if (empty($db_list)) {
+            throw new Exception("There are no new converters enabled in the converters table!");
+        }
+
+        // Look for the exact version required, or at least the
+        // higher but closest version we have in db.
+        foreach ($db_list as $db_row) {
+            $examined_version = $db_row['conversion_api_version'];
+            if ($examined_version >= "open $version") {
+                // We found the same version, or we are on the
+                // closest higher one.
+                $candidate_version = $examined_version;
+                break;
+            }
+        }
+
+        if (empty($candidate_version)) {
+            // The previous loop ended, but all versions were
+            // lower than the one provided: as fallback, use the
+            // highest version we have, i.e. the last examined.
+            $candidate_version = $examined_version;
+        }
+
+        return $candidate_version;
     }
 
 
@@ -316,7 +386,7 @@ class FileFormatConverter {
 
         // The new converters need a new way to check if they are
         // online, so we have two different methods for this task.
-        if ($this->useLegacyConverters) {
+        if ($this->converterVersion == "legacy") {
             $isServiceAvailable = $this->checkOpenLegacyService($url);
         } else {
             $isServiceAvailable = $this->checkOpenService();
@@ -374,7 +444,7 @@ class FileFormatConverter {
         $filename    = FilesStorage::pathinfo_fix( $file_path, PATHINFO_FILENAME );
         $extension = FilesStorage::pathinfo_fix($file_path, PATHINFO_EXTENSION);
 
-        if ($this->useLegacyConverters) {
+        if ($this->converterVersion == "legacy") {
             // Legacy converters need encode sanitize
 
             $fileContent = file_get_contents($file_path);
