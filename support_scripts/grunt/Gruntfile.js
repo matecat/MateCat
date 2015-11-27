82c8d99747accf7cb676ed0d8a8aa30ac7b23aed
Fabrizio Regini <freegenie@gmail.com>
2015-11-27 16:00:51 +0100

    Gruntfile.js tabs removed

--- support_scripts/grunt/Gruntfile.js
+++ support_scripts/grunt/Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
     }
 
     // Configuration goes here
-	grunt.initConfig({
+    grunt.initConfig({
         handlebars : {
             options : {
                 namespace : 'MateCat.Templates',
@@ -45,23 +45,23 @@ module.exports = function(grunt) {
                 }
             }
         },
-		concat: {
-			components: {
-				src: [
+        concat: {
+            components: {
+                src: [
                     basePath + 'build/templates.js',
-					basePath + 'cat_source/ui.core.js',
-					basePath + 'cat_source/ui.scrollsegment.js',
-					basePath + 'cat_source/ui.classes.js',
-					basePath + 'cat_source/ui.init.js',
-					basePath + 'cat_source/ui.render.js',
-					basePath + 'cat_source/ui.events.js',
-					basePath + 'cat_source/ui.contribution.js',
-					basePath + 'cat_source/ui.tags.js',
-					basePath + 'cat_source/ui.concordance.js',
-					basePath + 'cat_source/ui.glossary.js',
-					basePath + 'cat_source/ui.search.js',
-					basePath + 'cat_source/functions.js',
-					basePath + 'cat_source/ui.customization.js',
+                    basePath + 'cat_source/ui.core.js',
+                    basePath + 'cat_source/ui.scrollsegment.js',
+                    basePath + 'cat_source/ui.classes.js',
+                    basePath + 'cat_source/ui.init.js',
+                    basePath + 'cat_source/ui.render.js',
+                    basePath + 'cat_source/ui.events.js',
+                    basePath + 'cat_source/ui.contribution.js',
+                    basePath + 'cat_source/ui.tags.js',
+                    basePath + 'cat_source/ui.concordance.js',
+                    basePath + 'cat_source/ui.glossary.js',
+                    basePath + 'cat_source/ui.search.js',
+                    basePath + 'cat_source/functions.js',
+                    basePath + 'cat_source/ui.customization.js',
                     basePath + 'cat_source/ui.review.js',
                     basePath + 'cat_source/ui.offline.js',
                     basePath + 'cat_source/ui.split.js',
@@ -72,45 +72,45 @@ module.exports = function(grunt) {
                     basePath + 'cat_source/segment_notes.*.js',
                     basePath + 'cat_source/review_improved.*.js',
                     basePath + 'tm.js'
-				],
-				dest: buildPath + 'cat.js'
-			},
-			libraries: {
-				src: [
+                ],
+                dest: buildPath + 'cat.js'
+            },
+            libraries: {
+                src: [
                     basePath + 'lib/handlebars.runtime-v4.0.5.js',
-					basePath + 'lib/jquery-1.11.0.min.js',
+                    basePath + 'lib/jquery-1.11.0.min.js',
                     basePath + 'lib/waypoints.min.js',
                     basePath + 'lib/jquery-ui.js',
                     basePath + 'lib/jquery.hotkeys.min.js',
                     basePath + 'lib/jquery.cookie.js',
                     basePath + 'lib/jquery.tablesorter-fork-mottie.js',
-					basePath + 'lib/diff_match_patch.js',
-					basePath + 'lib/rangy-core.js',
-					basePath + 'lib/rangy-selectionsaverestore.js'
-				],
-				dest: buildPath + 'libs.js'
-			},
-			app: {
-				src: [
-					basePath + 'common.js',
-					buildPath + 'cat.js',
-					basePath + 'logout.js'
-				],
-				dest: buildPath + 'app.js'
-			},
-			styles: {
-				src: [
-					basePath + '../css/common.css',
-					basePath + '../css/style.css',
+                    basePath + 'lib/diff_match_patch.js',
+                    basePath + 'lib/rangy-core.js',
+                    basePath + 'lib/rangy-selectionsaverestore.js'
+                ],
+                dest: buildPath + 'libs.js'
+            },
+            app: {
+                src: [
+                    basePath + 'common.js',
+                    buildPath + 'cat.js',
+                    basePath + 'logout.js'
+                ],
+                dest: buildPath + 'app.js'
+            },
+            styles: {
+                src: [
+                    basePath + '../css/common.css',
+                    basePath + '../css/style.css',
                     basePath + '../css/mbc-style.css',
                     basePath + '../css/segment-notes.css',
                     basePath + '../css/project-completion-feature.css'
-				],
-				dest: basePath + '../css/app.css'
-			}
-		},
-		watch: {
-			scripts: {
+                ],
+                dest: basePath + '../css/app.css'
+            }
+        },
+        watch: {
+            scripts: {
                 files: [
                     basePath + 'cat_source/templates/**/*.hbs',
                     basePath + 'cat_source/*.js',
@@ -141,22 +141,22 @@ module.exports = function(grunt) {
                 }
             }
         },
-		replace: {
-		  version: {
-			src: [buildPath + 'cat.js'],             // source files array (supports minimatch)
-			dest: buildPath + 'cat.js',             // destination directory or file
-			replacements: [{
-			  from: /this\.version \= \"(.*?)\"/gi,      // regex replacement ('Fooo' to 'Mooo')
-			  to: 'this.version = "' + version + '"'
-			}]
-		  }
-		}
-	});
+        replace: {
+          version: {
+            src: [buildPath + 'cat.js'],             // source files array (supports minimatch)
+            dest: buildPath + 'cat.js',             // destination directory or file
+            replacements: [{
+              from: /this\.version \= \"(.*?)\"/gi,      // regex replacement ('Fooo' to 'Mooo')
+              to: 'this.version = "' + version + '"'
+            }]
+          }
+        }
+    });
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-text-replace');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-text-replace');
     grunt.loadNpmTasks('grunt-strip');
     grunt.loadNpmTasks('grunt-contrib-handlebars');
 
