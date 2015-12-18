c7a4538445cb2e6838a37d04df03446740efa156
Giuseppe Silvano <giuseppe.silvano@gmail.com>
2015-12-18 11:23:01 +0100

    Use a ViewController to serve the static page

---
+++ lib/Controller/xliffToTargetViewController.php
@@ -0,0 +1,15 @@
+<?php
+
+class xliffToTargetViewController extends viewController {
+
+	public function __construct()
+	{
+		parent::__construct(false);
+		parent::makeTemplate("xliffToTarget.html");
+	}
+
+	function doAction() {}
+
+	function setTemplateVars() {}
+
+}
