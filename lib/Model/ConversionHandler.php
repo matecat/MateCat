d908ee8845800b26406da8b19e44c67bfd8854f1
Roberto Tucci <tucciroberto2000@gmail.com>
2016-01-14 15:23:52 +0100

    Restored old converters for legacy conversion

--- lib/Model/ConversionHandler.php
+++ lib/Model/ConversionHandler.php
@@ -155,6 +155,10 @@ class ConversionHandler {
                 $converterVersion = Constants_ConvertersVersions::LEGACY;
             }
 
+            //TODO: REMOVE SET ENVIRONMENT FOR LEGACY CONVERSION INSTANCES
+            if( INIT::$LEGACY_CONVERSION !== false ){
+                $converterVersion = Constants_ConvertersVersions::LEGACY;
+            }
             $converter = new FileFormatConverter($converterVersion);
 
             if ( strpos( $this->target_lang, ',' ) !== false ) {
