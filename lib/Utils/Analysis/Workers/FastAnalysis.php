cc400978ef7c4c02e769c243be36ca8d6127ff2c
domenico <domenico@translated.net>
2016-02-02 18:44:33 +0100

    Fixed the issue for the Fast analysis logger
    Logs on the wrong file

--- lib/Utils/Analysis/Workers/FastAnalysis.php
+++ lib/Utils/Analysis/Workers/FastAnalysis.php
@@ -69,6 +69,7 @@ class FastAnalysis extends AbstractDaemon {
         parent::__construct();
 
         $this->_configFile = $configFile;
+        Log::resetLogger();
         Log::$fileName = 'fastAnalysis.log';
 
         try {
