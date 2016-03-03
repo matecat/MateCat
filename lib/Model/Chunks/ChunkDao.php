190ac961e51f88f9d9955c02ae7f162a77f3ba98
Roberto Tucci <tucciroberto2000@gmail.com>
2016-03-03 12:28:18 +0100

    Added ordering in chunkDao getByJobID query

--- lib/Model/Chunks/ChunkDao.php
+++ lib/Model/Chunks/ChunkDao.php
@@ -38,7 +38,8 @@ class Chunks_ChunkDao extends DataAccess_AbstractDao {
     public function getByJobID( $id_job ) {
         $conn = $this->con->getConnection();
         $stmt = $conn->prepare("SELECT * FROM " .
-                "jobs WHERE id = ?");
+                "jobs WHERE id = ?
+                order by job_first_segment asc");
 
         $stmt->execute(array( $id_job ));
         $stmt->setFetchMode(PDO::FETCH_CLASS, 'Chunks_ChunkStruct');
