37a59e3fe4566dad1b73614c66a2df5d4815d433
FrancescoE <francesco@translated.net>
2016-04-01 19:56:50 +0200

    Fixed bug scroll bar of the filename

--- public/js/cat_source/ui.core.js
+++ public/js/cat_source/ui.core.js
@@ -526,7 +526,7 @@ UI = {
 		var menu = '<nav id="jobMenu" class="topMenu">' +
 				'    <ul>';
 		$.each(config.firstSegmentOfFiles, function() {
-			menu += '<li data-file="' + this.id_file + '" data-segment="' + this.first_segment + '"><span class="' + UI.getIconClass(this.file_name.split('.')[this.file_name.split('.').length -1]) + '"></span><a href="#" title="' + this.file_name + '" >' + this.file_name + '</a></li>';
+			menu += '<li data-file="' + this.id_file + '" data-segment="' + this.first_segment + '"><span class="' + UI.getIconClass(this.file_name.split('.')[this.file_name.split('.').length -1]) + '"></span><a href="#" title="' + this.file_name + '" >' + this.file_name.substring(0,20).concat("[...]" ).concat((this.file_name).substring(this.file_name.length-20))  + '</a></li>';
 		});
 
 		menu += '    </ul>' +
