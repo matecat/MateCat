16d2dd08865279aa3cd32d63b16076a6ecf9d0b4
Edgar Eler <eler@edgar.systems>
2016-04-11 10:06:54 -0300

    Fix migration for move original remote files

--- migrations/20160408162926_drop_remote_id_from_files.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class DropRemoteIdFromFiles extends AbstractMatecatMigration {
-    public $sql_up =  "ALTER TABLE `files` DROP COLUMN `remote_id`;";
-
-    public $sql_down = "ALTER TABLE `files` ADD COLUMN `remote_id` text;";
-}
