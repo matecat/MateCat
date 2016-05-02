b09f81fb2bce65ed0855ce3bf98a60740514f9e5
Fabrizio Regini <freegenie@gmail.com>
2016-05-02 13:16:58 +0200

    Introduce PHPTALWithAppend for custom js in plugins

---
+++ inc/PHPTAL/PHPTALWithAppend.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: fregini
+ * Date: 02/05/16
+ * Time: 11:09
+ */
+class PHPTALWithAppend extends PHPTAL {
+
+    protected $internal_store = array();
+
+    /**
+     *
+     * This method popolates an array of arrays that can be used
+     * to push values on the template so that plugins can append
+     * their own javascripts or assets.
+     *
+     * @param $name
+     * @param $value
+     */
+    public function append($name, $value) {
+        if (! array_key_exists($name, $this->internal_store) ) {
+            $this->internal_store[ $name ] = array() ;
+        }
+
+        array_push( $this->internal_store[ $name ], $value );
+
+        $this->$name = $this->internal_store[ $name ] ;
+    }
+}
