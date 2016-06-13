821ffc96722a8ef6afafee56f496a29c942524d9
Fabrizio Regini <freegenie@gmail.com>
2016-06-13 21:12:04 +0200

    validate boolean input in ChunkOptionsController

--- lib/Controller/API/V2/ChunkOptionsController.php
+++ lib/Controller/API/V2/ChunkOptionsController.php
@@ -15,7 +15,7 @@ class ChunkOptionsController extends ProtectedKleinController {
     public function update() {
         $chunk_options_model = new \ChunkOptionsModel( $this->validator->getChunk() ) ; 
         
-        $chunk_options_model->setOptions( $this->request->params() ) ; 
+        $chunk_options_model->setOptions( $this->filteredParams() ) ;
         $chunk_options_model->save(); 
         
         $this->response->json( array( 'options' => $chunk_options_model->toArray() ) ) ;
@@ -27,5 +27,15 @@ class ChunkOptionsController extends ProtectedKleinController {
 
     protected function validateRequest() {
         $this->validator->validate();
+    }
+
+    protected function filteredParams() {
+        $args = array(
+            'speech2text' => array( 'filter' => FILTER_VALIDATE_BOOLEAN ),
+            'lexiqa' => array( 'filter' => FILTER_VALIDATE_BOOLEAN ),
+            'tag_projection' => array( 'filter' => FILTER_VALIDATE_BOOLEAN ),
+        );
+
+        return filter_var_array( $this->request->params(), $args);
     }
 }
