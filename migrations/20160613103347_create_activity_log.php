af53b25fc4bce0425a812e2d6dd336fa9e1a964c
Domenico Lupinetti <ostico@gmail.com>
2016-06-23 19:02:30 +0200

    Improved table creation by adding partition by Year

--- migrations/20160613103347_create_activity_log.php
+++ migrations/20160613103347_create_activity_log.php
@@ -1,7 +1,5 @@
 <?php
 
-use Phinx\Migration\AbstractMigration;
-
 class CreateActivityLog extends AbstractMatecatMigration
 {
 
@@ -14,13 +12,24 @@ CREATE TABLE `activity_log` (
   `ip` varchar(45) NOT NULL,
   `uid` int(10) unsigned DEFAULT NULL,
   `event_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`ID`),
+  PRIMARY KEY (`ID`,`event_date`),
   KEY `ip_idx` (`ip`) USING BTREE,
   KEY `id_job_idx` (`id_job`) USING BTREE,
   KEY `id_project_idx` (`id_project`) USING BTREE,
   KEY `uid_idx` (`uid`) USING BTREE,
   KEY `event_date_idx` (`event_date`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
+) 
+
+ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
+
+PARTITION BY RANGE( YEAR(event_date) ) (
+    PARTITION p2016 VALUES LESS THAN (2017),
+    PARTITION p2017 VALUES LESS THAN (2018),
+    PARTITION p2018 VALUES LESS THAN (2019),
+    PARTITION p2019 VALUES LESS THAN (2020),
+    PARTITION p9999 VALUES LESS THAN MAXVALUE
+)
+;
 EOF;
 
     public $sql_down = 'DROP TABLE `activity_log`';
