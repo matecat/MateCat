3b99ae7861003d7622cacf0eac0618d8427d10bd
Edgar Eler <eler@edgar.systems>
2016-06-27 17:09:43 -0300

    Remove key MEMCACHE_SERVERS

--- inc/INIT.php
+++ inc/INIT.php
@@ -14,7 +14,6 @@ class INIT {
     public static $DB_DATABASE;
     public static $DB_USER;
     public static $DB_PASS;
-    public static $MEMCACHE_SERVERS    = array();
     public static $REDIS_SERVERS       = array();
     public static $QUEUE_BROKER_ADDRESS;
     public static $QUEUE_DQF_ADDRESS;
