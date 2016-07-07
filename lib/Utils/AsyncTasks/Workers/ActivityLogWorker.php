5d46c794a13d17eda74a54f08dcc9b328062bed9
Domenico Lupinetti <ostico@gmail.com>
2016-07-07 12:01:51 +0200

    Fixed Activity Log database issue ( MySQL has gone away )

--- lib/Utils/AsyncTasks/Workers/ActivityLogWorker.php
+++ lib/Utils/AsyncTasks/Workers/ActivityLogWorker.php
@@ -3,6 +3,8 @@ namespace AsyncTasks\Workers;
 
 use ActivityLog\ActivityLogDao;
 use ActivityLog\ActivityLogStruct;
+use Database;
+use PDOException;
 use TaskRunner\Commons\AbstractElement,
         TaskRunner\Commons\AbstractWorker,
         TaskRunner\Commons\QueueElement,
@@ -24,6 +26,9 @@ class ActivityLogWorker extends AbstractWorker {
         $this->_checkForReQueueEnd( $queueElement );
 
         $logEvent = new ActivityLogStruct( $queueElement->params->toArray() );
+
+        //re inizialize DB if socked is closed
+        $this->_checkDatabaseConnection();
 
         $this->_writeLog( $logEvent );
 
@@ -59,6 +64,32 @@ class ActivityLogWorker extends AbstractWorker {
 
         $logActivityDao = new ActivityLogDao();
         $logActivityDao->create( $logEvent );
+
+    }
+
+    /**
+     * Check the connection.
+     * MySql timeout close the socket and throws Exception in the nex read/write access
+     *
+     * <code>
+     * By default, the server closes the connection after eight hours if nothing has happened.
+     * You can change the time limit by setting thewait_timeout variable when you start mysqld.
+     * @see http://dev.mysql.com/doc/refman/5.0/en/gone-away.html
+     * </code>
+     *
+     */
+    protected function _checkDatabaseConnection(){
+
+        $db = Database::obtain();
+        try {
+            $db->ping();
+        } catch ( PDOException $e ) {
+            $this->_doLog( "--- (Worker " . $this->_workerPid . ") : {$e->getMessage()} " );
+            $this->_doLog( "--- (Worker " . $this->_workerPid . ") : Database connection reloaded. " );
+            $db->close();
+            //reconnect
+            $db->getConnection();
+        }
 
     }
 
