12c7bd1e95d2125c9bde03663f131d1ddcab9d43
riccio82 <riccio82@gmail.com>
2017-01-05 18:17:56 +0100

    Fixed bug pressing enter on search input

--- public/js/cat_source/es6/react/components/SearchInput.js
+++ public/js/cat_source/es6/react/components/SearchInput.js
@@ -18,20 +18,20 @@ class SearchInput extends React.Component {
     render () {
 
         return (
-                <form className="col l4 offset-l4 s4 offset-s4">
+                <div className="col l4 offset-l4 s4 offset-s4">
 
                     <div className="row">
                         <div className="input-field">
-                            <i className="icon-search prefix"></i>
+                            <i className="icon-search prefix"/>
                             <input id="icon_prefix" type="text" className="valid"
                                    placeholder="Search by project name"
                                    ref={(input) => this.textInput = input}
                                    onChange={this.filterByName.bind(this)}/>
                             <i className="prefix close-x"
-                            onClick={this.closeSearch.bind(this)}></i>
+                            onClick={this.closeSearch.bind(this)}/>
                         </div>
                     </div>
-                </form>
+                </div>
 
 
         );
