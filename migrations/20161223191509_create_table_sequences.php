022f20ffe9ba3a64210910968a9649e07c89e0ea
domenico <domenico@translated.net>
2017-01-09 15:43:21 +0100

    Changed integer type in mysql sequences table

--- migrations/20161223191509_create_table_sequences.php
+++ migrations/20161223191509_create_table_sequences.php
@@ -4,7 +4,7 @@ class CreateTableSequences extends AbstractMatecatMigration
 {
 
     public $sql_up = array(
-            " CREATE TABLE `sequences` ( `id_segment` INT UNSIGNED NOT NULL ) ENGINE = InnoDB ",
+            " CREATE TABLE `sequences` ( `id_segment` BIGINT UNSIGNED NOT NULL ) ENGINE = InnoDB ",
             " LOCK TABLES `sequences` WRITE, `segments` READ ",
             " INSERT INTO `sequences` ( id_segment ) VALUES ( ( SELECT MAX( id ) + 1 FROM segments ) ) ",
             " UNLOCK TABLES ",
