fd1283d4ba3c504289e3b7f7e430a41a53266303
Fabrizio Regini <freegenie@gmail.com>
2017-01-18 16:38:09 +0100

    fix query to return unresolved thread count

--- lib/Utils/manage.class.php
+++ lib/Utils/manage.class.php
@@ -36,6 +36,9 @@ class ManageUtils {
         //get job data using job IDs
         $jobData = getJobsFromProjects( $projectIDs, $search_source, $search_target, $search_status, $search_onlycompleted );
 
+        $dao = new Comments_CommentDao() ;
+        $openThreads = $dao->getOpenThreadsForProjects( $projectIDs ) ;
+
         $lang_handler = Langs_Languages::getInstance();
 
         //Prepare job data
@@ -61,7 +64,13 @@ class ManageUtils {
             $job[ 'mt_engine_name' ]        = $job_array[ 'name' ];
             $job[ 'id_tms' ]                = $job_array[ 'id_tms' ];
 
-            $job[ 'open_threads_count' ]    = $job_array[ 'open_threads_count' ];
+
+            $job[ 'open_threads_count' ] = 0 ;
+            foreach( $openThreads as $openThread ) {
+                if ( $openThread[ 'id_job' ] == $job[ 'id' ] && $openThread[ 'password' ] == $job[ 'password' ] ) {
+                    $job[ 'open_threads_count' ] = (int) $openThread[ 'count' ] ;
+                }
+            }
 
             //generate and set job stats
             $jobStats = new WordCount_Struct();
