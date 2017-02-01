6c1cb1f0cf9e871de70aa27f60fc5ff7f5030677
Fabrizio Regini <freegenie@gmail.com>
2017-02-01 17:07:02 +0100

    remove automatic schema dump from migrations

--- lib/Utils/AbstractMatecatMigration.php
+++ lib/Utils/AbstractMatecatMigration.php
@@ -1,12 +1,19 @@
 <?php
 
+/**
+ * Class AbstractMatecatMigration
+ *
+ * @deprecated this class is deprecated because there's no longer need to
+ * regenerate the SQL schema at each migration. Extend \Phinx\Migration\AbstractMigration
+ * and up and down methods.
+ *
+ */
 class AbstractMatecatMigration extends \Phinx\Migration\AbstractMigration {
 
     public $sql_up;
     public $sql_down;
 
     public function up() {
-
         if ( !is_array( $this->sql_up ) ) {
             $this->sql_up = array( $this->sql_up ) ;
         }
@@ -15,8 +22,6 @@ class AbstractMatecatMigration extends \Phinx\Migration\AbstractMigration {
             Log::doLog( $sql . "\n" );
             $this->execute($sql);
         }
-
-        $this->resetSchemaDump('up');
     }
 
     public function down() {
@@ -28,48 +33,5 @@ class AbstractMatecatMigration extends \Phinx\Migration\AbstractMigration {
         foreach( $this->sql_down as $sql ) {
             $this->execute($sql);
         }
-
-        $this->resetSchemaDump('down');
     }
-
-    private function resetSchemaDump($direction) {
-
-        $config_ini = parse_ini_file(INIT::$ROOT . '/inc/config.ini', true);
-        $current_env = $config_ini['ENV'];
-        $database = new SchemaCopy($config_ini[$current_env]);
-        $statements = $database->getTablesStatements();
-
-        $sql = $database->getDatabaseCreationStatement();
-        foreach($statements as $key => $statement ) {
-            $sql .= preg_replace('/AUTO_INCREMENT=\d+/i', '', $statement[0]['Create Table']);
-            $sql .= "; \n\n";
-        }
-
-        $seedLoader = new SeedLoader( $config_ini[ $current_env ]);
-        $sql .= $seedLoader->getSeedSql();
-
-
-        $pdo = Database::obtain()->getConnection();
-
-        $phinx_records = $pdo->query("select * from phinxlog");
-
-        foreach($phinx_records as $record) {
-            if ( $direction == 'down' && $record['version'] == $this->version ) {
-                continue;
-            }
-
-            $sql .= "\nINSERT INTO `phinxlog` ( version ) VALUES ( '${record['version']}' );";
-        }
-
-        if ( $direction == 'up' ) {
-            $sql .= "\nINSERT INTO `phinxlog` ( version ) VALUES ( '$this->version' );";
-        }
-
-        $sql .= "\n";
-        
-        $sql .= $seedLoader->getConversionLogSchema();
-
-        file_put_contents( INIT::$ROOT . '/lib/Model/matecat.sql', $sql );
-    }
-
 }
