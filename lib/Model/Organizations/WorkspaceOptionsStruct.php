c535d03537b992de62cba213f1b37d8feef64544
domenico <domenico@translated.net>
2017-02-09 20:41:29 +0100

    API v2 - Workspace creation

---
+++ lib/Model/Organizations/WorkspaceOptionsStruct.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author domenico domenico@translated.net / ostico@gmail.com
+ * Date: 08/02/17
+ * Time: 18.37
+ *
+ */
+
+namespace Organizations;
+
+use DataAccess_AbstractDaoSilentStruct;
+use DataAccess_IDaoStruct;
+
+/**
+ * Class WorkspaceOptionsStruct
+ * @package Organizations
+ */
+class WorkspaceOptionsStruct extends DataAccess_AbstractDaoSilentStruct implements DataAccess_IDaoStruct {
+
+
+}
