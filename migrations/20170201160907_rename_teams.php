f0906842b64004052df051baea39c038ca7306a1
Fabrizio Regini <freegenie@gmail.com>
2017-02-17 12:16:09 +0100

    remove deprecation for AbstractMatecatMigration

--- migrations/20170201160907_rename_teams.php
+++ migrations/20170201160907_rename_teams.php
@@ -2,38 +2,29 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class RenameTeams extends AbstractMigration
+class RenameTeams extends AbstractMatecatMigration
 {
 
-    public function up() {
+    public $sql_up = array(
+        " ALTER TABLE teams RENAME TO organizations",
+        " ALTER TABLE teams_users CHANGE id_team id_organization int(11)"  ,
+        " ALTER TABLE teams_users RENAME INDEX id_team_uid TO id_organization_uid ",
+        " ALTER TABLE teams_users RENAME TO organizations_users " ,
+        " ALTER TABLE owner_features CHANGE id_team id_organization int(11) ",
+        " ALTER TABLE owner_features RENAME INDEX id_team_feature TO id_organization_feature ",
+        " ALTER TABLE projects CHANGE id_team id_organization int(11)" ,
+    );
 
-        $this->execute(" ALTER TABLE teams RENAME TO organizations" ) ;
-        $this->execute(" ALTER TABLE teams_users CHANGE id_team id_organization int(11)" ) ;
+    public $sql_down = array(
+        " ALTER TABLE organizations RENAME TO teams" ,
+        " ALTER TABLE organizations_users CHANGE id_organization id_team int(11)" ,
+        " ALTER TABLE organizations_users RENAME INDEX id_organization_uid TO id_team_uid ",
+        " ALTER TABLE organizations_users RENAME TO teams_users " ,
+        " ALTER TABLE owner_features CHANGE id_organization id_team int(11) ",
+        " ALTER TABLE owner_features RENAME INDEX id_organization_feature TO id_team_feature ",
+        " ALTER TABLE projects CHANGE id_organization id_team int(11)"
+    );
 
-        $this->execute(" ALTER TABLE teams_users RENAME INDEX id_team_uid TO id_organization_uid " ) ;
-        $this->execute(" ALTER TABLE teams_users RENAME TO organizations_users " ) ;
-
-        $this->execute(" ALTER TABLE owner_features CHANGE id_team id_organization int(11) " ) ;
-        $this->execute(" ALTER TABLE owner_features RENAME INDEX id_team_feature TO id_organization_feature " ) ;
-
-        $this->execute(" ALTER TABLE projects CHANGE id_team id_organization int(11)" );
-
-    }
-
-    public function down() {
-
-        $this->execute(" ALTER TABLE organizations RENAME TO teams" ) ;
-        $this->execute(" ALTER TABLE organizations_users CHANGE id_organization id_team int(11)" ) ;
-
-        $this->execute(" ALTER TABLE organizations_users RENAME INDEX id_organization_uid TO id_team_uid " ) ;
-        $this->execute(" ALTER TABLE organizations_users RENAME TO teams_users " ) ;
-
-        $this->execute(" ALTER TABLE owner_features CHANGE id_organization id_team int(11) " ) ;
-        $this->execute(" ALTER TABLE owner_features RENAME INDEX id_organization_feature TO id_team_feature " ) ;
-
-        $this->execute(" ALTER TABLE projects CHANGE id_organization id_team int(11)" );
-
-    }
 }
 
 
--- migrations/20170202125007_prepare_organizations.php
+++ migrations/20170202125007_prepare_organizations.php
@@ -2,13 +2,12 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class PrepareOrganizations extends AbstractMigration {
+class PrepareOrganizations extends AbstractMatecatMigration  {
 
-    public function up() {
-
-        $this->execute( " ALTER TABLE `organizations` ADD COLUMN `type` VARCHAR(45) NOT NULL DEFAULT 'personal' AFTER `created_at` " );
-        $this->execute( " ALTER TABLE `organizations_users` ADD INDEX `uid` (`uid` ASC) " );
-        $this->execute( "
+    public $sql_up = array(
+        " ALTER TABLE `organizations` ADD COLUMN `type` VARCHAR(45) NOT NULL DEFAULT 'personal' AFTER `created_at` " ,
+        " ALTER TABLE `organizations_users` ADD INDEX `uid` (`uid` ASC) " ,
+        "
             CREATE TABLE `workspaces` (
               `id` INT NOT NULL AUTO_INCREMENT,
               `name` VARCHAR(255) NOT NULL,
@@ -16,32 +15,27 @@ class PrepareOrganizations extends AbstractMigration {
               `options` VARCHAR(10240) NULL DEFAULT '{}',
               PRIMARY KEY (`id`) )
             ENGINE = InnoDB
-        " );
-        $this->execute( "
-            ALTER TABLE `projects`
+        ",
+        " ALTER TABLE `projects`
             ADD COLUMN `id_assignee` INT UNSIGNED NULL DEFAULT NULL,
             ADD COLUMN `id_workspace` INT UNSIGNED NULL DEFAULT NULL,
             ADD INDEX `id_assignee_idx` (`id_assignee` ASC),
             ADD INDEX `id_workspace_idx` (`id_workspace` ASC),
             algorithm=INPLACE, lock=NONE
-        " );
+        "
+    );
 
-    }
-
-    public function down() {
-
-        $this->execute( " ALTER TABLE `organizations` DROP COLUMN `type` " );
-        $this->execute( " ALTER TABLE `organizations_users` DROP INDEX `uid` " );
-        $this->execute( " DROP TABLE `workspaces` " );
-        $this->execute( " 
-              ALTER TABLE `projects` 
+    public $sql_down = array(
+        " ALTER TABLE `organizations` DROP COLUMN `type` ",
+        " ALTER TABLE `organizations_users` DROP INDEX `uid` ",
+        " DROP TABLE `workspaces` ",
+        " ALTER TABLE `projects`
               DROP COLUMN `id_assignee`,
               DROP INDEX `id_assignee_idx`,
               DROP COLUMN `id_workspace`,
               DROP INDEX `id_workspace_idx`
-        " );
+        ");
 
-    }
 }
 
 
