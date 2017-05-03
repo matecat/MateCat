9bb62f234df8cb99ca65621a111c7510a4015128
domenico <domenico@translated.net>
2017-05-03 17:13:00 +0200

    Updated last migration

--- migrations/20170428150013_alter_confirmation_outsources_to_timestamps.php
+++ migrations/20170428150013_alter_confirmation_outsources_to_timestamps.php
@@ -3,16 +3,18 @@
 class AlterConfirmationOutsourcesToTimestamps extends AbstractMatecatMigration
 {
 
-    public $sql_up = "
-        ALTER TABLE `outsource_confirmation` 
-        CHANGE COLUMN `create_date` `create_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-        CHANGE COLUMN `delivery_date` `delivery_date` TIMESTAMP NOT NULL ;
-    ";
+    public $sql_up = [
+            "ALTER TABLE `outsource_confirmation` 
+                CHANGE COLUMN `create_date` `create_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+                CHANGE COLUMN `delivery_date` `delivery_date` TIMESTAMP NOT NULL ;",
+            "ALTER TABLE `jobs_translators` CHANGE COLUMN `delivery_date` `delivery_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ;"
+    ];
 
-    public $sql_down = "
-            ALTER TABLE `outsource_confirmation` 
-            CHANGE COLUMN `create_date` `create_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-            CHANGE COLUMN `delivery_date` `delivery_date` DATETIME NOT NULL ;
-        ";
+    public $sql_down = [
+            "ALTER TABLE `outsource_confirmation` 
+                CHANGE COLUMN `create_date` `create_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+                CHANGE COLUMN `delivery_date` `delivery_date` DATETIME NOT NULL ;",
+            "ALTER TABLE `jobs_translators` CHANGE COLUMN `delivery_date` `delivery_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ;"
+    ];
 
 }
