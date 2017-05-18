e9d93b056b2a97e22016a06d0bf9ca8c37d9e5af
Fabrizio Regini <freegenie@gmail.com>
2017-05-18 18:18:07 +0200

    fix bug of missing uid on chunk_completion_updates

--- lib/Plugins/Features/ProjectCompletion.php
+++ lib/Plugins/Features/ProjectCompletion.php
@@ -17,11 +17,15 @@ class ProjectCompletion extends BaseFeature {
         // from a review page or a translate page.
 
         $chunk = $params['chunk'];
-        $chunk_completion_update_struct = new Chunks_ChunkCompletionUpdateStruct( $chunk->attributes() );
+        $chunk_completion_update_struct            = new Chunks_ChunkCompletionUpdateStruct( $chunk->attributes() );
         $chunk_completion_update_struct->is_review = $params['is_review'];
-        $chunk_completion_update_struct->source = 'user' ;
-        $chunk_completion_update_struct->uid = $params['uid'];
-        $chunk_completion_update_struct->id_job = $chunk->id ;
+        $chunk_completion_update_struct->source    = 'user' ;
+        $chunk_completion_update_struct->id_job    = $chunk->id ;
+
+        if ( isset( $params['logged_user'] ) && $params['logged_user']->uid ) {
+            $chunk_completion_update_struct->uid = $params['logged_user']->uid ;
+        }
+
         $chunk_completion_update_struct->setTimestamp('last_translation_at', strtotime('now'));
 
         $dao = new Chunks_ChunkCompletionEventDao();
