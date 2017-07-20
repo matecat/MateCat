ca18082c92a92653067f7ee9da52cf33416c14d9
Ruben Santillan <rubensantillan83@gmail.com>
2017-07-20 14:51:50 +0200

    New Analysis: improvements

--- public/js/cat_source/es6/react/components/analyze/ChunkAnalyzeHeader.js
+++ public/js/cat_source/es6/react/components/analyze/ChunkAnalyzeHeader.js
@@ -57,10 +57,10 @@ class ChunkAnalyzeHeader extends React.Component {
     render() {
         let total = this.props.total;
         this.checkWhatChanged();
-        let id = (this.props.chunksSize > 1) ? this.props.jobInfo.jid + '-' +this.props.index : this.props.jobInfo.jid;
+        let id = (this.props.chunksSize > 1) ? 'Part ' +this.props.index : this.props.jobInfo.jid;
         return <div className="chunk sixteen wide column shadow-1 pad-right-10">
                 <div className="left-box">
-                    <div className="job-id">({id})</div>
+                    <div className="job-id">{id}</div>
                     <div className="file-details" onClick={this.props.showFiles}>
                         File <span className="details">details </span>
                     </div>
