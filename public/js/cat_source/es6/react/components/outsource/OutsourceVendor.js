2dc26c5848349d0571be9035558751bde5c87c03
Ruben Santillan <rubensantillan83@gmail.com>
2017-08-03 10:46:32 +0200

    New Outsource: structure for compact view are added

--- public/js/cat_source/es6/react/components/outsource/OutsourceVendor.js
+++ public/js/cat_source/es6/react/components/outsource/OutsourceVendor.js
@@ -296,7 +296,8 @@ class OutsourceVendor extends React.Component {
                             <span>at</span>
                             <div className="delivery-time">{delivery.time}</div>
                             <div className="gmt">
-                                <GMTSelect changeValue={this.changeTimezone.bind(this)}/>
+                                {/*<GMTSelect changeValue={this.changeTimezone.bind(this)}/>*/}
+                                <div className="gmt-outsourced"> GMT +2 </div>
                             </div>
                             {/*<div className="need-it-faster">
                                 <a className="faster"
@@ -304,7 +305,7 @@ class OutsourceVendor extends React.Component {
                                 >Need it faster?</a>
                             </div>*/}
                         </div>
-                        <div className="confirm-delivery-input">
+                        {/*<div className="confirm-delivery-input">
                             <div className="back">
                                 <a href="#"><i className="icon-chevron-left icon"></i>Back</a>
                             </div>
@@ -313,12 +314,12 @@ class OutsourceVendor extends React.Component {
                                 <input type="text" placeholder="ruben.santillan@translated.net" value="ruben.santillan@translated.net" />
                             </div>
 
-                        </div>
-                        {/*<div className="confirm-delivery-box">
+                        </div>*/}
+                        <div className="confirm-delivery-box">
                             <div className="confirm-title">Order sent correctly</div>
                             <p>Thank you for choosing our Outsource service<br />
                                 You will soon be contacted by a Account Manager to send you an invoice</p>
-                        </div>*/}
+                        </div>
                     </div>
                     <div className="order-box-outsource">
                         <div className="order-box">
@@ -357,8 +358,9 @@ class OutsourceVendor extends React.Component {
                             </div>
                         </div>
                         <div className="order-button-outsource">
-                            {/*<a className="open-order ui green button" onClick={this.sendOutsource.bind(this)}>Order now</a>*/}
-                            <a className="open-order ui green button">Confirm</a>
+                            {/*<button className="open-order ui green button" onClick={this.sendOutsource.bind(this)}>Order now</button>*/}
+                            {/*<button className="confirm-order ui green button">Confirm</button>*/}
+                            <button className="open-outsourced ui button ">View status</button>
                         </div>
                     </div>
                 </div>
@@ -383,35 +385,52 @@ class OutsourceVendor extends React.Component {
         let pricePWord = this.getPricePW(price);
         return <div className="outsource-to-vendor-reduced sixteen wide column">
             {this.state.outsource ? (
-                <div className="container-reduced">
-                    <div className="title-reduced">Let us do it for you</div>
+                <div className="reduced-boxes">
+                    <div className="container-reduced">
+                        <div className="title-reduced">Let us do it for you</div>
 
-                    <div className="payment-service">
-                        <div className="service-box">
-                            <div className="service project-management">Outsource: PM </div>
-                            <div className="service translation"> + Translation </div>
-                            <div className="service revision"> + Revision</div>
-                        </div>
-                        <div className="fiducial-logo">
-                            <div className="translated-logo">Guaranteed by
-                                <img className="logo-t" src="/public/img/logo_translated.png" />
+                        <div className="payment-service">
+                            <div className="service-box">
+                                <div className="service project-management">Outsource: PM </div>
+                                <div className="service translation"> + Translation </div>
+                                <div className="service revision"> + Revision</div>
+                            </div>
+                            <div className="fiducial-logo">
+                                <div className="translated-logo">Guaranteed by
+                                    <img className="logo-t" src="/public/img/logo_translated.png" />
+                                </div>
+                            </div>
+                            <div className="view-more">
+                                <a className="open-view-more"
+                                   onClick={this.viewMoreClick.bind(this)}>+ view more</a>
                             </div>
                         </div>
-                        <div className="view-more">
-                            <a className="open-view-more"
-                               onClick={this.viewMoreClick.bind(this)}>+ view more</a>
-                        </div>
-                    </div>
-                    <div className="delivery-order">
-                        <div className="delivery-box">
-                            <label>Delivery date:</label><br />
-                            <div className="delivery-date">{delivery.day + ' ' + delivery.month}</div>
-                            <span>at</span>
-                            <div className="delivery-time">{delivery.time}</div>
-                            <div className="gmt">
-                                <GMTSelect direction="up" changeValue={this.changeTimezone.bind(this)}/>
+                        <div className="delivery-order">
+                            <div className="delivery-box">
+                                <label>Delivery date:</label>{/*<br />*/}
+                                <div className="delivery-date">{delivery.day + ' ' + delivery.month}</div>
+                                <span>at</span>
+                                <div className="delivery-time">{delivery.time}</div>
+                                <div className="gmt">
+                                    <GMTSelect direction="up" changeValue={this.changeTimezone.bind(this)}/>
+                                </div>
                             </div>
                         </div>
+                        <div className="confirm-delivery-input">
+                            <div className="back">
+                                <a href="#"><i className="icon-chevron-left icon"></i>Back</a>
+                            </div>
+                            <div className="email-confirm">Great, an Account Manager will contact you to send you the invoice as a customer to this email</div>
+                            <div className="ui input">
+                                <input type="text" placeholder="ruben.santillan@translated.net" />
+                            </div>
+
+                        </div>
+                            {/*<div className="confirm-delivery-box">
+                             <div className="confirm-title">Order sent correctly</div>
+                             <p>Thank you for choosing our Outsource service<br />
+                             You will soon be contacted by a Account Manager to send you an invoice</p>
+                             </div>*/}
                     </div>
                     <div className="order-box-outsource">
                         <div className="order-box">
@@ -449,8 +468,10 @@ class OutsourceVendor extends React.Component {
                             </div>
                         </div>
                         <div className="order-button-outsource">
-                            <a className="open-order ui green button"
-                               onClick={this.sendOutsource.bind(this)}>Order now</a>
+                            {/*<button className="open-order ui green button"
+                               onClick={this.sendOutsource.bind(this)}>Order now</button>*/}
+                            <button className="confirm-order ui green button">Confirm</button>
+                            {/*<button className="open-outsourced ui button ">View status</button>*/}
                         </div>
                     </div>
                 </div>
