cd367cf2eb6dea08d2f5c7a2597cb3efffac5f30
riccio82 <riccio82@gmail.com>
2017-08-07 17:08:03 +0200

    Outsource: slider customers

--- public/js/cat_source/es6/react/components/outsource/OutsourceInfo.js
+++ public/js/cat_source/es6/react/components/outsource/OutsourceInfo.js
@@ -5,6 +5,9 @@ class OutsourceInfo extends React.Component {
 
     constructor(props) {
         super(props);
+        this.sliderIndex = 0;
+        this.startSlider = this.startSlider.bind(this);
+        this.timeoutSlider;
     }
 
     allowHTML(string) {
@@ -15,7 +18,31 @@ class OutsourceInfo extends React.Component {
         $(document).trigger('openChat');
     }
 
-    componentDidMount () {}
+    startSlider() {
+        var i;
+        var x = this.slider.getElementsByClassName("customer-box-info");
+        clearTimeout(this.timeoutSlider);
+        var pointer = this.slider.getElementsByClassName("pointer");
+        for (i = 0; i < x.length; i++) {
+            x[i].classList.remove("fade-in");
+            pointer[i].classList.remove("active");
+        }
+        this.sliderIndex++;
+        if (this.sliderIndex > x.length) {this.sliderIndex = 1}
+        x[this.sliderIndex-1].classList.add("fade-in");
+        pointer[this.sliderIndex-1].classList.add("active");
+        this.timeoutSlider = setTimeout(this.startSlider, 6000);
+
+    }
+
+    slideItem(n) {
+        this.sliderIndex = n-1;
+        this.startSlider()
+    }
+
+    componentDidMount () {
+        this.startSlider()
+    }
 
     componentWillUnmount() {}
 
@@ -24,22 +51,22 @@ class OutsourceInfo extends React.Component {
     render() {
         return <div className="customer-request sixteen wide column">
             <div className="ui grid">
-                <div className="customer-box nine wide column">
+                <div className="customer-box nine wide column" ref={(slider)=> this.slider = slider}>
                     <div className="title-pointer">
                         <h3>Our customer said</h3>
                         <div className="pointers">
-                            <div className="pointer active"/>
-                            <div className="pointer"/>
-                            <div className="pointer"/>
+                            <div className="pointer active" onClick={this.slideItem.bind(this, 1)}/>
+                            <div className="pointer" onClick={this.slideItem.bind(this, 2)}/>
+                            <div className="pointer" onClick={this.slideItem.bind(this, 3)}/>
                         </div>
                     </div>
-                    <div className="slider-box">
+                    <div className="slider-box" >
                         <div className="appendix">
                             <i className="icon-quote-client icon" />
                         </div>
                         <div className="customer-box-info">
                             <div className="customer-text">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
                                 labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
                             </div>
@@ -47,7 +74,45 @@ class OutsourceInfo extends React.Component {
                                 <div className="customer-photo">
                                     <img className="ui circular image" src="../../public/img/outsource-clients/boss-example-client.jpg" />                                </div>
                                 <div className="customer-name">
-                                    Elena
+                                    Gina
+                                </div>
+                                <div className="customer-role">
+                                    - Digital Strategist & Project Manager
+                                </div>
+                            </div>
+                            <div className="customer-corporate-logo">
+                                <img src="../../public/img/outsource-clients/client-example.png" />
+                            </div>
+                        </div>
+                        <div className="customer-box-info">
+                            <div className="customer-text">
+                                2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                labore et dolore magna aliqua. Lorem ipsum dolor sit amet.
+                            </div>
+                            <div className="customer-info">
+                                <div className="customer-photo">
+                                    <img className="ui circular image" src="../../public/img/outsource-clients/boss-example-client.jpg" />                                </div>
+                                <div className="customer-name">
+                                    Franca
+                                </div>
+                                <div className="customer-role">
+                                    - Digital Strategist & Project Manager
+                                </div>
+                            </div>
+                            <div className="customer-corporate-logo">
+                                <img src="../../public/img/outsource-clients/client-example.png" />
+                            </div>
+                        </div>
+                        <div className="customer-box-info">
+                            <div className="customer-text">
+                                3 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </div>
+                            <div className="customer-info">
+                                <div className="customer-photo">
+                                    <img className="ui circular image" src="../../public/img/outsource-clients/boss-example-client.jpg" />                                </div>
+                                <div className="customer-name">
+                                    Paola
                                 </div>
                                 <div className="customer-role">
                                     - Digital Strategist & Project Manager
@@ -58,6 +123,7 @@ class OutsourceInfo extends React.Component {
                             </div>
                         </div>
                     </div>
+
                 </div>
                 <div className="request-box seven wide column">
                     <div className="title-request">
