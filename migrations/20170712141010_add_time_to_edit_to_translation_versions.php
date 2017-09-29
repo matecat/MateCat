800928818316e08cec285d1f842795f8c56867ca
Fabrizio Regini <freegenie@gmail.com>
2017-09-29 10:35:07 +0200

    change migration

--- migrations/20170712141010_add_time_to_edit_to_translation_versions.php
+++ migrations/20170712141010_add_time_to_edit_to_translation_versions.php
@@ -5,7 +5,7 @@ use Phinx\Migration\AbstractMigration;
 class AddTimeToEditToTranslationVersions extends AbstractMatecatMigration {
 
     public $sql_up = [
-            'ALTER TABLE segment_translation_versions ADD COLUMN  time_to_edit INT(11) NULL'
+            'ALTER TABLE segment_translation_versions ADD COLUMN  time_to_edit INT(11) NULL, algorithm=INPLACE, lock=NONE'
     ];
 
     public $sql_down = [
