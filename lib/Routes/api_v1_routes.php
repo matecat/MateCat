0d85dd5385d2b42b97167acc1f88e84e4a835914
Fabrizio Regini <freegenie@gmail.com>
2017-10-26 15:27:35 +0200

    new endpoint to check for project status

--- lib/Routes/api_v1_routes.php
+++ lib/Routes/api_v1_routes.php
@@ -8,3 +8,7 @@
 
 
 route( '/api/v1/jobs/[:id_job]/[:password]/stats', 'GET',  'API\V1\StatsController', 'stats' );
+
+$klein->with('/api/v1/projects/[:id_project]/[:password]', function() {
+    route( '/creation_status',      'GET',  'API\V2\ProjectCreationStatusController',   'get' );
+});
