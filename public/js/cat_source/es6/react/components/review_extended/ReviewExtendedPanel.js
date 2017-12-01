f3596275089f762c2bf8cec823d6a97288d6e901
Ruben Santillan <rubensantillan83@gmail.com>
2017-12-01 17:27:36 +0100

    Set box sizing for review panel

--- public/js/cat_source/es6/react/components/review_extended/ReviewExtendedPanel.js
+++ public/js/cat_source/es6/react/components/review_extended/ReviewExtendedPanel.js
@@ -18,8 +18,13 @@ class ReviewExtendedPanel extends React.Component{
     }
 
     render() {
-        return <div >
-            Pippo
+        return <div className="re-track-changes-box">
+            <div className="re-header-track">
+                <h4>Revise Track changes</h4>
+                <div className="re-track-changes">
+                    Prova <span className="deleted"> per track</span> changes <span className="added">che bella</span> la vita
+                </div>
+            </div>
         </div>;
     }
 }
