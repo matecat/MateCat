9d56c1512b4deb40fd31d7d538e737e4c980c38c
riccio82 <riccio82@gmail.com>
2017-12-01 11:17:05 +0100

    Review-extended v2

---
+++ public/js/cat_source/es6/react/components/review_extended/ReviewExtendedPanel.js
@@ -0,0 +1,27 @@
+class ReviewExtendedPanel extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+
+    }
+
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return <div >
+            Pippo
+        </div>;
+    }
+}
+
+export default ReviewExtendedPanel ;
