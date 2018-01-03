7399efca0b15fef0e206952d0c98b185f00a3852
Ruben Santillan <rubensantillan83@gmail.com>
2018-01-03 18:23:37 +0100

    PayPal: fix CSS for revise page

--- public/js/cat_source/es6/react/components/segments/SegmentBody.js
+++ public/js/cat_source/es6/react/components/segments/SegmentBody.js
@@ -127,7 +127,6 @@ class SegmentBody extends React.Component {
                     <a href="#" title={status_change_title}
                        className="status" id={"segment-"+ this.props.segment.sid + "-changestatus"}/>
                 </div>
-                {/*<div className="triangle-text"></div>*/}
             </div>
         )
     }
