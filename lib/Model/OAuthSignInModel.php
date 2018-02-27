9507e56df65fdcc2eb1cc368df71b08645a1308e
Fabrizio Regini <freegenie@gmail.com>
2018-02-27 18:44:46 +0100

    refactoring of oauth SignIn

---
+++ lib/Model/OAuthSignInModel.php
@@ -0,0 +1,104 @@
+<?php
+
+use Email\WelcomeEmail;
+use Teams\TeamDao;
+
+/**
+ * Created by PhpStorm.
+ * User: fregini
+ * Date: 27/02/2018
+ * Time: 17:34
+ */
+
+class OAuthSignInModel {
+
+    protected $user ;
+    protected $profilePictureUrl ;
+
+    public function __construct( $firstName, $lastName, $email ) {
+        $this->user = new Users_UserStruct( [
+                'first_name' => $firstName,
+                'last_name' => $lastName,
+                'email' => $email
+        ] );
+
+    }
+
+    public function setAccessToken( $token ) {
+        $this->user->oauth_access_token = OauthTokenEncryption::getInstance()->encrypt(
+                $token
+        );
+    }
+
+    public function signIn() {
+        $userDao = new Users_UserDao() ;
+        $existingUser = $userDao->getByEmail( $this->user->email ) ;
+
+        if ( $existingUser ) {
+            $welcome_new_user = !$existingUser->everSignedIn();
+            $this->_updateExistingUser($existingUser) ;
+
+        } else {
+            $welcome_new_user = true ;
+            $this->_createNewUser();
+        }
+
+        if ( $welcome_new_user ) {
+            $this->_welcomeNewUser();
+        }
+
+        $this->_authenticateUser();
+
+        if ( !is_null( $this->profilePictureUrl ) ) {
+            $this->_updateProfilePicture() ;
+        }
+
+        $project = new \Users\RedeemableProject($this->user, $_SESSION)  ;
+        $project->tryToRedeem()  ;
+
+        return true ;
+    }
+
+    protected function _updateProfilePicture() {
+        $dao = new \Users\MetadataDao();
+        $dao->set($this->user->uid, 'gplus_picture', $this->profilePictureUrl );
+    }
+
+    public function setProfilePicture( $pictureUrl ) {
+        $this->profilePictureUrl = $pictureUrl ;
+    }
+
+    protected function _createNewUser() {
+        $this->user->create_date = Utils::mysqlTimestamp(time() ) ;
+        $this->user->uid = Users_UserDao::insertStruct($this->user);
+
+        $dao = new TeamDao();
+        $dao->getConnection()->begin();
+        $dao->createPersonalTeam($this->user);
+        $dao->getConnection()->commit();
+    }
+
+    protected function _updateExistingUser(Users_UserStruct $existing_user) {
+        $this->user->uid = $existing_user->uid ;
+        Users_UserDao::updateStruct( $this->user, array('fields' =>
+                                                                array('oauth_access_token')
+        ) ) ;
+    }
+
+    protected function _authenticateUser() {
+        AuthCookie::setCredentials($this->user->email, $this->user->uid );
+        $_SESSION[ 'cid' ]  = $this->user->email ;
+        $_SESSION[ 'uid' ]  = $this->user->uid ;
+    }
+
+    protected function _welcomeNewUser() {
+        $email = new WelcomeEmail($this->user) ;
+        $email->send() ;
+        FlashMessage::set('popup', 'profile', FlashMessage::SERVICE);
+    }
+
+
+
+
+
+}
