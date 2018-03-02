bf6db50ff94386562998026b16c430858f60ff04
Ruben Santillan <rubensantillan83@gmail.com>
2018-03-02 11:34:02 +0100

    Shortcut-modal: Customize modal for shortcuts created

--- public/js/cat_source/es6/react/modals/ShortCutsModal.js
+++ public/js/cat_source/es6/react/modals/ShortCutsModal.js
@@ -12,6 +12,34 @@ class ShortCutsModal extends React.Component {
 
             </div>
             <div className="matecat-modal-middle">
+                <div className="shortcut-list">
+                    <h2>Translate/Revise Page</h2>
+                    <div className="shortcut-item-list">
+                        <div className="shortcut-item">
+                            <div className="shortcut-title">
+                                Translate/Approve & go Next
+                            </div>
+                            <div className="shortcut-keys">
+                                <div className="shortcuts mac">
+                                    <div className="keys ctrl" />+
+                                    <div className="keys shift" />+
+                                    <div className="keys return" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="shortcut-item">
+                            <div className="shortcut-title">
+                                Translate/Approve & go Next
+                            </div>
+                            <div className="shortcut-keys">
+                                <div className="shortcuts mac">
+                                    <div className="keys ctrl" />+
+                                    <div className="keys shift" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
             <div className="matecat-modal-bottom">
