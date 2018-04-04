07d3c565c276ec74c622c598f3ab75f2f5f40658
Vincenzo Ruffa <vincenzoruffa1991@gmail.com>
2018-04-04 17:55:13 +0200

    manage decorator

--- lib/Decorator/AnalyzeDecorator.php
+++ lib/Decorator/AnalyzeDecorator.php
@@ -6,9 +6,6 @@
  * Time: 12:39
  */
 
-use Jobs\JobStatsStruct;
-use LexiQA\LexiQADecorator;
-
 class AnalyzeDecorator {
 
     private $controller;
@@ -18,22 +15,19 @@ class AnalyzeDecorator {
      */
     private $template;
 
-
-    private $lang_handler ;
-
     public function __construct( analyzeController $controller, PHPTAL $template ) {
 
-        $this->controller     = $controller;
-        $this->template       = $template;
+        $this->controller = $controller;
+        $this->template   = $template;
 
-        $this->lang_handler = Langs_Languages::getInstance();
     }
+
 
     public function decorate() {
         $this->template->build_number = INIT::$BUILD_NUMBER;
         $this->template->support_mail = INIT::$SUPPORT_MAIL;
 
-        $this->template->split_enabled = true;
+        $this->template->split_enabled    = true;
         $this->template->enable_outsource = true;
 
     }
