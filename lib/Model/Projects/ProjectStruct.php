738f34e92b519139e98da1d489d34d12adaeed3f
Fabrizio Regini <freegenie@gmail.com>
2018-04-05 12:40:00 +0200

    make use of static loadForProject method

--- lib/Model/Projects/ProjectStruct.php
+++ lib/Model/Projects/ProjectStruct.php
@@ -166,9 +166,7 @@ class Projects_ProjectStruct extends DataAccess_AbstractDaoSilentStruct implemen
      */
     public function getFeatures() {
         return $this->cachable(__METHOD__, $this, function( Projects_ProjectStruct $project ) {
-            $featureSet = new FeatureSet() ;
-            $featureSet->loadForProject( $project );
-            return $featureSet ;
+            return FeatureSet::loadForProject( $project ) ;
         });
     }
 
