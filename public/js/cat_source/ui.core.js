c0eb6fe1d4e20450d421a330851ded3182a6d6a4
Ruben Santillan <rubensantillan83@gmail.com>
2018-05-04 15:03:28 +0200

    HotFix: Fix alignments for: ID Segment, More/Fewer button on concordance, Edit & Cancel on Glossary

--- public/js/cat_source/ui.core.js
+++ public/js/cat_source/ui.core.js
@@ -488,7 +488,7 @@ UI = {
 	},
 
 
-	createJobMenu: function() {
+	/*createJobMenu: function() {
 		var menu = '<nav id="jobMenu" class="topMenu">' +
 				'    <ul>';
 		$.each(config.firstSegmentOfFiles, function() {
@@ -501,7 +501,22 @@ UI = {
 				'    </ul>' +
 				'</nav>';
 		this.body.append(menu);
-	},
+	},*/
+
+    createJobMenu: function() {
+        var menu = '<nav id="jobMenu" class="topMenu">' +
+            '<ul class="gotocurrentsegment">' +
+            '<li class="currSegment" data-segment="' + UI.currentSegmentId + '"><a href="#">Go to current segment</a></li>' +
+            '</ul>' +
+            '<ul class="jobmenu-list">';
+        $.each(config.firstSegmentOfFiles, function() {
+            menu += '<li data-file="' + this.id_file + '" data-segment="' + this.first_segment + '"><span class="' + UI.getIconClass(this.file_name.split('.')[this.file_name.split('.').length -1]) + '"></span><a href="#" title="' + this.file_name + '" >' + this.file_name.substring(0,20).concat("[...]" ).concat((this.file_name).substring(this.file_name.length-20))  + '</a></li>';
+        });
+        menu += '</ul>' +
+            '</nav>';
+        this.body.append(menu);
+    },
+
     handleReturn: function(e) {
         if(!this.hiddenTextEnabled) return;
         e.preventDefault();
