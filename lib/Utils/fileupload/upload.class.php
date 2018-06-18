3b8ec5c38ea1660d6cd4a8b79e95d1c793ec9c1a
domenico <domenico@translated.net>
2018-06-18 13:23:18 +0200

    Removed unuseful doc

--- lib/Utils/fileupload/upload.class.php
+++ lib/Utils/fileupload/upload.class.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
- * jQuery File Upload Plugin PHP Class 5.11.2
- * https://github.com/blueimp/jQuery-File-Upload
- *
- * Copyright 2010, Sebastian Tschan
- * https://blueimp.net
- *
- * Licensed under the MIT license:
- * http://www.opensource.org/licenses/MIT
- */
 define( "DIRSEP", "//" );
 
 class UploadHandler {
