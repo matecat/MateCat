b786b125aa5cebcdf43495ff74c4e02b139f9b3c
riccio82 <riccio82@gmail.com>
2018-07-18 10:40:09 +0200

    Segments infinite scroll

--- public/js/cat_source/es6/react/ajax_utils/quality_report/qrUtils.js
+++ public/js/cat_source/es6/react/ajax_utils/quality_report/qrUtils.js
@@ -5,6 +5,21 @@ let QUALITY_REPORT =  {
     getSegmentsFiles() {
 
         let data = {
+            jid: 840,
+            password: "1389c4989bdf",
+            step: 30,
+            segment: "756318",
+            where: 'after'
+        };
+        return $.ajax({
+            data: data,
+            type: "POST",
+            url : "/?action=getSegments"
+        });
+    },
+    getSegmentsFiles2() {
+
+        let data = {
             jid: 841,
             password: "5ec5f5a60ca2",
             step: 30,
@@ -16,7 +31,7 @@ let QUALITY_REPORT =  {
             type: "POST",
             url : "/?action=getSegments"
         });
-    }
+    },
 }
 
 export default QUALITY_REPORT ;
--- public/js/cat_source/es6/react/components/quality_report/SegmentsDetailsContainer.js
+++ public/js/cat_source/es6/react/components/quality_report/SegmentsDetailsContainer.js
@@ -1,5 +1,6 @@
 import Filters from "./FilterSegments";
 import FileDetails from "./FileDetails"
+import QualityReportActions from "./../../actions/QualityReportActions"
 
 class SegmentsDetails extends React.Component {
 
@@ -12,6 +13,29 @@ class SegmentsDetails extends React.Component {
             });
         }
         return files;
+    }
+
+    scrollDebounceFn() {
+        let self = this;
+        return _.debounce(function() {
+            self.onScroll();
+        }, 200)
+    }
+
+    onScroll(){
+        // (window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 500)
+        if ( $(window).scrollTop() + $(window).height() > $(document).height() - 200)  {
+            console.log("Load More Segments!");
+            QualityReportActions.getMoreQRSegments();
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.scrollDebounceFn(), false);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.scrollDebounceFn(), false);
     }
 
     render () {
