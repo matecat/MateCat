cce72100b30ae42e6c47fb139a268d56020b4665
Vincenzo Ruffa <vincenzoruffa1991@gmail.com>
2018-09-18 16:17:46 +0200

    fix query filter category on qr

--- lib/Model/Segments/SegmentDao.php
+++ lib/Model/Segments/SegmentDao.php
@@ -164,7 +164,7 @@ class Segments_SegmentDao extends DataAccess_AbstractDao {
 
         if ( (isset( $options[ 'filter' ][ 'issue_category' ] ) && $options[ 'filter' ][ 'issue_category' ] != '' ) OR (isset( $options[ 'filter' ][ 'severity' ] ) &&  $options[ 'filter' ][ 'severity' ] != '') ) {
 
-            $options_join_query .= " LEFT JOIN qa_entries e ON e.id_segment = st.id_segment ";
+            $options_join_query .= " LEFT JOIN qa_entries e ON e.id_segment = st.id_segment AND e.id_job = st.id_job ";
             $options_join_query .= " LEFT JOIN segment_revisions sr ON sr.id_segment = st.id_segment ";
 
             if ( isset( $options[ 'filter' ][ 'issue_category' ] ) && $options[ 'filter' ][ 'issue_category' ] != '' ) {
