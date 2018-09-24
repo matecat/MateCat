22e5548048c2ea905d9e3f73404c39f25ea394ff
Vincenzo Ruffa <vincenzoruffa1991@gmail.com>
2018-09-24 11:21:49 +0200

    fix count severty point by chunk

--- lib/Model/LQA/ChunkReviewDao.php
+++ lib/Model/LQA/ChunkReviewDao.php
@@ -64,6 +64,27 @@ class ChunkReviewDao extends \DataAccess_AbstractDao {
      *
      * @return int
      */
+    public static function countPenaltyPointsForChunk( \Chunks_ChunkStruct $chunk ) {
+        $sql = "SELECT SUM(penalty_points) FROM qa_entries e
+            JOIN jobs on jobs.id = e.id_job
+            WHERE jobs.id = :id_job AND jobs.password = :password
+              BETWEEN jobs.job_first_segment AND jobs.job_last_segment
+             ";
+
+        $conn = \Database::obtain()->getConnection();
+        $stmt = $conn->prepare( $sql );
+        $stmt->execute(array('id_job' => $chunk->id , 'password' => $chunk->password ));
+        $count =  $stmt->fetch();
+
+        $penalty_points = $count[0] == null ? 0 : $count[0];
+        return $penalty_points ;
+    }
+
+    /**
+     * @param \Chunks_ChunkStruct $chunk
+     *
+     * @return int
+     */
     public static function getPenaltyPointsForChunk( \Chunks_ChunkStruct $chunk ) {
         $sql = "SELECT SUM(penalty_points) FROM qa_entries e
             JOIN segment_translations st
