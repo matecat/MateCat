d44dffe33ecbc4c372cdc687ac2bd9139a0c8d33
Domenico Lupinetti <Ostico@users.noreply.github.com>
2018-10-31 12:53:54 +0100

    Update 20181026145655_update_translation_versions_new_and_old_status.php
    Fast with one row command and not locking with INPLACE algorithm.

--- migrations/20181026145655_update_translation_versions_new_and_old_status.php
+++ migrations/20181026145655_update_translation_versions_new_and_old_status.php
@@ -1,14 +1,11 @@
 <?php
 
-class UpdateTranslationVersionsNewAndOldStatus extends AbstractMatecatMigration
-{
+class UpdateTranslationVersionsNewAndOldStatus extends AbstractMatecatMigration {
     public $sql_up = [
-            "ALTER TABLE `segment_translation_versions` ADD COLUMN old_status int(11) default null ",
-            "ALTER TABLE `segment_translation_versions` ADD COLUMN new_status int(11) default null ",
-    ] ;
+            "ALTER TABLE `segment_translation_versions` ADD COLUMN old_status INT(11) DEFAULT NULL, ADD COLUMN new_status INT(11) DEFAULT NULL, algorithm=INPLACE, lock=NONE"
+    ];
 
     public $sql_down = [
-            "ALTER TABLE `segment_translation_versions` DROP COLUMN old_status",
-            "ALTER TABLE `segment_translation_versions` DROP COLUMN new_status"
+            "ALTER TABLE `segment_translation_versions` DROP COLUMN old_status, DROP COLUMN new_status, algorithm=INPLACE, lock=NONE"
     ];
 }
