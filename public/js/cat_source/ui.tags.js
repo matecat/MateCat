d49f065834c0fd5bb4080b6c926c942b8efcb268
riccio82 <riccio82@gmail.com>
2018-12-10 12:16:36 +0100

    Edit area: fix paste text

--- public/js/cat_source/ui.tags.js
+++ public/js/cat_source/ui.tags.js
@@ -141,11 +141,11 @@ $.extend(UI, {
             .replace( /\&lt;\/div\>/gi, "</div>" )
             .replace( /\&lt;br\>/gi, "<br />" )
             .replace( /\&lt;br \/>/gi, "<br />" )
-            .replace( /\&lt;mark/gi, "<mark" )
+            .replace( /\&lt;mark /gi, "<mark " )
             .replace( /\&lt;\/mark/gi, "</mark" )
-            .replace( /\&lt;ins/gi, "<ins" ) // For translation conflicts tab
+            .replace( /\&lt;ins /gi, "<ins " ) // For translation conflicts tab
             .replace( /\&lt;\/ins/gi, "</ins" ) // For translation conflicts tab
-            .replace( /\&lt;del/gi, "<del" ) // For translation conflicts tab
+            .replace( /\&lt;del /gi, "<del " ) // For translation conflicts tab
             .replace( /\&lt;\/del/gi, "</del" ) // For translation conflicts tab
             .replace( /\&lt;br class=["\'](.*?)["\'][\s]*[\/]*(\&gt;|\>)/gi, '<br class="$1" />' )
             .replace( /(&lt;\s*\/\s*(g|x|bx|ex|bpt|ept|ph|it|mrk)\s*&gt;)/gi, brTx2 );
