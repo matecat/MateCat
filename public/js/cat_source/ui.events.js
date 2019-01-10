99f9353eb0b084fc74157083f65bb0515678aa16
riccio82 <riccio82@gmail.com>
2019-01-10 10:51:10 +0100

    Disable shortcut to add next tag

--- public/js/cat_source/ui.events.js
+++ public/js/cat_source/ui.events.js
@@ -66,10 +66,12 @@ $.extend(UI, {
         }).on('keydown.shortcuts', null, UI.shortcuts.cattol.events.copyContribution3.keystrokes[this.shortCutskey], function(e) {
             e.preventDefault();
             SegmentActions.chooseContribution(UI.getSegmentId(UI.currentSegment), 3);
-        }).on('keydown.shortcuts', null, UI.shortcuts.cattol.events.addNextTag.keystrokes[this.shortCutskey], function(e) {
-            e.preventDefault();
-            UI.autoFillNextTagInTarget()
-        }).on('keydown.shortcuts', null, "ctrl+u", function(e) {
+        })
+        //     .on('keydown.shortcuts', null, UI.shortcuts.cattol.events.addNextTag.keystrokes[this.shortCutskey], function(e) {
+        //     e.preventDefault();
+        //     UI.autoFillNextTagInTarget()
+        // })
+            .on('keydown.shortcuts', null, "ctrl+u", function(e) {
             // to prevent the underline shortcut
             e.preventDefault();
         }).on('keydown.shortcuts', null, "ctrl+b", function(e) {
