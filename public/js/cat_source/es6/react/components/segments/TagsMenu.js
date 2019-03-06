affd008a9604ebd90ac8cf7cd4ef512793e1af1f
riccio82 <riccio82@gmail.com>
2019-03-06 18:12:18 +0100

    fix

--- public/js/cat_source/es6/react/components/segments/TagsMenu.js
+++ public/js/cat_source/es6/react/components/segments/TagsMenu.js
@@ -175,7 +175,7 @@ class TagsMenu extends React.Component {
         }
 
         if ( missingItems.length <= 1 && addedItems.length <= 1 ) {
-            menuItems.push(<div className={"item added-tag no-results"} key={0} data-original="item">No results</div> );
+            menuItems.push(<div className={"item added-tag no-results"} key={0} data-original="item">No matches</div> );
             return <div className="ui vertical menu">
                 <div>
                 {menuItems}
