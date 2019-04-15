683b8035db4150be2f6a6a74d65123fdc93d3454
Fabrizio Regini <freegenie@gmail.com>
2019-04-15 11:58:03 +0200

    prevent auto-activation on project

--- lib/Plugins/Features/SecondPassReview.php
+++ lib/Plugins/Features/SecondPassReview.php
@@ -18,6 +18,8 @@ use LQA\ChunkReviewDao;
 class SecondPassReview extends BaseFeature {
     const FEATURE_CODE = 'second_pass_review' ;
 
+    protected $autoActivateOnProject = false ;
+
     protected static $dependencies = [
             Features::REVIEW_EXTENDED
     ];
