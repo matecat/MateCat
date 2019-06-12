3b046b31e7ea9a3ddfa92c47dc4f993354eb741f
Vincenzo Ruffa <vincenzo.ruffa@kromin.it>
2019-06-12 14:59:47 +0200

    improved revise and translate link with password on cat page

--- lib/Decorator/CatDecorator.php
+++ lib/Decorator/CatDecorator.php
@@ -3,14 +3,14 @@
 use Jobs\JobStatsStruct;
 use LexiQA\LexiQADecorator;
 
-class CatDecorator {
+class CatDecorator extends \AbstractDecorator {
 
-    private $controller;
+    protected $controller;
 
     /**
      * @var PHPTALWithAppend
      */
-    private $template;
+    protected $template;
 
     /**
      * @var Chunks_ChunkStruct
@@ -43,7 +43,6 @@ class CatDecorator {
     public function decorate() {
         $this->template->isReview                         = $this->controller->isRevision();
         $this->template->header_quality_report_item_class = '';
-        $this->template->review_password                  = $this->controller->getReviewPassword();
 
         $this->template->header_main_button_enabled = true;
         $this->template->header_main_button_label   = $this->getHeaderMainButtonLabel();
@@ -63,11 +62,7 @@ class CatDecorator {
 
         $this->template->status_labels = json_encode( $this->getStatusLabels() );
 
-        if ( $this->controller->isRevision() ) {
-            $this->decorateForRevision();
-        } else {
-            $this->decorateForTranslate();
-        }
+        $this->assignCatDecorator();
 
         $this->setQualityReportHref();
 
@@ -148,27 +143,7 @@ class CatDecorator {
       return $label;
   }
 
-    private function decorateForRevision() {
-        $this->template->footer_show_revise_link    = false;
-        $this->template->footer_show_translate_link = true;
-        $this->template->review_class               = 'review';
-        $this->template->review_type                = 'simple';
 
-        // TODO: move this logic in javascript QualityReportButton component
-        if ( $this->controller->getQaOverall() == 'fail' ||
-                $this->controller->getQaOverall() == 'poor'
-        ) {
-            $this->template->header_quality_report_item_class = 'hide';
-        }
-
-    }
-
-    private function decorateForTranslate() {
-        $this->template->footer_show_revise_link    = true;
-        $this->template->footer_show_translate_link = false;
-        $this->template->review_class               = '';
-        $this->template->review_type                = 'simple';
-    }
 
     private function setQualityReportHref() {
         $this->template->quality_report_href =
