35ef2dd564585427edd718f50e93988c20447911
riccio82 <riccio82@gmail.com>
2019-08-07 16:13:01 +0200

    Fix getSegments crash, utils namespace

--- lib/Plugins/Features/SecondPassReview.php
+++ lib/Plugins/Features/SecondPassReview.php
@@ -160,7 +160,7 @@ class SecondPassReview extends BaseFeature {
 
         foreach ( $data['files'] as $file => $content ) {
             foreach( $content['segments'] as $key => $segment ) {
-                $data ['files'] [ $file ] ['segments'] [ $key ] ['revision_number'] = Utils::sourcePageToRevisionNumber(
+                $data ['files'] [ $file ] ['segments'] [ $key ] ['revision_number'] = SecondPassReviewUtils::sourcePageToRevisionNumber(
                         $by_id_segment[ $segment['sid'] ]->source_page
                 );
             }
