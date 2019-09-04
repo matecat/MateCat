5ba536a5546ea86fdcb13b03f13052c31341324b
mauretto78 <maurocassani1978@gmail.com>
2019-09-04 17:28:41 +0200

    updated to WholeTextFinder v1.0.5

--- lib/Model/Search/SearchModel.php
+++ lib/Model/Search/SearchModel.php
@@ -11,8 +11,8 @@ namespace Search;
 
 use Database;
 use Exception;
-use Finder\WholeTextFinder;
 use Log;
+use Matecat\Finder\WholeTextFinder;
 use PDO;
 use PDOException;
 use SubFiltering\Filter;
