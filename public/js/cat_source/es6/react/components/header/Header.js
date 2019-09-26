4650829a9715afb7d61ab978b49c3a7c933d0b5a
Manuel Fontana <manuel.fontana@kromin.it>
2019-09-26 15:03:44 +0200

    Icon manage hover style

--- public/js/cat_source/es6/react/components/header/Header.js
+++ public/js/cat_source/es6/react/components/header/Header.js
@@ -172,7 +172,11 @@ class Header extends React.Component {
 							selectedTeamId={selectedTeamId}
 						/>}
 						<div className={"separator"}></div>
-						{!!loggedUser && !showFilterProjects && <a class={"action-submenu"} href={'/manage'}><IconManage width={'36'} height={'36'} style={{float:'right'}} /></a>}
+						{!!loggedUser && !showFilterProjects && <div title="Manage" id="action-manage">
+																	<a className={"action-submenu"} href={'/manage'}>
+																		<IconManage width={'36'} height={'36'} style={{float:'right'}} />
+																	</a>
+																</div>}
 						{userIcon}
 					</div>
 
