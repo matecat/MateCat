7478025efb10bc0c0557f4897a292c6d515ad204
Marco Ciotoli <marco@kromin.it>
2019-11-05 18:40:41 +0100

    Improvement list of languages for navigation and selection

--- public/js/cat_source/es6/components/languageSelector/LanguageSelectorList.js
+++ public/js/cat_source/es6/components/languageSelector/LanguageSelectorList.js
@@ -1,6 +1,9 @@
 class LanguageSelectorList extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			position: 0
+		}
 	}
 
 	componentDidMount() {
@@ -10,32 +13,40 @@ class LanguageSelectorList extends React.Component {
 	componentWillUnmount() {
 	}
 
-	componentDidUpdate() {
-
+	componentDidUpdate(prevProps) {
+		if (prevProps.querySearch !== this.props.querySearch) {
+			this.setState({
+				position: 0
+			})
+		}
 	}
 
 	render() {
 		const languages = this.getLanguagesInColumns();
+		const {querySearch} = this.props;
+		const {position} = this.state;
 		return <div className="languages-columns">
 			{languages.map((languagesColumn, key) => {
 				return (
 					<ul key={key} className={'dropdown__list'}>
-						{languagesColumn.map(e=><li>{e.name}</li>)}
+						{languagesColumn.map((e, key2) => {
+							return <li key={`${key}${key2}`}
+									   className={(querySearch && key2 === position) ? 'hover' : null}>{e.name}</li>
+						})}
 					</ul>
 				);
 			})}
-			</div>
+		</div>
 	}
 
-	getFilteredLanguages = () =>{
-		const {languagesList,querySearch} = this.props;
-		const filteredLanguages = languagesList.filter(e=>e.name.toLowerCase().indexOf(querySearch.toLowerCase()) > -1);
-		return filteredLanguages
+	getFilteredLanguages = () => {
+		const {languagesList, querySearch} = this.props;
+		return languagesList.filter(e => e.name.toLowerCase().indexOf(querySearch.toLowerCase()) === 0);
 	}
 
 	getLanguagesInColumns = () => {
 		const {getFilteredLanguages} = this;
-		const { languagesList } = this.props;
+		const {languagesList} = this.props;
 		const languagesPerColumn = Math.ceil(languagesList.length / 4);
 		const filteredLanguagesInColumns = chunk(getFilteredLanguages(), languagesPerColumn);
 
@@ -70,4 +81,4 @@ export const chunk = (array, size) => {
 };
 
 
-export const buildRangeArray = items => Array.apply(null, { length: items }).map(Number.call, Number);
+export const buildRangeArray = items => Array.apply(null, {length: items}).map(Number.call, Number);
