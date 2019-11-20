3d683b9b64b5eb8fd461b24aef2c59023470e492
riccio82 <riccio82@gmail.com>
2019-11-20 17:31:23 +0100

    Fix revise buttons

--- public/js/cat_source/es6/components/segments/SegmentButtons.js
+++ public/js/cat_source/es6/components/segments/SegmentButtons.js
@@ -57,7 +57,7 @@ class SegmentButton extends React.Component {
         const classDisable = (this.props.disabled) ? 'disabled' : '';
         let nextButton, currentButton;
         let nextSegment = SegmentStore.getNextSegment(this.props.segment.sid, this.props.segment.fid);
-        let enableGoToNext = !_.isUndefined(nextSegment) && nextSegment.status === "APPROVED";
+        let enableGoToNext = !_.isUndefined(nextSegment) && nextSegment.status !== "APPROVED";
         const filtering = (SegmentFilter.enabled() && SegmentFilter.filtering() && SegmentFilter.open);
         const className = ReviewExtended.enabled() ? "revise-button-" + ReviewExtended.number : '';
         enableGoToNext = ReviewExtended.enabled() ? enableGoToNext && nextSegment.revision_number === config.revisionNumber: enableGoToNext;
