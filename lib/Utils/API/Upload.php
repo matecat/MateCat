a271f5382db0d4e47a7a267a1a50e2188bcd53fc
domenico <domenico@translated.net>
2019-11-20 12:04:09 +0100

    Fixed upload/download issues with file names containing #§

--- lib/Utils/API/Upload.php
+++ lib/Utils/API/Upload.php
@@ -274,7 +274,7 @@ class Upload {
 
         //Fix Bug: Zip files, file names with contiguous whitespaces ( replaced with only one _ and not found inside the zip on download )
         $string = preg_replace( '/\p{Zs}/u', chr(0x1A), $stringName ); // substitute whitespaces
-        $string = preg_replace( '/[^#\pL0-9,\.\-\=_&()\'\"\+\x1A]/u', '', $string ); //strips odd chars and preserve preceding placeholder
+        $string = preg_replace( '/[^#\pL0-9,\.\-=_&()\'\"\+\x1A§]/u', '', $string ); //strips odd chars and preserve preceding placeholder
         $string = preg_replace( '/' . chr(0x1A) . '/', '_', $string ); //strips whitespace and odd chars
         $string = filter_var( $string, FILTER_SANITIZE_STRING, array( 'flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_NO_ENCODE_QUOTES ) );
 
