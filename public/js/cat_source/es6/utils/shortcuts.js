43f715b1369336ca75dd9a333d9c7bb7ba6e99d3
riccio82 <riccio82@gmail.com>
2020-01-27 18:00:48 +0100

    shortcuts: next tab

--- public/js/cat_source/es6/utils/shortcuts.js
+++ public/js/cat_source/es6/utils/shortcuts.js
@@ -177,7 +177,7 @@ const Shortcuts = {
                     "equivalent": "",
                     "keystrokes": {
                         "standard": "alt+s",
-                        "mac": "alt+s",
+                        "mac": "ctrl+alt+s",
                     }
                 }
             }
