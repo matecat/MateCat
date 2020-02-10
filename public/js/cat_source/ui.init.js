a55e31bee271018639ea07f207f6cdab7b04f307
riccio82 <riccio82@gmail.com>
2020-02-10 12:07:10 +0100

    Fix: last opened segment at refresh

--- public/js/cat_source/ui.init.js
+++ public/js/cat_source/ui.init.js
@@ -48,6 +48,12 @@ $.extend(UI, {
 
         options.openCurrentSegmentAfter = !!((!seg) && (!this.firstLoad));
 
+        var hash = CommonUtils.parsedHash.segmentId;
+        config.last_opened_segment = CommonUtils.getLastSegmentFromLocalStorage();
+        if (!config.last_opened_segment) {
+            config.last_opened_segment = files.first_job_segment;
+        }
+        this.startSegmentId = (hash && hash != "") ? hash : config.last_opened_segment;
 
         if ( UI.firstLoad ) {
 
