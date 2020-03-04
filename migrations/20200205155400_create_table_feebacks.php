dfe4d083b8075cc201e9b0614f2712301b95b648
mauretto78 <maurocassani1978@gmail.com>
2020-03-04 16:17:19 +0100

    Added migrations

---
+++ migrations/20200205155400_create_table_feebacks.php
@@ -0,0 +1,21 @@
+<?php
+
+class CreateTableFeedbacks extends AbstractMatecatMigration {
+
+    public $sql_up = <<<EOF
+            CREATE TABLE `revision_feedbacks` (
+          `id` int(11) NOT NULL AUTO_INCREMENT,
+          `id_job` int(11) NOT NULL,
+          `password` varchar(45) NOT NULL,
+          `revision_number` int(1) NOT NULL,
+          `feedback` text,
+          PRIMARY KEY (`id`),
+          UNIQUE KEY `job_unique_key` (`id_job`,`password`,`revision_number`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
+    )
+;
+EOF;
+
+    public $sql_down = "DROP TABLE revision_feedbacks";
+
+}
