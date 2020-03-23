2ef7de18cc83ee7b89a025fd3a4b089ffa1124de
mauretto78 <maurocassani1978@gmail.com>
2020-03-23 17:07:05 +0100

    migration fix

--- migrations/20200323145227_add_standard_wc_to_job_table.php
+++ migrations/20200323145227_add_standard_wc_to_job_table.php
@@ -2,15 +2,9 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class AddStandardWcToJobTable extends AbstractMigration  {
+class AddStandardWcToJobTable extends AbstractMatecatMigration
+{
+    public $sql_up = [ 'ALTER TABLE `jobs` ADD COLUMN `standard_analysis_wc` DOUBLE(20,2) NULL DEFAULT 0.00 AFTER `total_raw_wc`' ];
 
-    public $sql_up = <<<EOF
-      ALTER TABLE `jobs` 
-ADD COLUMN `standard_analysis_wc` DOUBLE(20,2) NULL DEFAULT 0.00 AFTER `total_raw_wc`;
-EOF;
-
-    public $sql_down = <<<EOF
-    ALTER TABLE `jobs` 
-    DROP COLUMN `standard_analysis_wc`;
-EOF;
+    public $sql_down = ['ALTER TABLE `jobs` DROP COLUMN `standard_analysis_wc`' ] ;
 }
