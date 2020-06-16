59914d4808301b7525ca58d9cab5d931963cbcfd
mauretto78 <maurocassani1978@gmail.com>
2020-06-16 16:39:54 +0200

    Hidden tab (&#157;) handling improvement

--- lib/Utils/SubFiltering/Filters/PlaceBreakingSpacesInXliff.php
+++ lib/Utils/SubFiltering/Filters/PlaceBreakingSpacesInXliff.php
@@ -10,16 +10,14 @@ class PlaceBreakingSpacesInXliff extends AbstractHandler {
      * @inheritDoc
      */
     public function transform( $segment ) {
-        $segment = str_replace(
-                [ "\r\n", "\r", "\n", "\t" ],
+        return str_replace(
+                [ "\r\n", "\r", "\n", "\t", "" ],
                 [
                         '&#13;&#10;',
                         '&#13;',
                         '&#10;',
                         '&#09;',
+                        '&#157;'
                 ], $segment );
-
-        // handle 9D character (TAB)
-        return preg_replace('/\x9d/', '&#09;', $segment);
     }
 }
