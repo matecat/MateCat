244015845ada1c935acc802909a031fbcc6f822f
Luca Barone <baro.luc@gmail.com>
2021-04-08 10:50:46 +0200

    ✅ First frontend test (#1953)
    * ✅ add jest for JavaScript testing
    
    * :memo: describe test related scripts
    
    * :fire: remove jest from global package.json
    
    * :sparkles: add first test
    
    * :heavy_plus_sign: add jest dependency and scripts
    
    * :see_no_evil: ignore coverage folder generated by jest
    
    * :wrench: add babel and jest config files
    
    * :wrench: add setup files for jest
    
    * :memo: add docs for yarn test and coverage scripts
    
    * :white_check_mark: add tests for InputField module
    
    * add comment

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = api => {
+  const isTest = api.env('test')
+
+  if (isTest) {
+    return {presets: ['@babel/preset-react', ['@babel/preset-env', {targets: {node: 'current'}}]]}
+  }
+
+  throw new Error('babel.config.js :: environment not supported, yet.')
+}
