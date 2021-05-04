9a12d92e1ba0bde210a3b4f4eba29b793a048dca
Mauro Cassani <m.cassani@bestnetwork.it>
2021-05-04 10:00:37 +0200

    _isTagValid() removed [h1-6] check

--- lib/Utils/SubFiltering/Filters/HtmlToPh.php
+++ lib/Utils/SubFiltering/Filters/HtmlToPh.php
@@ -102,14 +102,14 @@ class HtmlToPh extends AbstractHandler {
          * - starting with / ( optional )
          * - NOT starting with a number
          * - containing [a-zA-Z0-9\-\._] at least 1
-         * - ending with a letter a-zA-Z or a quote "' or /
+         * - ending with a letter a-zA-Z0-9 or a quote "' or /
          *
          */
         if ( preg_match( '#<[/]{0,1}(?![0-9]+)[a-z0-9\-\._]+?(?:\s[:_a-z]+=.+?)?\s*[\/]{0,1}>#is', $buffer ) ){
-            if( is_numeric( substr( $buffer, -2, 1 ) ) && !preg_match( '#<[/]{0,1}[h][1-6][^>]*>#is', $buffer ) ){ //H tag are an exception
-                //tag can not end with a number
-                return false;
-            }
+//            if( is_numeric( substr( $buffer, -2, 1 ) ) && !preg_match( '#<[/]{0,1}[h][1-6][^>]*>#is', $buffer ) ){ //H tag are an exception
+//                //tag can not end with a number
+//                return false;
+//            }
 
             //this case covers when filters create an xliff tag inside an html tag:
             //EX:
