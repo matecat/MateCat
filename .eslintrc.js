7fd4ca7b8baeb7a58062638347f838f8e893f3a0
Luca Barone <baro.luc@gmail.com>
2021-07-07 12:37:18 +0200

    🚨 add eslint plugins for testing (#2090)
    * 🚨 add eslint plugins for testing
    
    * Fix linting issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,11 @@ module.exports = {
         ecmaFeatures: {jsx: true},
       },
       env: {jest: true, node: true, browser: true, es6: true},
+      extends: [
+        'plugin:jest/recommended',
+        'plugin:jest-dom/recommended',
+        'plugin:testing-library/react',
+      ],
     },
 
     // grunt browserify compiled files
