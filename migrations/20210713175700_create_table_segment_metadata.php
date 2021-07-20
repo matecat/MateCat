383bcf44ccd2047d0dd6d6752c3fbd42753c62f4
Mauro Cassani <m.cassani@bestnetwork.it>
2021-07-20 17:53:08 +0200

    Fix migrations

--- migrations/20210713175700_create_table_segment_metadata.php
+++ migrations/20210713175700_create_table_segment_metadata.php
@@ -7,8 +7,7 @@ class CreateTableSegmentMetadata extends AbstractMatecatMigration {
           `id_segment` bigint(20) NOT NULL,
           `meta_key` varchar(45) NOT NULL,
           `meta_value` varchar(255) NOT NULL,
-          PRIMARY KEY (`id_segment`,`meta_key`),
-          KEY `id_segment_idx` (`id_segment`)
+          PRIMARY KEY (`id_segment`,`meta_key`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
     ' ];
 
