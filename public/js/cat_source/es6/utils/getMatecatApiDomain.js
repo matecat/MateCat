0ee6e8e3f279f7b97335bc448f7e79b5a43b8f5f
Luca Barone <baro.luc@gmail.com>
2021-07-22 09:50:35 +0200

    Fix rebase

--- public/js/cat_source/es6/utils/getMatecatApiDomain.js
+++
@@ -1,9 +0,0 @@
-export const getMatecatApiDomain = () => {
-  if (config.enableMultiDomainApi) {
-    const randomInt = Math.floor(Math.random() * config.ajaxDomainsNumber)
-
-    return `//${randomInt}.ajax.${location.host}/`
-  }
-
-  return config.basepath
-}
