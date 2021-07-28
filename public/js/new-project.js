07bb14023fbe8a6640bdbd534d02e14460621e3c
Luca Barone <baro.luc@gmail.com>
2021-07-28 15:53:05 +0200

    🔥 remove LanguageSelector module global export

--- public/js/new-project.js
+++ public/js/new-project.js
@@ -7,6 +7,7 @@ import TeamsActions from './cat_source/es6/actions/TeamsActions'
 import ModalsActions from './cat_source/es6/actions/ModalsActions'
 import CatToolActions from './cat_source/es6/actions/CatToolActions'
 import Header from './cat_source/es6/components/header/Header'
+import LanguageSelector from './cat_source/es6/components/languageSelector'
 
 APP.openOptionsPanel = function (tab, elem) {
   var elToClick = $(elem).attr('data-el-to-click') || null
