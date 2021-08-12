22f25d33e682f0bd491ff5b6a5d31dae73ca42b7
Federico Ricciuti <riccio82@gmail.com>
2021-08-12 17:41:21 +0200

    Update Header.test.js

--- public/js/cat_source/es6/components/header/Header.test.js
+++ public/js/cat_source/es6/components/header/Header.test.js
@@ -1,6 +1,7 @@
 import {render, screen, waitFor} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import React from 'react'
+import ReactDOM from 'react-dom'
 import {rest} from 'msw'
 
 import {mswServer} from '../../../../../mocks/mswServer'
