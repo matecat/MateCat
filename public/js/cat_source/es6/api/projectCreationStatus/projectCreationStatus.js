cf1a1ec5c1b280325896ab073993a84b7d11d1de
piedicianni <pierluigi.dicianni@translated.net>
2021-09-15 18:02:44 +0200

    Refactoring some api's

---
+++ public/js/cat_source/es6/api/projectCreationStatus/projectCreationStatus.js
@@ -0,0 +1,24 @@
+import {getMatecatApiDomain} from '../../utils/getMatecatApiDomain'
+
+/**
+ * Project creation status
+ *
+ * @param {string} idProject
+ * @param {string} password
+ * @returns {Promise<object>}
+ */
+export const projectCreationStatus = async (idProject, password) => {
+  const response = await fetch(
+    `${getMatecatApiDomain()}api/v2/projects/${idProject}/${password}/creation_status`,
+    {
+      credentials: 'include',
+    },
+  )
+
+  if (!response.ok) return Promise.reject(response)
+
+  const {errors, ...data} = await response.json()
+  if (errors && errors.length > 0) return Promise.reject(errors)
+  const {status} = response
+  return {data, status}
+}
