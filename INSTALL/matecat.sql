44c075a6ac919e33f1c56cfe66c9045a8f2fc752
Mauro Cassani <maurocassani1978@gmail.com>
2022-01-14 14:33:43 +0100

    matecat.sql

--- INSTALL/matecat.sql
+++ INSTALL/matecat.sql
@@ -71,6 +71,27 @@ CREATE TABLE `api_keys` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `blacklist_files`
+--
+
+DROP TABLE IF EXISTS `blacklist_files`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE IF NOT EXISTS `blacklist_files` (
+      `id` bigint(20) NOT NULL AUTO_INCREMENT,
+      `id_job` bigint(20) NOT NULL,
+      `password` varchar(45) NOT NULL,
+      `file_path` varchar(255) NOT NULL,
+      `file_name` varchar(255) NOT NULL,
+      `target` VARCHAR(10) NOT NULL,
+      `uid` bigint(20) NOT NULL,
+        PRIMARY KEY (`id`),
+        KEY `uid` (`uid`) USING BTREE,
+        KEY `id_job` (`id_job`) USING BTREE,
+        UNIQUE KEY `id_job_password` (`id_job`, `password`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+--
 -- Table structure for table `chunk_completion_events`
 --
 
