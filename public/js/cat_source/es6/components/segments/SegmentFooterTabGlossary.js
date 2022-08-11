b88a3afa9f7b30f2c515cd67f6a12781d9845e27
riccio <riccio82@gmail.com>
2022-08-11 12:29:02 +0200

    Glossary: new glossary segment tab - wip

--- public/js/cat_source/es6/components/segments/SegmentFooterTabGlossary.js
+++ public/js/cat_source/es6/components/segments/SegmentFooterTabGlossary.js
@@ -41,13 +41,62 @@ export const SegmentFooterTabGlossary = ({active_class}) => {
               </div>
             </div>
             <div className={'glossary_item-actions'}>
-              <span>Mod</span>
-              <span>del</span>
+              <span>
+                <ModifyIcon />
+              </span>
+              <span>
+                <DeleteIcon />
+              </span>
             </div>
           </div>
-          <div className={'glossary_item-body'}></div>
+
+          <div className={'glossary_item-body'}>
+            <div className={'glossary-item_column'}>
+              <div className={'glossary_word'}>
+                Payment <span className={'glossary_badge'}>Ex</span>
+              </div>
+              <div className={'glossary-description'}>
+                The amount a rider, eater, user of UberRUSH, and other products,
+                pays to get a ride, get a meal or a package delivered, etc.
+              </div>
+            </div>
+            <div className={'glossary-item_column'}>
+              <div className={'glossary_word'}>
+                Pagamento <span className={'glossary_badge'}>Ex</span>
+              </div>
+              <div className={'glossary-description'}>
+                L'importo che un rider, un cliente UberEats, un utente di
+                UberRUSH e di altri prodotti paga per ottenere una corsa, farsi
+                consegnare un pasto o un pacco, ecc
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+  )
+}
+
+const DeleteIcon = () => {
+  return (
+    <svg width="14" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M1 3.333a.667.667 0 0 0 0 1.334V3.333Zm12 1.334a.667.667 0 1 0 0-1.334v1.334ZM11.667 4h.666a.667.667 0 0 0-.666-.667V4Zm-9.334 9.333h-.666.666ZM3.667 4A.667.667 0 1 0 5 4H3.667Zm2-2.667V.667v.666Zm2.666 0V.667v.666ZM9 4a.667.667 0 0 0 1.333 0H9ZM6.333 7.333a.667.667 0 1 0-1.333 0h1.333Zm-1.333 4a.667.667 0 0 0 1.333 0H5Zm4-4a.667.667 0 0 0-1.333 0H9Zm-1.333 4a.667.667 0 1 0 1.333 0H7.667ZM1 4.667h1.333V3.333H1v1.334Zm1.333 0H13V3.333H2.333v1.334ZM11 4v9.333h1.333V4H11Zm0 9.333c0 .177-.07.347-.195.472l.943.943a2 2 0 0 0 .585-1.415H11Zm-.195.472a.667.667 0 0 1-.472.195v1.333a2 2 0 0 0 1.415-.585l-.943-.943Zm-.472.195H3.667v1.333h6.666V14Zm-6.666 0a.667.667 0 0 1-.472-.195l-.943.943a2 2 0 0 0 1.415.585V14Zm-.472-.195A.667.667 0 0 1 3 13.333H1.667a2 2 0 0 0 .585 1.415l.943-.943ZM3 13.333V4H1.667v9.333H3Zm-.667-8.666h9.334V3.333H2.333v1.334ZM5 4V2.667H3.667V4H5Zm0-1.333c0-.177.07-.347.195-.472l-.943-.943a2 2 0 0 0-.585 1.415H5Zm.195-.472A.667.667 0 0 1 5.667 2V.667a2 2 0 0 0-1.415.585l.943.943ZM5.667 2h2.666V.667H5.667V2Zm2.666 0c.177 0 .347.07.472.195l.943-.943A2 2 0 0 0 8.333.667V2Zm.472.195A.667.667 0 0 1 9 2.667h1.333a2 2 0 0 0-.585-1.415l-.943.943ZM9 2.667V4h1.333V2.667H9ZM5 7.333v4h1.333v-4H5Zm2.667 0v4H9v-4H7.667Z"
+        fillRule="evenodd"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
+
+const ModifyIcon = () => {
+  return (
+    <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M7.333 3.333a.667.667 0 0 0 0-1.333v1.333Zm-4.666-.666V2v.667ZM1.333 4H.667h.666Zm0 9.333H.667h.666ZM14 8.667a.667.667 0 0 0-1.333 0H14Zm-1.667-7-.471-.472.471.472Zm1-.415V.586v.666Zm1 2.415-.471-.472.471.472ZM8 10l.162.647a.668.668 0 0 0 .31-.176L8 10Zm-2.667.667-.647-.162a.667.667 0 0 0 .809.808l-.162-.646ZM6 8l-.471-.471a.667.667 0 0 0-.176.31L6 8Zm1.333-6H2.667v1.333h4.666V2ZM2.667 2a2 2 0 0 0-1.415.586l.943.943a.667.667 0 0 1 .472-.196V2Zm-1.415.586A2 2 0 0 0 .667 4H2c0-.177.07-.346.195-.471l-.943-.943ZM.667 4v9.333H2V4H.667Zm0 9.333a2 2 0 0 0 .585 1.415l.943-.943A.667.667 0 0 1 2 13.333H.667Zm.585 1.415a2 2 0 0 0 1.415.585V14a.666.666 0 0 1-.472-.195l-.943.943Zm1.415.585H12V14H2.667v1.333Zm9.333 0a2 2 0 0 0 1.414-.585l-.943-.943A.666.666 0 0 1 12 14v1.333Zm1.414-.585A2 2 0 0 0 14 13.332h-1.333c0 .177-.07.347-.196.472l.943.943ZM14 13.332V8.667h-1.333v4.666H14ZM12.805 2.138c.14-.14.33-.219.528-.219V.586c-.552 0-1.08.219-1.471.61l.943.942Zm.528-.219c.198 0 .389.079.529.22l.943-.944c-.39-.39-.92-.61-1.472-.61V1.92Zm.529.22c.14.14.219.33.219.528h1.333c0-.552-.22-1.082-.61-1.472l-.942.943Zm.219.528a.748.748 0 0 1-.22.528l.944.943c.39-.39.61-.92.61-1.471H14.08Zm-.22.528L7.53 9.53l.942.942 6.334-6.333-.943-.943ZM7.839 9.353l-2.666.667.323 1.293 2.667-.666-.324-1.294ZM5.98 10.828l.667-2.666-1.294-.324-.667 2.667 1.294.323Zm.491-2.357 6.334-6.333-.943-.943L5.529 7.53l.942.942Z"
+        fillRule="evenodd"
+        fill="currentColor"
+      />
+    </svg>
   )
 }
