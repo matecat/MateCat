ad27ca397b3019dd7b110f80cf4c2a45b4dbc959
riccio <riccio82@gmail.com>
2022-08-23 16:22:24 +0200

    Glossary: new glossary segment tab - wip

--- public/js/cat_source/es6/components/segments/SegmentFooterTabGlossary.js
+++ public/js/cat_source/es6/components/segments/SegmentFooterTabGlossary.js
@@ -7,72 +7,178 @@ import React, {useState} from 'react'
 export const SegmentFooterTabGlossary = ({active_class}) => {
   const [searchSource, setSearchSource] = useState()
   const [searchTarget, setSearchTarget] = useState()
+  const [showAddBox, setShowAddBox] = useState(true)
+  const [showMore, setShowMore] = useState(false)
+  const openAddTerm = () => {
+    setShowAddBox(true)
+  }
+
+  const getAddTermBox = () => {
+    return (
+      <div className={'glossary_add-container'}>
+        <div>
+          <div className={'input-with-label__wrapper'}>
+            <label>Definition</label>
+            <input name="glossary-term-definition" />
+          </div>
+          <div>
+            <div className={'input-with-label__wrapper'}>
+              <label>Glossary</label>
+              <input name="glossary-term-tm" />
+            </div>
+            <div className={'input-with-label__wrapper'}>
+              <label>Domain</label>
+              <input name="glossary-term-domain" />
+            </div>
+            <div className={'input-with-label__wrapper'}>
+              <label>Subdomain</label>
+              <input name="glossary-term-subdomani" />
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className={'input-with-label__wrapper'}>
+            <label>Original term*</label>
+            <input name="glossary-term-original" />
+          </div>
+          <div className={'input-with-label__wrapper'}>
+            <label>Translated term*</label>
+            <input name="glossary-term-translated" />
+          </div>
+        </div>
+
+        <div className={'more-line'}>
+          <div>
+            <div className={'input-with-label__wrapper'}>
+              <label>Description</label>
+              <input
+                className={'input-large'}
+                name="glossary-term-description-source"
+              />
+            </div>
+            <div className={'input-with-label__wrapper'}>
+              <label>Example phrase</label>
+              <input
+                className={'input-large'}
+                name="glossary-term-example-source"
+              />
+            </div>
+          </div>
+          <div>
+            <div className={'input-with-label__wrapper'}>
+              <label>Description</label>
+              <input
+                className={'input-large'}
+                name="glossary-term-description-target"
+              />
+            </div>
+            <div className={'input-with-label__wrapper'}>
+              <label>Example phrase</label>
+              <input
+                className={'input-large'}
+                name="glossary-term-example-target"
+              />
+            </div>
+          </div>
+        </div>
+        <div className={'glossary_buttons-container'}>
+          <div></div>
+          <div
+            className={`glossary-more ${!showMore ? 'show-less' : 'show-more'}`}
+            onClick={() => setShowMore(!showMore)}
+          >
+            <MoreIcon />
+            <span>{showMore ? 'Hide options' : 'More options'}</span>
+          </div>
+          <div className={'glossary_buttons'}>
+            <button className={'glossary__button-cancel'}>Cancel</button>
+            <button className={'glossary__button-add'}>Add</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  const getGlossaryItemBox = () => {
+    return (
+      <div className={'glossary_item'}>
+        <div className={'glossary_item-header'}>
+          <div className={'glossary_definition-container'}>
+            <span className={'glossary_definition'}>
+              <GlossaryDefinitinIcon />
+              The action or process of paying someone or something or of being
+              paid.
+            </span>
+            <span className={'glossary_badge'}>Uber</span>
+            <span className={'glossary_badge'}>Rider</span>
+            <div className={'glossary_source'}>
+              <b>Uber Glossary</b>
+              <span>2022-07-08</span>
+            </div>
+          </div>
+          <div className={'glossary_item-actions'}>
+            <ModifyIcon />
+            <DeleteIcon />
+          </div>
+        </div>
+
+        <div className={'glossary_item-body'}>
+          <div className={'glossary-item_column'}>
+            <div className={'glossary_word'}>
+              Payment <span className={'glossary_badge'}>Ex</span>
+            </div>
+            <div className={'glossary-description'}>
+              The amount a rider, eater, user of UberRUSH, and other products,
+              pays to get a ride, get a meal or a package delivered, etc.
+            </div>
+          </div>
+          <div className={'glossary-item_column'}>
+            <div className={'glossary_word'}>
+              Pagamento <span className={'glossary_badge'}>Ex</span>
+            </div>
+            <div className={'glossary-description'}>
+              L'importo che un rider, un cliente UberEats, un utente di UberRUSH
+              e di altri prodotti paga per ottenere una corsa, farsi consegnare
+              un pasto o un pacco, ecc
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
   return (
     <div className={`tab sub-editor glossary ${active_class}`}>
-      <div className={'glossary_search'}>
-        <div className={'glossary_search-source'}>
-          <input
-            className={'glossary_search-input'}
-            placeholder={'Search source'}
-            onChange={(event) => setSearchSource(event.target.value)}
-          />
-        </div>
-        <div className={'glossary_search-target'}>
-          <input
-            className={'glossary_search-input'}
-            placeholder={'Search target'}
-            onChange={(event) => setSearchTarget(event.target.value)}
-          />
-          <button className={'glossary__button-add'}>+ Add Term</button>
-        </div>
-      </div>
-      <div className={'glossary_items'}>
-        <div className={'glossary_item'}>
-          <div className={'glossary_item-header'}>
-            <div className={'glossary_definition-container'}>
-              <span className={'glossary_badge'}>Uber</span>
-              <span className={'glossary_badge'}>Rider</span>
-              <span className={'glossary_definition'}>
-                The action or process of paying someone or something or of being
-                paid.
-              </span>
-              <div className={'glossary_source'}>
-                Source: <b>Uber Glossary</b> 2022-07-08
-              </div>
+      {showAddBox ? (
+        getAddTermBox()
+      ) : (
+        <>
+          <div className={'glossary_search'}>
+            <div className={'glossary_search-source'}>
+              <input
+                name="search_source"
+                className={'glossary_search-input'}
+                placeholder={'Search source'}
+                onChange={(event) => setSearchSource(event.target.value)}
+              />
             </div>
-            <div className={'glossary_item-actions'}>
-              <span>
-                <ModifyIcon />
-              </span>
-              <span>
-                <DeleteIcon />
-              </span>
+            <div className={'glossary_search-target'}>
+              <input
+                name="search_target"
+                className={'glossary_search-input'}
+                placeholder={'Search target'}
+                onChange={(event) => setSearchTarget(event.target.value)}
+              />
+              <button
+                className={'glossary__button-add'}
+                onClick={() => openAddTerm()}
+              >
+                + Add Term
+              </button>
             </div>
           </div>
-
-          <div className={'glossary_item-body'}>
-            <div className={'glossary-item_column'}>
-              <div className={'glossary_word'}>
-                Payment <span className={'glossary_badge'}>Ex</span>
-              </div>
-              <div className={'glossary-description'}>
-                The amount a rider, eater, user of UberRUSH, and other products,
-                pays to get a ride, get a meal or a package delivered, etc.
-              </div>
-            </div>
-            <div className={'glossary-item_column'}>
-              <div className={'glossary_word'}>
-                Pagamento <span className={'glossary_badge'}>Ex</span>
-              </div>
-              <div className={'glossary-description'}>
-                L'importo che un rider, un cliente UberEats, un utente di
-                UberRUSH e di altri prodotti paga per ottenere una corsa, farsi
-                consegnare un pasto o un pacco, ecc
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+          <div className={'glossary_items'}>{getGlossaryItemBox()}</div>
+        </>
+      )}
     </div>
   )
 }
@@ -96,6 +202,28 @@ const ModifyIcon = () => {
         d="M7.333 3.333a.667.667 0 0 0 0-1.333v1.333Zm-4.666-.666V2v.667ZM1.333 4H.667h.666Zm0 9.333H.667h.666ZM14 8.667a.667.667 0 0 0-1.333 0H14Zm-1.667-7-.471-.472.471.472Zm1-.415V.586v.666Zm1 2.415-.471-.472.471.472ZM8 10l.162.647a.668.668 0 0 0 .31-.176L8 10Zm-2.667.667-.647-.162a.667.667 0 0 0 .809.808l-.162-.646ZM6 8l-.471-.471a.667.667 0 0 0-.176.31L6 8Zm1.333-6H2.667v1.333h4.666V2ZM2.667 2a2 2 0 0 0-1.415.586l.943.943a.667.667 0 0 1 .472-.196V2Zm-1.415.586A2 2 0 0 0 .667 4H2c0-.177.07-.346.195-.471l-.943-.943ZM.667 4v9.333H2V4H.667Zm0 9.333a2 2 0 0 0 .585 1.415l.943-.943A.667.667 0 0 1 2 13.333H.667Zm.585 1.415a2 2 0 0 0 1.415.585V14a.666.666 0 0 1-.472-.195l-.943.943Zm1.415.585H12V14H2.667v1.333Zm9.333 0a2 2 0 0 0 1.414-.585l-.943-.943A.666.666 0 0 1 12 14v1.333Zm1.414-.585A2 2 0 0 0 14 13.332h-1.333c0 .177-.07.347-.196.472l.943.943ZM14 13.332V8.667h-1.333v4.666H14ZM12.805 2.138c.14-.14.33-.219.528-.219V.586c-.552 0-1.08.219-1.471.61l.943.942Zm.528-.219c.198 0 .389.079.529.22l.943-.944c-.39-.39-.92-.61-1.472-.61V1.92Zm.529.22c.14.14.219.33.219.528h1.333c0-.552-.22-1.082-.61-1.472l-.942.943Zm.219.528a.748.748 0 0 1-.22.528l.944.943c.39-.39.61-.92.61-1.471H14.08Zm-.22.528L7.53 9.53l.942.942 6.334-6.333-.943-.943ZM7.839 9.353l-2.666.667.323 1.293 2.667-.666-.324-1.294ZM5.98 10.828l.667-2.666-1.294-.324-.667 2.667 1.294.323Zm.491-2.357 6.334-6.333-.943-.943L5.529 7.53l.942.942Z"
         fillRule="evenodd"
         fill="currentColor"
+      />
+    </svg>
+  )
+}
+
+const GlossaryDefinitinIcon = () => {
+  return (
+    <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M13.333 12.667v-2H4.667a2 2 0 0 0-2 2m3.2 2H11.2c.747 0 1.12 0 1.405-.146.251-.128.455-.332.583-.582.145-.286.145-.659.145-1.406V3.467c0-.747 0-1.12-.145-1.406a1.333 1.333 0 0 0-.583-.582c-.285-.146-.658-.146-1.405-.146H5.867c-1.12 0-1.68 0-2.108.218a2 2 0 0 0-.875.874c-.217.428-.217.988-.217 2.108v6.934c0 1.12 0 1.68.217 2.108a2 2 0 0 0 .875.874c.427.218.987.218 2.108.218Z"
+        stroke="currentColor"
+      />
+    </svg>
+  )
+}
+
+const MoreIcon = () => {
+  return (
+    <svg width="12" height="8" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M5.2 6.933 1.2 1.6A1 1 0 0 1 2 0h8a1 1 0 0 1 .8 1.6l-4 5.333a1 1 0 0 1-1.6 0Z"
+        fill="#AEBDCD"
       />
     </svg>
   )
