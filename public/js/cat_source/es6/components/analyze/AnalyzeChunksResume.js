d11290d174dd6032c6060fc14630a15a6c7b92ab
riccio <riccio82@gmail.com>
2022-10-20 17:33:10 +0200

    New matecat logo

--- public/js/cat_source/es6/components/analyze/AnalyzeChunksResume.js
+++ public/js/cat_source/es6/components/analyze/AnalyzeChunksResume.js
@@ -607,7 +607,7 @@ class AnalyzeChunksResume extends React.Component {
                 </h5>
               </div>
               <div className="title-matecat-words">
-                <h5>MateCat weighted</h5>
+                <h5>Matecat weighted</h5>
               </div>
             </div>
           </div>
