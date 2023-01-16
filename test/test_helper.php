e08574024f952bb1c0bbe213b3ae0538694d0172
Mauro Cassani <maurocassani1978@gmail.com>
2023-01-16 14:46:12 +0100

    Glossary CSV header validation improvement (allow empty headers) (#2621)

--- test/test_helper.php
+++ test/test_helper.php
@@ -2,7 +2,9 @@
 putenv( 'phpunit=1' );
 
 declare( ticks = 10 );
-require( 'functions.php' );
+
+require( 'support/functions.php' );
+
 setupSignalHandler();
 
 define( 'PROJECT_ROOT', realpath( dirname( __FILE__ ) . '/../' ) . DIRECTORY_SEPARATOR );
@@ -11,8 +13,7 @@ define( 'TEST_DIR', realpath( dirname( __FILE__ ) ) );
 set_include_path( get_include_path() . PATH_SEPARATOR . TEST_DIR );
 
 require( 'lib/IntegrationTest.php' );
-require( 'php_versions_override.php' );
-
+require( 'support/php_versions_override.php' );
 require( PROJECT_ROOT . 'inc/Bootstrap.php' );
 
 register_shutdown_function( function () {
