a61556735b2968a69c94f6e66db9c9c14fc2bb14
Mauro Cassani <maurocassani1978@gmail.com>
2023-01-27 17:27:12 +0100

    Fixed test

--- test/unit/JobUrlBuilderTest.php
+++ test/unit/JobUrlBuilderTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Url\JobUrlStruct;
+
 class JobUrlBuilderTest extends AbstractTest {
 
     /**
@@ -10,25 +12,32 @@ class JobUrlBuilderTest extends AbstractTest {
     public function setUp() {
         parent::setUp();
 
-        $this->database_instance = Database::obtain( INIT::$DB_SERVER, INIT::$DB_USER, INIT::$DB_PASS, INIT::$DB_DATABASE );
+        $test_ini = TestHelper::parseConfigFile( 'test' );
+
+        $db_server = (isset($test_ini[ 'test' ])) ? $test_ini[ 'test' ]['DB_SERVER'] : INIT::$DB_SERVER;
+        $db_user = (isset($test_ini[ 'test' ])) ? $test_ini[ 'test' ]['DB_USER'] : INIT::$DB_USER;
+        $db_pass = (isset($test_ini[ 'test' ])) ? $test_ini[ 'test' ]['DB_PASS'] : INIT::$DB_PASS;
+        $db_database = (isset($test_ini[ 'test' ])) ? $test_ini[ 'test' ]['DB_DATABASE'] : INIT::$DB_DATABASE;
+
+        $this->database_instance = Database::obtain( $db_server, $db_user, $db_pass, $db_database );
     }
 
     public function testBuildTranslationUrl(){
         $job = new Jobs_JobStruct( $this->database_instance->getConnection()->query( "SELECT * FROM jobs ORDER BY id DESC LIMIT 1" )->fetch() );
         $jobUrlStruct = \Url\JobUrlBuilder::createFromJobStruct($job, [
-            'id_segment' => 1
+            'id_segment' => $job->job_first_segment
         ]);
 
-        $this->assertInstanceOf(\Url\JobUrlStruct::class, $jobUrlStruct);
+        $this->assertInstanceOf(JobUrlStruct::class, $jobUrlStruct);
         $this->assertNotNull($jobUrlStruct->getTranslationUrl());
         $this->assertEquals($jobUrlStruct->getUrlByRevisionNumber(), $jobUrlStruct->getTranslationUrl());
-        $this->assertNull($jobUrlStruct->getReviseUrl());
+        $this->assertNotNull($jobUrlStruct->getReviseUrl());
         $this->assertNull($jobUrlStruct->getRevise2Url());
-        $this->assertNull($jobUrlStruct->getUrlByRevisionNumber(1));
+        $this->assertNotNull($jobUrlStruct->getUrlByRevisionNumber(1));
         $this->assertNull($jobUrlStruct->getUrlByRevisionNumber(2));
-        $this->assertNull($jobUrlStruct->getUrlByRevisionNumber("1"));
+        $this->assertNotNull($jobUrlStruct->getUrlByRevisionNumber("1"));
         $this->assertNull($jobUrlStruct->getUrlByRevisionNumber("2"));
-        $this->assertFalse($jobUrlStruct->hasReview());
+        $this->assertTrue($jobUrlStruct->hasReview());
         $this->assertFalse($jobUrlStruct->hasSecondPassReview());
     }
 }
