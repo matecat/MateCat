fff7972b4b10c921764299f0ab7cc2aed19e84f0
piedicianni <53812578+piedicianni@users.noreply.github.com>
2023-03-02 16:08:29 +0100

    Fix change case buttons activation by keyboard (#2683)
    * Fix change case buttons activation by keyboard
    
    * Exclude selection of entity

---
+++ public/js/cat_source/es6/components/segments/utils/DraftMatecatUtils/getSelectedTextWithoutEntities.js
@@ -0,0 +1,40 @@
+const getSelectedTextWithoutEntities = (editorState) => {
+  const selectionState = editorState.getSelection()
+  const anchorKey = selectionState.getAnchorKey()
+  const currentContent = editorState.getCurrentContent()
+  const currentContentBlock = currentContent.getBlockForKey(anchorKey)
+  const start = selectionState.getStartOffset()
+  const end = selectionState.getEndOffset()
+
+  const result = new Array(end - start)
+    .fill({})
+    .map((item, index) => start + index)
+    .reduce((acc, cur) => {
+      const isEntity = !!currentContentBlock.getEntityAt(cur)
+      const updateAccumulator = [...acc]
+
+      const lastItem =
+        updateAccumulator.length &&
+        (!isEntity || (isEntity && !updateAccumulator.slice(-1)[0]?.value))
+          ? updateAccumulator.pop()
+          : {}
+
+      const item = {
+        ...lastItem,
+        ...(!isEntity && {
+          ...(lastItem.start === undefined && {start: cur}),
+          value: `${lastItem.value ? lastItem.value : ''}${currentContentBlock
+            .getText()
+            .substring(cur, cur + 1)}`,
+          end: cur + 1,
+        }),
+      }
+
+      return [...updateAccumulator, item]
+    }, [])
+    .filter((item) => item.value)
+
+  return result
+}
+
+export default getSelectedTextWithoutEntities
