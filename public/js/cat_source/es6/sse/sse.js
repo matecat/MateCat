f4dbcd477be557366bc314210bdcf40e8c1cb394
riccio82 <riccio82@gmail.com>
2023-03-08 16:15:40 +0100

    Improve sse channel tracking

--- public/js/cat_source/es6/sse/sse.js
+++ public/js/cat_source/es6/sse/sse.js
@@ -225,7 +225,7 @@ let NOTIFICATIONS = {
     this.connectionStatus = {
       connectionOpened: false,
       connectionError: false,
-      messageReceived: false,
+      messageAckReceived: false,
     }
     this.source = SSE.getSource('notifications')
 
@@ -240,10 +240,11 @@ let NOTIFICATIONS = {
     this.source.addEventListener(
       'message',
       (e) => {
-        this.connectionStatus.messageReceived = true
         var message = new SSE.Message(JSON.parse(e.data))
         if (message.isValid()) {
           $(document).trigger(message.eventIdentifier, message)
+          if (message._type === 'ack')
+            this.connectionStatus.messageAckReceived = true
         }
       },
       false,
@@ -255,9 +256,9 @@ let NOTIFICATIONS = {
         console.error('SSE: server disconnect')
         let {connectionError} = this.connectionStatus
         if (!connectionError) {
-          const {connectionOpened, connectionError, messageReceived} =
+          const {connectionOpened, connectionError, messageAckReceived} =
             this.connectionStatus
-          const message = `SSE channel disconnect. Connection opened = ${connectionOpened} / Connection error = ${connectionError} / Some message received = ${messageReceived} `
+          const message = `SSE channel disconnect. Connection opened = ${connectionOpened} / Connection error = ${connectionError} / Some message received = ${messageAckReceived} / Id Client: ${config.id_client}`
           CommonUtils.dispatchTrackingError(message)
         }
         this.connectionStatus.connectionError = true
