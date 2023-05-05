c0cf466d0f8380aa9230e90ebc8b07a291c0125f
Mauro Cassani <maurocassani1978@gmail.com>
2023-05-05 16:41:49 +0200

    owner fix

--- lib/Controller/API/NewController.php
+++ lib/Controller/API/NewController.php
@@ -599,6 +599,7 @@ class NewController extends ajaxController {
         $projectStructure[ 'tms_engine' ]           = $this->postInput[ 'tms_engine' ];
         $projectStructure[ 'status' ]               = Constants_ProjectStatus::STATUS_NOT_READY_FOR_ANALYSIS;
         $projectStructure[ 'skip_lang_validation' ] = true;
+        $projectStructure[ 'owner' ]                = $this->user->email;
         $projectStructure[ 'metadata' ]             = $this->metadata;
         $projectStructure[ 'pretranslate_100' ]     = (int)!!$this->postInput[ 'pretranslate_100' ]; // Force pretranslate_100 to be 0 or 1
 
