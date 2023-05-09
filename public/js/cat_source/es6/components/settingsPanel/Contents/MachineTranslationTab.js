576b1935a2bad95156459b5ef5fcc74c8c82d751
riccio82 <riccio82@gmail.com>
2023-05-09 18:07:33 +0200

    Machine Translation Tab - wip

--- public/js/cat_source/es6/components/settingsPanel/Contents/MachineTranslationTab.js
+++ public/js/cat_source/es6/components/settingsPanel/Contents/MachineTranslationTab.js
@@ -1,5 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
+import IconAdd from '../../icons/IconAdd'
 
 export const MachineTranslationTab = () => {
-  return <h2>ContentWrapper: Machine Translation</h2>
+  const [addMTVisible, setAddMTVisible] = useState(false)
+  return (
+    <div className="machine-translation-tab">
+      {!addMTVisible ? (
+        <div className="add-mt-button">
+          <button
+            className="ui primary button"
+            onClick={() => setAddMTVisible(true)}
+          >
+            <IconAdd /> Add MT engine
+          </button>
+        </div>
+      ) : (
+        <div className="add-mt-container">
+          <h2>Add New MT</h2>
+          <button
+            className="ui button orange"
+            onClick={() => setAddMTVisible(false)}
+          >
+            Close
+          </button>
+        </div>
+      )}
+    </div>
+  )
 }
