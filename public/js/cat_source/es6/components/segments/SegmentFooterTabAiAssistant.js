0511d786141029e0d6e60acad201ce245171b710
riccio82 <riccio82@gmail.com>
2023-05-18 12:54:16 +0200

    Bump version - v2.20.59

--- public/js/cat_source/es6/components/segments/SegmentFooterTabAiAssistant.js
+++ public/js/cat_source/es6/components/segments/SegmentFooterTabAiAssistant.js
@@ -108,7 +108,7 @@ export const SegmentFooterTabAiAssistant = ({
     const aiSuggestionHandler = ({sid, suggestion, isCompleted, hasError}) => {
       if (sid === segment.sid && !isCachedLastRequest) {
         if (!hasError) {
-          setHasError(true)
+          setHasError(false)
           setSuggestion({value: suggestion, isCompleted})
           if (isCompleted) {
             const pendingCache = [...memoSuggestions]
