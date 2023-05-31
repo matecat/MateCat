474d2288193ec3271ce5d4fc2894679c465d66c7
riccio82 <riccio82@gmail.com>
2023-05-31 10:47:19 +0200

    Add Switch component

---
+++ public/js/cat_source/es6/components/common/Switch.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Switch = ({
+  name,
+  active = false,
+  disabled = false,
+  onChange = () => {},
+  className = '',
+  testId,
+}) => {
+  const handleChange = (e) => {
+    onChange(e.target.checked)
+  }
+
+  return (
+    <label className={`container ${className}`}>
+      <input
+        type="checkbox"
+        name={name}
+        onChange={handleChange}
+        checked={active}
+        aria-checked={active}
+        disabled={disabled}
+        data-testid={testId}
+      />
+      <span></span>
+    </label>
+  )
+}
+
+Switch.propTypes = {
+  name: PropTypes.string,
+  active: PropTypes.bool,
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
+  className: PropTypes.string,
+  testId: PropTypes.string,
+}
+
+export default Switch
