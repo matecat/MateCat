4fd77647dc8ed2ff581cfb1b68ad3525119809b1
Mauro Cassani <maurocassani1978@gmail.com>
2023-06-12 16:01:25 +0200

    refactoring

--- lib/Model/ProjectManager/ProjectManagerModel.php
+++ lib/Model/ProjectManager/ProjectManagerModel.php
@@ -186,15 +186,7 @@ class ProjectManagerModel {
                     if(isset($attributes['json'][$index])) {
                         $metaKey = $attributes['json'][$index];
 
-                        $metaDataKeys = [
-                            'id_request',
-                            'id_content',
-                            'id_order',
-                            'id_order_group',
-                            'screenshot'
-                        ];
-
-                        if(!in_array($metaKey, $metaDataKeys)){
+                        if(!self::isAMetadata($metaKey)){
                             $insert_values[] = [ $id_segment, $internal_id, null, $json ];
                         }
 
@@ -261,15 +253,7 @@ class ProjectManagerModel {
                         $metaKey = strip_tags(html_entity_decode($attributes['entries'][$index]));
                         $metaValue = strip_tags(html_entity_decode($note));
 
-                        $metaDataKeys = [
-                            'id_request',
-                            'id_content',
-                            'id_order',
-                            'id_order_group',
-                            'screenshot'
-                        ];
-
-                        if(in_array($metaKey, $metaDataKeys)){
+                        if(self::isAMetadata($metaKey)){
                             $insert_values[] = [ $id_segment, $metaKey, $metaValue ];
                         }
                     }
@@ -315,6 +299,23 @@ class ProjectManagerModel {
             Log::doJsonLog( "Notes attributes Flattened Values Dump: " . var_export( $flattened_values, true ) );
             throw new Exception( "Notes attributes import - DB Error: " . $e->getMessage(), 0, $e );
         }
+    }
+
+    /**
+     * @param $metaKey
+     * @return bool
+     */
+    private static function isAMetadata($metaKey)
+    {
+        $metaDataKeys = [
+            'id_request',
+            'id_content',
+            'id_order',
+            'id_order_group',
+            'screenshot'
+        ];
+
+        return in_array($metaKey, $metaDataKeys);
     }
 
     /**
