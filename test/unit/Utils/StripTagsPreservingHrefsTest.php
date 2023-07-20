de07598c5ef14115f77208cd83807a056d2f8410
Mauro Cassani <maurocassani1978@gmail.com>
2023-07-20 15:58:24 +0200

    stripTagsPreservingHrefs() improvements

--- test/unit/Utils/StripTagsPreservingHrefsTest.php
+++ test/unit/Utils/StripTagsPreservingHrefsTest.php
@@ -2,11 +2,20 @@
 
 class StripTagsPreservingHrefsTest extends PHPUnit_Framework_TestCase
 {
+    public function testCanStripTagsFromString()
+    {
+        $string = 'This is a simple test.';
+        $stripTags = Utils::stripTagsPreservingHrefs($string);
+        $expected = $string;
+
+        $this->assertEquals($expected, $stripTags);
+    }
+
     public function testCanStripTagsFromHtml()
     {
         $html = '<p>This is a simple test. <span>This is nested <a href="http://test.com">link</a></span></p>';
         $stripTags = Utils::stripTagsPreservingHrefs($html);
-        $expected = 'This is a simple test. This is nested http://test.com';
+        $expected = 'This is a simple test. This is nested link(http://test.com)';
 
         $this->assertEquals($expected, $stripTags);
     }
@@ -15,7 +24,7 @@ class StripTagsPreservingHrefsTest extends PHPUnit_Framework_TestCase
     {
         $json = '{"AdditionalInfo":"{}","MobileUsages":"[]","ReferenceLinks":"[]","Component":"[]","DynamicValueExample":"<span>Example</span>","KeyName":"<a href=\"https://text.com\" target=\"_blank\">Test</a><br>","Repo":"<a href =\"https://test2.com\" target=\"_blank\">test2</a>"}';
         $stripTags = Utils::stripTagsPreservingHrefs($json);
-        $expected = '{"AdditionalInfo":"{}","MobileUsages":"[]","ReferenceLinks":"[]","Component":"[]","DynamicValueExample":"Example","KeyName":"https://text.com","Repo":"https://test2.com"}';
+        $expected = '{"AdditionalInfo":"{}","MobileUsages":"[]","ReferenceLinks":"[]","Component":"[]","DynamicValueExample":"Example","KeyName":"Test(https://text.com)","Repo":"test2(https://test2.com)"}';
 
         $this->assertEquals($expected, $stripTags);
     }
