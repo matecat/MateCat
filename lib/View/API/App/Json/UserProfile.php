2f1c1236455d360626fc5a2ffb03d3e1bcdf6b7b
Domenico Lupinetti <8008416+Ostico@users.noreply.github.com>
2023-09-19 11:52:48 +0200

    Login improvements and removed old *StructValidator (#2878)
    * Login improvements and removed old *StructValidator
    
    * Updated email
    
    * Fixed Gdrive service user profile
    
    * Check password from eight to twelve chars length
    
    * Css fix
    
    ---------
    
    Co-authored-by: pierluigi.dicianni <pierluigi.dicianni@translated.net>
    Co-authored-by: riccio82 <riccio82@gmail.com>

---
+++ lib/View/API/App/Json/UserProfile.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author hashashiyyin domenico@translated.net / ostico@gmail.com
+ * Date: 05/09/23
+ * Time: 12:09
+ *
+ */
+
+namespace API\App\Json;
+
+use API\V2\Json\Team;
+use API\V2\Json\User;
+use ConnectedServices\ConnectedServiceStruct;
+use Teams\TeamStruct;
+use Users_UserStruct;
+
+class UserProfile {
+
+    /**
+     * @param Users_UserStruct              $user
+     * @param TeamStruct[]                         $teams
+     * @param ConnectedServiceStruct[]|null $servicesStruct
+     * @param array                         $userMetadata
+     *
+     * @return array
+     */
+    public function renderItem( Users_UserStruct $user, array $teams, array $servicesStruct = null, array $userMetadata = [] ) {
+
+        return [
+                'user'               => User::renderItem( $user ),
+                'connected_services' => ( new ConnectedService( $servicesStruct ) )->render(),
+                'teams'              => ( new Team() )->render( $teams ),
+
+            //TODO: this is likely to be unsafe to be passed here without a whitelist.
+                'metadata'           => ( empty( $userMetadata ) ? null : $userMetadata ),
+
+        ];
+
+    }
+
+}
