4de2b43e3ebda4a68436070639c41ecfd82aa863
Mauro Cassani <maurocassani1978@gmail.com>
2023-11-03 11:04:10 +0100

    Custom GDrive error messages

--- lib/Controller/ConnectedServices/GDrive/GDriveController.php
+++ lib/Controller/ConnectedServices/GDrive/GDriveController.php
@@ -202,10 +202,27 @@ class GDriveController extends KleinController {
     private function doResponse() {
         $this->response->json( [
             "success" => $this->isImportingSuccessful,
-            "error_msg" => isset($this->error['msg']) ? $this->error['msg'] : null,
+            "error_msg" => isset($this->error['msg']) ? $this->formatErrorMessage($this->error['msg']) : null,
             "error_class" => isset($this->error['class']) ? $this->error['class'] : null,
             "error_code" => isset($this->error['code']) ? $this->error['code'] : null,
         ] );
+    }
+
+    /**
+     * @param $message
+     * @return string
+     */
+    private function formatErrorMessage($message){
+
+        if($message == "This file is too large to be exported."){
+            return "Upload failed: you are trying to upload a file bigger than 10 mb. Google Drive does not allow exports of files bigger than 10 mb. Please download the file and upload it from your computer.";
+        }
+
+        if($message == "Export only supports Docs Editors files."){
+            return "Upload failed: Google Drive does not allow exports of files in this format. Please open the file in Google Drive and save it as a Google Drive file.";
+        }
+
+        return $message;
     }
 
     /**
