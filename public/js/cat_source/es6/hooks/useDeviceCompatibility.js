fab77819252b85e8e6ca3e516ab43433cc25e3c9
riccio82 <riccio82@gmail.com>
2023-12-05 12:59:52 +0100

    Change browser compatibility check

---
+++ public/js/cat_source/es6/hooks/useDeviceCompatibility.js
@@ -0,0 +1,26 @@
+import {useMediaQuery} from './useMediaQuery'
+
+/**
+ * A React hook that indicate if user device is compatible
+ *
+ * @returns {boolean}
+ */
+function useDeviceCompatibility() {
+  const isFinePointerAvailable = useMediaQuery('(any-pointer:fine)')
+  const isPrimaryPointerFine = useMediaQuery('(pointer:fine)')
+  // const isPrimaryPointerCoarse = useMediaQuery('(pointer:coarse)');
+  // const isCoarsePointerAvailable = useMediaQuery('(any-pointer:coarse)');
+  // const isHoverAvailable = useMediaQuery('(any-hover:hover)');
+
+  const isNotMobileOrTablet = useMediaQuery('(min-device-width:768px)')
+
+  // console.log('DEBUG --> isCoarsePointerAvailable:', isCoarsePointerAvailable);
+  // console.log('DEBUG --> isHoverAvailable:', isHoverAvailable);
+  // console.log('DEBUG --> isPrimaryPointerCoarse:', isPrimaryPointerCoarse);
+  // console.log('DEBUG --> isFinePointerAvailable:', isFinePointerAvailable);
+  // console.log('DEBUG --> isPrimaryPointerFine:', isPrimaryPointerFine);
+  // console.log('DEBUG --> isNotMobileOrTablet:', isNotMobileOrTablet);
+  return isPrimaryPointerFine || isFinePointerAvailable || isNotMobileOrTablet
+}
+
+export default useDeviceCompatibility
