8a7a5c0d9323509be84275e5a05108657591fec4
Mauro Cassani <maurocassani1978@gmail.com>
2023-12-06 13:23:52 +0100

    wip

--- lib/View/API/V2/Json/Error.php
+++ lib/View/API/V2/Json/Error.php
@@ -1,39 +1,54 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * @author domenico domenico@translated.net / ostico@gmail.com
+ * Date: 06/02/17
+ * Time: 16.39
+ *
+ */
 
 namespace API\V2\Json;
 
+
 use Exception;
 
-class Error
-{
-    /**
-     * @var Exception
-     */
-    private $exception;
+class Error {
 
-    /**
-     * Error constructor.
-     * @param Exception $exception
-     */
-    public function __construct( Exception $exception )
-    {
-        $this->exception = $exception;
+    private $data;
+
+    public function __construct( $data = [] ) {
+        $this->data = $data;
     }
 
-    /**
-     * @return array
-     */
-    public function render()
-    {
-        return [
-            "errors" => [
-                [
-                    "code"    => $this->exception->getCode(),
-                    "message" => $this->exception->getMessage()
-                ]
-            ],
+    public function render( $data = null ) {
+
+        $row = [
+            "errors" => [],
             "data"   => []
         ];
+
+        if ( empty( $data ) ) {
+            $data = $this->data;
+        }
+
+        foreach ( $data as $error ) {
+
+            if ( $error instanceof Exception ) {
+                $code   = $error->getCode();
+                $output = $error->getMessage();
+            } else {
+                $code   = -1000;
+                $output = $error;
+            }
+
+            $row[ 'errors' ][] = [
+                "code"    => $code,
+                "message" => $output
+            ];
+
+        }
+
+        return $row;
     }
 
 }
