4e455f3e222ef2f985c838a8bec55e233d6d9c08
riccio82 <riccio82@gmail.com>
2024-02-20 12:12:08 +0100

    Home: Goodby Cat

--- public/js/cat_source/es6/pages/NewProject.js
+++ public/js/cat_source/es6/pages/NewProject.js
@@ -494,7 +494,6 @@ const NewProject = ({
       </div>
 
       <div className="wrapper-upload">
-        <div id="matecat-cat" />
         <div id="languageSelector" />
         <div className="translation-row">
           <div className="translation-options">
