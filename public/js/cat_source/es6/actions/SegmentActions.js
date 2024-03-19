ec65531788cf16fdecf5b74b0b0e14548ed4a110
riccio82 <riccio82@gmail.com>
2024-03-19 15:47:19 +0100

    Sentry fix: MATECAT-2TS

--- public/js/cat_source/es6/actions/SegmentActions.js
+++ public/js/cat_source/es6/actions/SegmentActions.js
@@ -1079,7 +1079,7 @@ const SegmentActions = {
   openIssuesPanel: function (data, openSegment) {
     const segment = SegmentStore.getSegmentByIdToJS(data.sid)
     const canOpenSegment =
-      segment.status !== 'NEW' && segment.status !== 'DRAFT'
+      segment && segment.status !== 'NEW' && segment.status !== 'DRAFT'
     if (segment && !canOpenSegment) {
       return false
     }
