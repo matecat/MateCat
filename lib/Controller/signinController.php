77f2f79c631a82086938042a11602461eecd75b5
Mauro Cassani <maurocassani1978@gmail.com>
2024-03-22 11:47:07 +0100

    template vars

--- lib/Controller/signinController.php
+++ lib/Controller/signinController.php
@@ -2,12 +2,17 @@
 
 class signinController extends viewController {
 
+    public function __construct()
+    {
+        parent::__construct();
+        parent::makeTemplate("signin.html");
+    }
+
     /**
      * @inheritDoc
      */
     function doAction()
     {
-        parent::makeTemplate( 'signin.html' );
     }
 
     /**
