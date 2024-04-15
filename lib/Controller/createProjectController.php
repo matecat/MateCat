b27fea7fd8087ea65276c3238758dc55f2f8d6df
Mauro Cassani <maurocassani1978@gmail.com>
2024-04-15 15:15:42 +0200

    createProjectController fix

--- lib/Controller/createProjectController.php
+++ lib/Controller/createProjectController.php
@@ -7,6 +7,8 @@ use Matecat\XliffParser\Utils\Files as XliffFiles;
 use Matecat\XliffParser\XliffUtils\XliffProprietaryDetect;
 use ProjectQueue\Queue;
 use Validator\EngineValidator;
+use Validator\JSONValidator;
+use Validator\JSONValidatorObject;
 use Validator\MMTValidator;
 
 class createProjectController extends ajaxController {
@@ -29,6 +31,7 @@ class createProjectController extends ajaxController {
     private $due_date;
     private $metadata;
     private $dialect_strict;
+    private $filters_extraction_parameters;
 
     /**
      * @var \Teams\TeamStruct
@@ -49,24 +52,25 @@ class createProjectController extends ajaxController {
         parent::__construct();
 
         $filterArgs = [
-                'file_name'          => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
-                'project_name'       => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
-                'source_lang'        => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
-                'target_lang'        => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
-                'job_subject'        => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
-                'due_date'           => [ 'filter' => FILTER_VALIDATE_INT ],
-                'mt_engine'          => [ 'filter' => FILTER_VALIDATE_INT ],
-                'disable_tms_engine' => [ 'filter' => FILTER_VALIDATE_BOOLEAN ],
-                'lang_detect_files'  => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
-                'private_tm_key'     => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
-                'pretranslate_100'   => [ 'filter' => FILTER_VALIDATE_INT ],
-                'id_team'            => [ 'filter' => FILTER_VALIDATE_INT, 'flags' => FILTER_REQUIRE_SCALAR ],
-                'mmt_glossaries' => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
-                'deepl_id_glossary'  => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
-                'deepl_formality'    => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
-                'project_completion' => [ 'filter' => FILTER_VALIDATE_BOOLEAN ], // features customization
-                'get_public_matches' => [ 'filter' => FILTER_VALIDATE_BOOLEAN ], // disable public TM matches
-                'dialect_strict'    => [ 'filter' => FILTER_SANITIZE_STRING ],
+                'file_name'                     => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
+                'project_name'                  => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
+                'source_lang'                   => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
+                'target_lang'                   => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
+                'job_subject'                   => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
+                'due_date'                      => [ 'filter' => FILTER_VALIDATE_INT ],
+                'mt_engine'                     => [ 'filter' => FILTER_VALIDATE_INT ],
+                'disable_tms_engine'            => [ 'filter' => FILTER_VALIDATE_BOOLEAN ],
+                'lang_detect_files'             => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
+                'private_tm_key'                => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
+                'pretranslate_100'              => [ 'filter' => FILTER_VALIDATE_INT ],
+                'id_team'                       => [ 'filter' => FILTER_VALIDATE_INT, 'flags' => FILTER_REQUIRE_SCALAR ],
+                'mmt_glossaries'                => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
+                'deepl_id_glossary'             => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
+                'deepl_formality'               => [ 'filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FLAG_STRIP_LOW ],
+                'project_completion'            => [ 'filter' => FILTER_VALIDATE_BOOLEAN ], // features customization
+                'get_public_matches'            => [ 'filter' => FILTER_VALIDATE_BOOLEAN ], // disable public TM matches
+                'dialect_strict'                => [ 'filter' => FILTER_SANITIZE_STRING ],
+                'filters_extraction_parameters' => [ 'filter' => FILTER_SANITIZE_STRING ],
 
         ];
 
@@ -159,6 +163,7 @@ class createProjectController extends ajaxController {
         $this->__validateMMTGlossaries();
         $this->__validateDeepLGlossaryParams();
         $this->__validateDialectStrictParam();
+        $this->__validateFiltersExtractionParameters();
         $this->__appendFeaturesToProject();
         $this->__generateTargetEngineAssociation();
         if ( $this->userIsLogged ) {
@@ -277,26 +282,26 @@ class createProjectController extends ajaxController {
 
         $projectStructure = $projectManager->getProjectStructure();
 
-        $projectStructure[ 'project_name' ]                 = $this->project_name;
-        $projectStructure[ 'private_tm_key' ]               = $this->private_tm_key;
-        $projectStructure[ 'uploadToken' ]                  = $_COOKIE[ 'upload_session' ];
-        $projectStructure[ 'array_files' ]                  = $arFiles; //list of file name
-        $projectStructure[ 'array_files_meta' ]             = $arMeta; //list of file metadata
-        $projectStructure[ 'source_language' ]              = $this->source_lang;
-        $projectStructure[ 'target_language' ]              = explode( ',', $this->target_lang );
-        $projectStructure[ 'job_subject' ]                  = $this->job_subject;
-        $projectStructure[ 'mt_engine' ]                    = $this->mt_engine;
-        $projectStructure[ 'tms_engine' ]                   = $this->tms_engine;
-        $projectStructure[ 'status' ]                       = Constants_ProjectStatus::STATUS_NOT_READY_FOR_ANALYSIS;
-        $projectStructure[ 'lang_detect_files' ]            = $this->lang_detect_files;
-        $projectStructure[ 'skip_lang_validation' ]         = true;
-        $projectStructure[ 'pretranslate_100' ]             = $this->pretranslate_100;
-        $projectStructure[ 'dialect_strict' ]               = $this->dialect_strict;
-        $projectStructure[ 'only_private' ]                 = $this->only_private;
-        $projectStructure[ 'due_date' ]                     = $this->due_date;
-        $projectStructure[ 'target_language_mt_engine_id' ] = $this->postInput[ 'target_language_mt_engine_id' ];
-        $projectStructure[ 'user_ip' ]                      = Utils::getRealIpAddr();
-        $projectStructure[ 'HTTP_HOST' ]                    = INIT::$HTTPHOST;
+        $projectStructure[ 'project_name' ]                  = $this->project_name;
+        $projectStructure[ 'private_tm_key' ]                = $this->private_tm_key;
+        $projectStructure[ 'uploadToken' ]                   = $_COOKIE[ 'upload_session' ];
+        $projectStructure[ 'array_files' ]                   = $arFiles; //list of file name
+        $projectStructure[ 'array_files_meta' ]              = $arMeta; //list of file metadata
+        $projectStructure[ 'source_language' ]               = $this->source_lang;
+        $projectStructure[ 'target_language' ]               = explode( ',', $this->target_lang );
+        $projectStructure[ 'job_subject' ]                   = $this->job_subject;
+        $projectStructure[ 'mt_engine' ]                     = $this->mt_engine;
+        $projectStructure[ 'tms_engine' ]                    = $this->tms_engine;
+        $projectStructure[ 'status' ]                        = Constants_ProjectStatus::STATUS_NOT_READY_FOR_ANALYSIS;
+        $projectStructure[ 'lang_detect_files' ]             = $this->lang_detect_files;
+        $projectStructure[ 'skip_lang_validation' ]          = true;
+        $projectStructure[ 'pretranslate_100' ]              = $this->pretranslate_100;
+        $projectStructure[ 'dialect_strict' ]                = $this->dialect_strict;
+        $projectStructure[ 'only_private' ]                  = $this->only_private;
+        $projectStructure[ 'due_date' ]                      = $this->due_date;
+        $projectStructure[ 'target_language_mt_engine_id' ]  = $this->postInput[ 'target_language_mt_engine_id' ];
+        $projectStructure[ 'user_ip' ]                       = Utils::getRealIpAddr();
+        $projectStructure[ 'HTTP_HOST' ]                     = INIT::$HTTPHOST;
 
         // MMT Glossaries
         // (if $engine is not an MMT instance, ignore 'mmt_glossaries')
@@ -312,6 +317,10 @@ class createProjectController extends ajaxController {
 
         if ( $engine instanceof Engines_DeepL and $this->deepl_id_glossary !== null ) {
             $projectStructure[ 'deepl_id_glossary' ] = $this->deepl_id_glossary;
+        }
+
+        if( $this->filters_extraction_parameters ) {
+            $projectStructure[ 'filters_extraction_parameters' ] = $this->filters_extraction_parameters;
         }
 
         //TODO enable from CONFIG
@@ -622,6 +631,26 @@ class createProjectController extends ajaxController {
             }
 
             $this->dialect_strict = html_entity_decode($dialect_strict);
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function __validateFiltersExtractionParameters()
+    {
+        if ( !empty( $this->postInput[ 'filters_extraction_parameters' ] ) ) {
+
+            $json = html_entity_decode( $this->postInput[ 'filters_extraction_parameters' ]);
+            $schema = file_get_contents( \INIT::$ROOT . '/inc/validation/schema/filters_extraction_parameters.json' );
+
+            $validatorObject = new JSONValidatorObject();
+            $validatorObject->json = $json;
+
+            $validator = new JSONValidator($schema);
+            $validator->validate($validatorObject);
+
+            $this->filters_extraction_parameters = json_decode($json);
         }
     }
 
