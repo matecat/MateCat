55680e40e867b9ea3b78e6c735f5269f369384fb
riccio82 <riccio82@gmail.com>
2024-05-07 11:07:26 +0200

    Social login: add url check

--- public/js/cat_source/es6/components/onBoarding/SocialButtons.js
+++ public/js/cat_source/es6/components/onBoarding/SocialButtons.js
@@ -6,36 +6,49 @@ const SocialButtons = () => {
 
   return (
     <div className="login-social-buttons">
-      <Button
-        size={BUTTON_SIZE.ICON_STANDARD}
-        onClick={() => socialLogin(socialUrls.googleUrl)}
-      >
-        <img alt="Google login" src="/public/img/icons/social/google.svg" />
-      </Button>
-      <Button
-        size={BUTTON_SIZE.ICON_STANDARD}
-        onClick={() => socialLogin(socialUrls.linkedIn)}
-      >
-        <img alt="Google login" src="/public/img/icons/social/linkedIn.svg" />
-      </Button>
-      <Button
-        size={BUTTON_SIZE.ICON_STANDARD}
-        onClick={() => socialLogin(socialUrls.microsoft)}
-      >
-        <img alt="Google login" src="/public/img/icons/social/microsoft.svg" />
-      </Button>
-      <Button
-        size={BUTTON_SIZE.ICON_STANDARD}
-        onClick={() => socialLogin(socialUrls.github)}
-      >
-        <img alt="Google login" src="/public/img/icons/social/github.svg" />
-      </Button>
-      <Button
-        size={BUTTON_SIZE.ICON_STANDARD}
-        onClick={() => socialLogin(socialUrls.meta)}
-      >
-        <img alt="Google login" src="/public/img/icons/social/meta.svg" />
-      </Button>
+      {socialUrls.googleUrl && (
+        <Button
+          size={BUTTON_SIZE.ICON_STANDARD}
+          onClick={() => socialLogin(socialUrls.googleUrl)}
+        >
+          <img alt="Google login" src="/public/img/icons/social/google.svg" />
+        </Button>
+      )}
+      {socialUrls.linkedIn && (
+        <Button
+          size={BUTTON_SIZE.ICON_STANDARD}
+          onClick={() => socialLogin(socialUrls.linkedIn)}
+        >
+          <img alt="Google login" src="/public/img/icons/social/linkedIn.svg" />
+        </Button>
+      )}
+      {socialUrls.microsoft && (
+        <Button
+          size={BUTTON_SIZE.ICON_STANDARD}
+          onClick={() => socialLogin(socialUrls.microsoft)}
+        >
+          <img
+            alt="Google login"
+            src="/public/img/icons/social/microsoft.svg"
+          />
+        </Button>
+      )}
+      {socialUrls.github && (
+        <Button
+          size={BUTTON_SIZE.ICON_STANDARD}
+          onClick={() => socialLogin(socialUrls.github)}
+        >
+          <img alt="Google login" src="/public/img/icons/social/github.svg" />
+        </Button>
+      )}
+      {socialUrls.meta && (
+        <Button
+          size={BUTTON_SIZE.ICON_STANDARD}
+          onClick={() => socialLogin(socialUrls.meta)}
+        >
+          <img alt="Google login" src="/public/img/icons/social/meta.svg" />
+        </Button>
+      )}
     </div>
   )
 }
