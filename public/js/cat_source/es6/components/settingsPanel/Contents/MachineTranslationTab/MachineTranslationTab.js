689307e5099fd12bba126215a6c8f1f0dd68a90c
pierluigi.dicianni <pierluigi.dicianni@translated.net>
2024-05-27 16:29:17 +0200

    Added new param filters_xliff_config_template_id

--- public/js/cat_source/es6/components/settingsPanel/Contents/MachineTranslationTab/MachineTranslationTab.js
+++ public/js/cat_source/es6/components/settingsPanel/Contents/MachineTranslationTab/MachineTranslationTab.js
@@ -206,34 +206,36 @@ export const MachineTranslationTab = () => {
   }
 
   useEffect(() => {
-    setMTRows(
-      mtEngines
-        .filter((row) => !activeMTEngine || row.id !== activeMTEngine)
-        .map((row, index) => {
-          return {
-            node: (
-              <MTRow
-                key={index}
-                {...{row}}
-                deleteMT={() => showConfirmDelete.current(row.id)}
-                onCheckboxClick={(row) => setActiveMTEngine(row)}
-              />
-            ),
-            isDraggable: false,
-            ...(deleteMTRequest &&
-              row.id === deleteMTRequest && {
-                isExpanded: true,
-                extraNode: (
-                  <DeleteResource
-                    row={row}
-                    onClose={() => setDeleteMTRequest()}
-                    onConfirm={deleteMT.current}
-                  />
-                ),
-              }),
-          }
-        }),
-    )
+    if (!config.is_cattool) {
+      setMTRows(
+        mtEngines
+          .filter((row) => !activeMTEngine || row.id !== activeMTEngine)
+          .map((row, index) => {
+            return {
+              node: (
+                <MTRow
+                  key={index}
+                  {...{row}}
+                  deleteMT={() => showConfirmDelete.current(row.id)}
+                  onCheckboxClick={(row) => setActiveMTEngine(row)}
+                />
+              ),
+              isDraggable: false,
+              ...(deleteMTRequest &&
+                row.id === deleteMTRequest && {
+                  isExpanded: true,
+                  extraNode: (
+                    <DeleteResource
+                      row={row}
+                      onClose={() => setDeleteMTRequest()}
+                      onConfirm={deleteMT.current}
+                    />
+                  ),
+                }),
+            }
+          }),
+      )
+    }
   }, [activeMTEngine, mtEngines, deleteMTRequest])
 
   const disableMT = () => setActiveMTEngine()
