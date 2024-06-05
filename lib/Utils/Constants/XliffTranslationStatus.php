21b8e2979f5cf9374b991bd1c0280ce9c39dcc2b
Mauro Cassani <maurocassani1978@gmail.com>
2024-06-05 19:28:50 +0200

    XliffTranslationStatus

--- lib/Utils/Constants/XliffTranslationStatus.php
+++ lib/Utils/Constants/XliffTranslationStatus.php
@@ -4,9 +4,24 @@ namespace Constants;
 
 class XliffTranslationStatus {
 
-    // xliff 1.2
+    // xliff 1.2 state-qualifiers
+    // @see https://docs.oasis-open.org/xliff/v1.2/os/xliff-core.html#state-qualifier
+    const EXACT_MATCH              = 'exact-match';
     const FUZZY_MATCH              = 'fuzzy-match';
+    const ID_MATCH                 = 'id-match';
+    const LEVERAGED_GLOSSARY       = 'leveraged-glossary';
+    const LEVERAGED_INHERITED      = 'leveraged-inherited';
+    const LEVERAGED_MT             = 'leveraged-mt';
+    const LEVERAGED_REPOSITORY     = 'leveraged-repository';
+    const LEVERAGED_TM             = 'leveraged-tm';
     const MT_SUGGESTION            = 'mt-suggestion';
+    CONST REJECTED_GRAMMAR         = 'rejected-grammar';
+    CONST REJECTED_INACCURATE      = 'rejected-inaccurate';
+    CONST REJECTED_LENGTH          = 'rejected-length';
+    CONST REJECTED_SPELLING        = 'rejected-spelling';
+    CONST TM_SUGGESTION            = 'tm-suggestion';
+
+    // xliff 1.2 states
     const NEEDS_TRANSLATION        = 'needs-translation';
     const NEEDS_L10N               = 'needs-l10n';
     const NEEDS_ADAPTATION         = 'needs-adaptation';
@@ -14,15 +29,13 @@ class XliffTranslationStatus {
     const NEEDS_REVIEW_L10N        = 'needs-review-l10n';
     const NEEDS_REVIEW_TRANSLATION = 'needs-review-translation';
     const SIGNED_OFF               = 'signed-off';
-    const LEVERAGED_INHERITED      = 'leveraged-inherited';
-    const LEVERAGED_TM             = 'leveraged-tm';
 
-    // xliff 2.0 AND 1.2
+    // xliff 2.0/1.2 states
     const NEW_STATE   = 'new';
     const TRANSLATED  = 'translated';
     const FINAL_STATE = 'final';
 
-    // xliff 2.0
+    // xliff 2.0 states
     const INITIAL  = 'initial';
     const REVIEWED = 'reviewed';
 
