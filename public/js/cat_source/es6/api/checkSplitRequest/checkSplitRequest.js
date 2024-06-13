665f3a6e85e2d0bf0418423b8aac1119f8bdd923
riccio82 <riccio82@gmail.com>
2024-06-13 10:26:31 +0200

    Fix flattenObject util

--- public/js/cat_source/es6/api/checkSplitRequest/checkSplitRequest.js
+++ public/js/cat_source/es6/api/checkSplitRequest/checkSplitRequest.js
@@ -8,7 +8,7 @@ export const checkSplitRequest = async (
   arrayValues,
   splitRawWords,
 ) => {
-  const params = {
+  const params = flattenObject({
     exec: 'check',
     project_id: project.id,
     project_pass: project.password,
@@ -17,7 +17,7 @@ export const checkSplitRequest = async (
     num_split: numsplit,
     split_values: arrayValues,
     split_raw_words: splitRawWords,
-  }
+  })
   const formData = new FormData()
 
   Object.keys(params).forEach((key) => {
