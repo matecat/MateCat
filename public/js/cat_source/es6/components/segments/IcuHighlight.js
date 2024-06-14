500cd4182c278508bc69c76460c8494d5b643356
riccio82 <riccio82@gmail.com>
2024-06-14 16:00:42 +0200

    ICU syntax: show errors

--- public/js/cat_source/es6/components/segments/IcuHighlight.js
+++ public/js/cat_source/es6/components/segments/IcuHighlight.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import Tooltip from '../common/Tooltip'
 
 export const IcuHighlight = ({start, end, tokens, children}) => {
   const token = tokens.find((item) => item.start === start && item.end === end)
+  const refToken = useRef()
   return (
     <div
-      className={`icuItem ${token && token.type === 'error' ? 'error' : ''}`}
+      className={`icuItem ${token && token.type === 'error' ? 'icuItem-error' : ''}`}
     >
-      <span>{children}</span>
+      {token.type === 'error' ? (
+        <Tooltip
+          content={
+            <div className="icu-tooltip">
+              <h3>ICU syntax error</h3>
+              <p>{token.message}</p>
+            </div>
+          }
+        >
+          <span ref={refToken}>{children}</span>
+        </Tooltip>
+      ) : (
+        <span>{children}</span>
+      )}
     </div>
   )
 }
