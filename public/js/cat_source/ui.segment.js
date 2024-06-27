48ee78d8eb23f27f9ef51adb8f71d78efdf3d26f
riccio82 <riccio82@gmail.com>
2024-06-27 15:30:54 +0200

    Fix merge

--- public/js/cat_source/ui.segment.js
+++
@@ -1,7 +0,0 @@
-import SegmentActions from './es6/actions/SegmentActions'
-import SegmentStore from './es6/stores/SegmentStore'
-import {getTagProjection} from './es6/api/getTagProjection'
-import {setCurrentSegment} from './es6/api/setCurrentSegment'
-;(function ($) {
-  $.extend(window.UI, {})
-})(jQuery)
