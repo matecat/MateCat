3f251de7d4c9d801c633f5853b080ca2d6fa9ad8
pierluigi.dicianni <pierluigi.dicianni@translated.net>
2024-06-28 12:39:31 +0200

    Fix lexiqa warnings

--- public/js/cat_source/es6/components/segments/utils/DraftMatecatUtils/tagModel.js
+++ public/js/cat_source/es6/components/segments/utils/DraftMatecatUtils/tagModel.js
@@ -223,8 +223,7 @@ const tagSignaturesMap = {
     style: 'tag-selfclosed tag-space-placeholder',
     showTooltip: false,
     replaceForLexiqa: true,
-    lexiqaText: ' ',
-    convertToLexiqaIgnoreAnglesBrackets: true,
+    lexiqaText: '​',
   },
 }
 
