9de65050d992b509673cc8ec631cb8be89cf8fd6
domenico <domenico@translated.net>
2024-09-17 18:29:26 +0200

    Fixed UI crash on no user metadata

--- public/js/cat_source/es6/components/header/UserMenu.js
+++ public/js/cat_source/es6/components/header/UserMenu.js
@@ -20,7 +20,7 @@ export const UserMenu = () => {
 
   const loggedRender = () => {
     const {metadata, user} = userInfo
-    const avatarImg = metadata[`${metadata.oauth_provider}_picture`] ?? null
+    const avatarImg = metadata ? (metadata[`${metadata.oauth_provider}_picture`] ?? null) : null
 
     const openManage = () => {
       document.location.href = '/manage'
