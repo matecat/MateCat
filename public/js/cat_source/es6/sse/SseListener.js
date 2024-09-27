cb7e30a43f34cd5fbb75481e7770752725df85b0
domenico <domenico@translated.net>
2024-09-27 15:52:10 +0200

    Fix sse route

--- public/js/cat_source/es6/sse/SseListener.js
+++ public/js/cat_source/es6/sse/SseListener.js
@@ -156,8 +156,8 @@ const SseListener = ({isAuthenticated, userId}) => {
     },
   }
   const getSource = function () {
-    let source =
-      '/channel/updates' +
+    let source = config.sse_base_url +
+        '/channel/updates' +
       '?jid=' +
       config.id_job +
       '&pw=' +
@@ -166,13 +166,9 @@ const SseListener = ({isAuthenticated, userId}) => {
       userId
 
     if (config.enableMultiDomainApi) {
-      source =
-        '//' +
-        Math.floor(Math.random() * config.ajaxDomainsNumber) +
-        '.ajax.' +
-        config.sse_base_url +
-        source
+      source = '//' + Math.floor(Math.random() * config.ajaxDomainsNumber) + '.ajax.' + source
     }
+
     return source
   }
   const {connectionState, connectionError} = useSse(
