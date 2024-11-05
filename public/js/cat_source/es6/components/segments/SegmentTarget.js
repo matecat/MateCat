c7d000592a15b2b85acb914b8a3ae35d8b94bc90
riccio82 <riccio82@gmail.com>
2024-11-05 16:12:26 +0100

    Css refactoring: style.scss

--- public/js/cat_source/es6/components/segments/SegmentTarget.js
+++ public/js/cat_source/es6/components/segments/SegmentTarget.js
@@ -170,7 +170,6 @@ class SegmentTarget extends React.Component {
         )
       }
 
-      // Todo: aggiungere la classe 'hasTagsAutofill' alla <section> del segmento permetteva al tasto di mostrarsi riga 3844 del file style.scss
       if (
         segment.missingTagsInTarget &&
         segment.missingTagsInTarget.length > 0 &&
