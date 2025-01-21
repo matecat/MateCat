3a5a141aee4019cdbd896423f5814bbb74494af8
Mauro Cassani <maurocassani1978@gmail.com>
2025-01-21 12:18:06 +0100

    Introduced `character_counter_mode` and `character_counter_count_tags` in project templates

--- lib/Utils/ProjectManager.php
+++ lib/Utils/ProjectManager.php
@@ -198,6 +198,8 @@ class ProjectManager {
                             'dictation'                               => null,
                             'show_whitespace'                         => null,
                             'character_counter'                       => null,
+                            'character_counter_mode'                  => null,
+                            'character_counter_count_tags'            => null,
                             'ai_assistant'                            => null,
                             'filters_extraction_parameters'           => new RecursiveArrayObject(),
                             'xliff_parameters'                        => new RecursiveArrayObject(),
@@ -392,22 +394,22 @@ class ProjectManager {
             $options[ 'from_api' ] = 1;
         }
 
-        // dictation
+        // dictation (LEGACY CODE TO BE REMOVED)
         if ( isset( $this->projectStructure[ 'dictation' ] ) and $this->projectStructure[ 'dictation' ] !== null ) {
             $options[ 'dictation' ] = $this->projectStructure[ 'dictation' ] == true ? 1 : 0;
         }
 
-        // show_whitespace
+        // show_whitespace (LEGACY CODE TO BE REMOVED)
         if ( isset( $this->projectStructure[ 'show_whitespace' ] ) and $this->projectStructure[ 'show_whitespace' ] !== null ) {
             $options[ 'show_whitespace' ] = $this->projectStructure[ 'show_whitespace' ] == true ? 1 : 0;
         }
 
-        // character_counter
+        // character_counter (LEGACY CODE TO BE REMOVED)
         if ( isset( $this->projectStructure[ 'character_counter' ] ) and $this->projectStructure[ 'character_counter' ] !== null ) {
             $options[ 'character_counter' ] = $this->projectStructure[ 'character_counter' ] == true ? 1 : 0;
         }
 
-        // ai_assistant
+        // ai_assistant (LEGACY CODE TO BE REMOVED)
         if ( isset( $this->projectStructure[ 'ai_assistant' ] ) and $this->projectStructure[ 'ai_assistant' ] !== null ) {
             $options[ 'ai_assistant' ] = $this->projectStructure[ 'ai_assistant' ] == true ? 1 : 0;
         }
@@ -1391,6 +1393,16 @@ class ProjectManager {
             $projectStructure[ 'array_jobs' ][ 'payable_rates' ]->offsetSet( $newJob->id, $payableRates );
 
             $jobsMetadataDao = new Jobs\MetadataDao();
+
+            // character_counter_count_tags
+            if ( isset( $projectStructure[ 'character_counter_count_tags' ] ) ) {
+                $jobsMetadataDao->set( $newJob->id, $newJob->password, 'character_counter_count_tags', ($projectStructure[ 'character_counter_count_tags' ] == true ? "1" : "0") );
+            }
+
+            // character_counter_mode
+            if ( isset( $projectStructure[ 'character_counter_mode' ] ) ) {
+                $jobsMetadataDao->set( $newJob->id, $newJob->password, 'character_counter_mode', $projectStructure[ 'character_counter_mode' ] );
+            }
 
             // tm_prioritization
             if ( isset( $projectStructure[ 'tm_prioritization' ] ) ) {
