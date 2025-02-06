eb8e89865e36bfbe0708f98acf445578c6745ba3
Mauro Cassani <maurocassani1978@gmail.com>
2025-02-06 12:08:06 +0100

    Redis fix

--- lib/Utils/GlobalMessages/GlobalMessagesPublisher.php
+++ lib/Utils/GlobalMessages/GlobalMessagesPublisher.php
@@ -3,18 +3,11 @@
 namespace GlobalMessages;
 
 use AMQHandler;
-use DateTime;
-use DateTimeInterface;
-use Exception;
 use INIT;
-use RedisHandler;
 use Stomp\Transport\Message;
 
 class GlobalMessagesPublisher
 {
-    const GLOBAL_MESSAGES_LIST_KEY = 'global_message_list_ids';
-    const GLOBAL_MESSAGES_ELEMENT_KEY = 'global_message_list_element_';
-
     /**
      * @param AMQHandler $queueHandler
      * @param array $message
