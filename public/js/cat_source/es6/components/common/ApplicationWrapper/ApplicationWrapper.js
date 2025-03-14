905928b5c85e26c5a3cf7a76d65ecf58446fa638
riccio82 <riccio82@gmail.com>
2025-03-14 17:06:27 +0100

    Upload refactoring: fix test

--- public/js/cat_source/es6/components/common/ApplicationWrapper/ApplicationWrapper.js
+++ public/js/cat_source/es6/components/common/ApplicationWrapper/ApplicationWrapper.js
@@ -8,24 +8,6 @@ import UserConstants from '../../../constants/UserConstants'
 import UserStore from '../../../stores/UserStore'
 import {ApplicationWrapperContext} from './ApplicationWrapperContext'
 
-// Custom event handler class: allows namespaced events
-class EventHandlerClass {
-  constructor() {
-    this.functionMap = {}
-  }
-
-  addEventListener(event, func) {
-    this.functionMap[event] = func
-    document.addEventListener(event.split('.')[0], this.functionMap[event])
-  }
-
-  removeEventListener(event) {
-    document.removeEventListener(event.split('.')[0], this.functionMap[event])
-    delete this.functionMap[event]
-  }
-}
-window.eventHandler = new EventHandlerClass()
-
 export const ApplicationWrapper = ({children}) => {
   const {
     isUserLogged,
