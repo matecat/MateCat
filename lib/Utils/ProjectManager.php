ea18aabdf0b79f331963f208384dc4e8ae3bac8d
Mauro Cassani <maurocassani1978@gmail.com>
2025-03-17 17:23:40 +0100

    removed log

--- lib/Utils/ProjectManager.php
+++ lib/Utils/ProjectManager.php
@@ -1186,8 +1186,6 @@ class ProjectManager {
 
                 if ( 'tmx' == $ext ) {
 
-                    \Log::doJsonLog("PIPPO ----> " . $this->projectStructure[ 'private_tm_key' ][ 0 ][ 'key' ]);
-
                     $file = new TMSFile(
                             "$this->uploadDir/$fileName",
                             $this->projectStructure[ 'private_tm_key' ][ 0 ][ 'key' ],
