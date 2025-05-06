56178cd4f170beb9a34ae9a0435a52a9e399c93f
riccio82 <riccio82@gmail.com>
2025-05-06 17:39:03 +0200

    UI support to mtqe analysis workflow - wip

--- public/js/cat_source/es6/components/analyze/ChunkAnalyzeHeader.js
+++ public/js/cat_source/es6/components/analyze/ChunkAnalyzeHeader.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ChevronDown from '../../../../../img/icons/ChevronDown'
+import {ANALYSIS_WORKFLOW_TYPES} from '../../constants/Constants'
 
 const ChunkAnalyzeHeader = ({
   total,
@@ -9,13 +10,14 @@ const ChunkAnalyzeHeader = ({
   jobInfo,
   chunksSize,
   rates,
+  workflowType,
 }) => {
-  return (
+  return workflowType === ANALYSIS_WORKFLOW_TYPES.STANDARD ? (
     <div
       className={`chunk-analyze-info ${
         rates.ICE_MT &&
         rates.ICE_MT !== rates.MT &&
-        total.find((item) => item.type === 'ice_MT').raw > 0
+        total.find((item) => item.type === 'ice_mt').raw > 0
           ? 'more-columns'
           : ''
       }`}
@@ -53,28 +55,30 @@ const ChunkAnalyzeHeader = ({
         <div>{total.find((item) => item.type === 'internal').equivalent}</div>
       </div>
       <div>
-        <div>{total.find((item) => item.type === '50_74').raw}</div>
-        <div>{total.find((item) => item.type === '50_74').equivalent}</div>
+        <div>{total.find((item) => item.type === 'tm_50_74').raw}</div>
+        <div>{total.find((item) => item.type === 'tm_50_74').equivalent}</div>
       </div>
       <div>
-        <div>{total.find((item) => item.type === '75_84').raw}</div>
-        <div>{total.find((item) => item.type === '75_84').equivalent}</div>
+        <div>{total.find((item) => item.type === 'tm_75_84').raw}</div>
+        <div>{total.find((item) => item.type === 'tm_75_84').equivalent}</div>
       </div>
       <div>
-        <div>{total.find((item) => item.type === '85_94').raw}</div>
-        <div>{total.find((item) => item.type === '85_94').equivalent}</div>
+        <div>{total.find((item) => item.type === 'tm_85_94').raw}</div>
+        <div>{total.find((item) => item.type === 'tm_85_94').equivalent}</div>
       </div>
       <div>
-        <div>{total.find((item) => item.type === '95_99').raw}</div>
-        <div>{total.find((item) => item.type === '95_99').equivalent}</div>
+        <div>{total.find((item) => item.type === 'tm_95_99').raw}</div>
+        <div>{total.find((item) => item.type === 'tm_95_99').equivalent}</div>
       </div>
       <div>
-        <div>{total.find((item) => item.type === '100').raw}</div>
-        <div>{total.find((item) => item.type === '100').equivalent}</div>
+        <div>{total.find((item) => item.type === 'tm_100').raw}</div>
+        <div>{total.find((item) => item.type === 'tm_100').equivalent}</div>
       </div>
       <div>
-        <div>{total.find((item) => item.type === '100_public').raw}</div>
-        <div>{total.find((item) => item.type === '100_public').equivalent}</div>
+        <div>{total.find((item) => item.type === 'tm_100_public').raw}</div>
+        <div>
+          {total.find((item) => item.type === 'tm_100_public').equivalent}
+        </div>
       </div>
       <div>
         <div>{total.find((item) => item.type === 'ice').raw}</div>
@@ -85,11 +89,11 @@ const ChunkAnalyzeHeader = ({
           <>
             <div>
               {total.find((item) => item.type === 'MT').raw +
-                total.find((item) => item.type === 'ice_MT').raw}
+                total.find((item) => item.type === 'ice_mt').raw}
             </div>
             <div>
               {total.find((item) => item.type === 'MT').equivalent +
-                total.find((item) => item.type === 'ice_MT').equivalent}
+                total.find((item) => item.type === 'ice_mt').equivalent}
             </div>
           </>
         ) : (
@@ -101,10 +105,10 @@ const ChunkAnalyzeHeader = ({
       </div>
       {rates.ICE_MT &&
       rates.ICE_MT !== rates.MT &&
-      total.find((item) => item.type === 'ice_MT').raw > 0 ? (
+      total.find((item) => item.type === 'ice_mt').raw > 0 ? (
         <div>
-          <div>{total.find((item) => item.type === 'ice_MT').raw}</div>
-          <div>{total.find((item) => item.type === 'ice_MT').equivalent}</div>
+          <div>{total.find((item) => item.type === 'ice_mt').raw}</div>
+          <div>{total.find((item) => item.type === 'ice_mt').equivalent}</div>
         </div>
       ) : null}
       <div className={'chunk-analyze-info-total'}>
@@ -112,68 +116,76 @@ const ChunkAnalyzeHeader = ({
         <div>{jobInfo.total_equivalent}</div>
       </div>
     </div>
-  )
-
-  /*return (
-      <div className="chunk sixteen wide column pad-right-10">
-        <div className="left-box">
-          {id}
-          <div className="file-details" onClick={showFiles}>
-            File <span className="details">details </span>
+  ) : workflowType === ANALYSIS_WORKFLOW_TYPES.MTQE ? (
+    <div className={`chunk-analyze-info`}>
+      {showFiles && <div className={`chunk-analyze-info-background`} />}
+      <div>
+        <div className={`chunk-analyze-info-header ${showFiles ? 'open' : ''}`}>
+          <div>
+            <span className={'chunk-analyze-info-index'}>
+              {chunksSize > 1 ? '#' + index : ''}
+            </span>
+            <span>Raw</span>
           </div>
-          <div className="f-details-number">({size(jobInfo.files)})</div>
-        </div>
-        <div className="single-analysis">
-          <div className="single total">{jobInfo.total_raw}</div>
-
-          <div className="single payable-words">{jobInfo.total_equivalent}</div>
-
-          <div className="single new">
-            {total.find((item) => item.type === 'new').raw}
-          </div>
-
-          <div className="single repetition">
-            {total.find((item) => item.type === 'repetitions').raw}
-          </div>
-
-          <div className="single internal-matches">
-            {total.find((item) => item.type === 'internal').raw}
-          </div>
-
-          <div className="single p-50-74">
-            {total.find((item) => item.type === '50_74').raw}
-          </div>
-
-          <div className="single p-75-84">
-            {total.find((item) => item.type === '75_84').raw}
-          </div>
-
-          <div className="single p-85-94">
-            {total.find((item) => item.type === '85_94').raw}
-          </div>
-
-          <div className="single p-95-99">
-            {total.find((item) => item.type === '95_99').raw}
-          </div>
-
-          <div className="single tm-100">
-            {total.find((item) => item.type === '100').raw}
-          </div>
-
-          <div className="single tm-public">
-            {total.find((item) => item.type === '100_public').raw}
-          </div>
-
-          <div className="single tm-context">
-            {total.find((item) => item.type === 'ice').raw}
-          </div>
-
-          <div className="single machine-translation">
-            {total.find((item) => item.type === 'MT').raw}
+          <div>
+            <div className={'chunk-analyze-info-files'} onClick={showFilesFn}>
+              <ChevronDown size={10} />
+              File ({jobInfo.files.length})
+            </div>
+            <span>Weighted</span>
           </div>
         </div>
       </div>
-    )*/
+      <div>
+        <div>{total.find((item) => item.type === 'ice').raw}</div>
+        <div>{total.find((item) => item.type === 'ice').equivalent}</div>
+      </div>
+      <div>
+        <div>{total.find((item) => item.type === 'tm_100').raw}</div>
+        <div>{total.find((item) => item.type === 'tm_100').equivalent}</div>
+      </div>
+      <div>
+        <div>{total.find((item) => item.type === 'tm_100_public').raw}</div>
+        <div>
+          {total.find((item) => item.type === 'tm_100_public').equivalent}
+        </div>
+      </div>
+      <div>
+        <div>{total.find((item) => item.type === 'repetitions').raw}</div>
+        <div>
+          {total.find((item) => item.type === 'repetitions').equivalent}
+        </div>
+      </div>
+      <div>
+        <div>{total.find((item) => item.type === 'ice_mt').raw}</div>
+        <div>{total.find((item) => item.type === 'ice_mt').equivalent}</div>
+      </div>
+      <div>
+        <div>{total.find((item) => item.type === 'top_quality_mt').raw}</div>
+        <div>
+          {total.find((item) => item.type === 'top_quality_mt').equivalent}
+        </div>
+      </div>
+      <div>
+        <div>{total.find((item) => item.type === 'higher_quality_mt').raw}</div>
+        <div>
+          {total.find((item) => item.type === 'higher_quality_mt').equivalent}
+        </div>
+      </div>
+      <div>
+        <div>
+          {total.find((item) => item.type === 'standard_quality_mt').raw}
+        </div>
+        <div>
+          {total.find((item) => item.type === 'standard_quality_mt').equivalent}
+        </div>
+      </div>
+      <div className={'chunk-analyze-info-total'}>
+        <div>{jobInfo.total_raw}</div>
+        <div>{jobInfo.total_equivalent}</div>
+      </div>
+    </div>
+  ) : null
 }
 
 export default ChunkAnalyzeHeader
