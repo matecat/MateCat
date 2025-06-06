e60e1c825d441a05564085b21fa63e678fbcdf46
domenico <domenico@translated.net>
2025-06-06 19:55:39 +0200

    Test Fixed

--- tests/unit/Model/ConvertFileModelTest.php
+++ tests/unit/Model/ConvertFileModelTest.php
@@ -6,28 +6,31 @@ use TestHelpers\AbstractTest;
 
 class ConvertFileModelTest extends AbstractTest {
 
+    /**
+     * @throws Exception
+     */
     function test_model() {
         $model = new ConvertedFileModel();
-        $this->assertEquals( $model->getCode(), ConversionHandlerStatus::NOT_CONVERTED );
+        $this->assertEquals( ConversionHandlerStatus::NOT_CONVERTED, $model->getCode() );
         $this->assertFalse( $model->hasErrors() );
 
         $model->changeCode( ConversionHandlerStatus::OK );
-        $this->assertEquals( $model->getCode(), ConversionHandlerStatus::OK );
+        $this->assertEquals( ConversionHandlerStatus::OK, $model->getCode() );
 
         try {
             $model->changeCode( 43434343 );
-        } catch ( \Exception $exception ) {
-            $this->assertEquals( $exception->getMessage(), '43434343 is not a valid code' );
+        } catch ( Exception $exception ) {
+            $this->assertEquals( '43434343 is not a valid code', $exception->getMessage() );
         }
 
         $model->changeCode( ConversionHandlerStatus::SOURCE_ERROR );
         $model->addError( 'Source not valid' );
 
-        $this->assertCount( 1, $model->getMessage() );
+        $this->assertEquals( 'Source not valid', $model->getMessage() );
         $this->assertTrue( $model->hasErrors() );
 
         $json = json_encode( $model );
 
-        $this->assertEquals( '{"code":-3,"errors":[{"code":-3,"message":"Source not valid","debug":null}],"warnings":[],"data":[]}', $json );
+        $this->assertEquals( '{"code":-3,"message":"Source not valid","warning":null}', $json );
     }
 }
