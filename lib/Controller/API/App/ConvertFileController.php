b3847f879fbe8c4ab03592de48e9a7cf8e981b99
Mauro Cassani <maurocassani1978@gmail.com>
2025-06-27 16:08:41 +0200

    Added `filters_extraction_parameters_template` param to api/app/convert-file endpoint

--- lib/Controller/API/App/ConvertFileController.php
+++ lib/Controller/API/App/ConvertFileController.php
@@ -24,6 +24,8 @@ use stdClass;
 use TaskRunner\Exceptions\EndQueueException;
 use TaskRunner\Exceptions\ReQueueException;
 use Utils;
+use Validator\JSONValidator;
+use Validator\JSONValidatorObject;
 use ZipArchiveExtended;
 
 class ConvertFileController extends KleinController {
@@ -85,13 +87,13 @@ class ConvertFileController extends KleinController {
      * @throws Exception
      */
     private function validateTheRequest(): array {
-        $file_name                     = filter_var( $this->request->param( 'file_name' ), FILTER_SANITIZE_STRING, [ 'flags' => FILTER_FLAG_STRIP_LOW ] );
-        $source_lang                   = filter_var( $this->request->param( 'source_lang' ), FILTER_SANITIZE_STRING, [ 'flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH ] );
-        $target_lang                   = filter_var( $this->request->param( 'target_lang' ), FILTER_SANITIZE_STRING, [ 'flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH ] );
-        $segmentation_rule             = filter_var( $this->request->param( 'segmentation_rule' ), FILTER_SANITIZE_STRING, [ 'flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH ] );
-        $filters_extraction_parameters = filter_var( $this->request->param( 'filters_extraction_parameters' ), FILTER_SANITIZE_STRING, [ 'flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH ] );
+        $file_name                                 = filter_var( $this->request->param( 'file_name' ), FILTER_SANITIZE_STRING, [ 'flags' => FILTER_FLAG_STRIP_LOW ] );
+        $source_lang                               = filter_var( $this->request->param( 'source_lang' ), FILTER_SANITIZE_STRING, [ 'flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH ] );
+        $target_lang                               = filter_var( $this->request->param( 'target_lang' ), FILTER_SANITIZE_STRING, [ 'flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH ] );
+        $segmentation_rule                         = filter_var( $this->request->param( 'segmentation_rule' ), FILTER_SANITIZE_STRING, [ 'flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH ] );
+        $filters_extraction_parameters             = filter_var( $this->request->param( 'filters_extraction_parameters' ), FILTER_SANITIZE_STRING, [ 'flags' => FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH ] );
         $filters_extraction_parameters_template_id = filter_var( $this->request->param( 'filters_extraction_parameters_template_id' ), FILTER_SANITIZE_NUMBER_INT );
-        $restarted_conversion          = filter_var( $this->request->param( 'restarted_conversion' ), FILTER_VALIDATE_BOOLEAN );
+        $restarted_conversion                      = filter_var( $this->request->param( 'restarted_conversion' ), FILTER_VALIDATE_BOOLEAN );
 
         if ( empty( $file_name ) ) {
             throw new InvalidArgumentException( "Missing file name." );
@@ -114,7 +116,7 @@ class ConvertFileController extends KleinController {
         }
 
         $segmentation_rule             = Constants::validateSegmentationRules( $segmentation_rule );
-        $filters_extraction_parameters = $this->validateFiltersExtractionParametersTemplateId( $filters_extraction_parameters_template_id );
+        $filters_extraction_parameters = $this->validateFiltersExtractionParametersTemplateId( $filters_extraction_parameters, $filters_extraction_parameters_template_id );
         $source_lang                   = $this->validateSourceLang( $source_lang );
         $target_lang                   = $this->validateTargetLangs( $target_lang );
 
@@ -154,15 +156,27 @@ class ConvertFileController extends KleinController {
     }
 
     /**
+     * @param null $filters_extraction_parameters_template
      * @param null $filters_extraction_parameters_template_id
      *
      * @return FiltersConfigTemplateStruct|null
      * @throws ReflectionException
      * @throws Exception
      */
-    private function validateFiltersExtractionParametersTemplateId( $filters_extraction_parameters_template_id = null ): ?FiltersConfigTemplateStruct {
+    private function validateFiltersExtractionParametersTemplateId( $filters_extraction_parameters_template = null, $filters_extraction_parameters_template_id = null ): ?FiltersConfigTemplateStruct {
 
-        if ( empty( $filters_extraction_parameters_template_id ) ) {
+        if ( !empty( $filters_extraction_parameters_template ) ) {
+            $json   = html_entity_decode( $filters_extraction_parameters_template );
+            $schema = file_get_contents( INIT::$ROOT . '/inc/validation/schema/filters_extraction_parameters.json' );
+
+            $validatorObject       = new JSONValidatorObject();
+            $validatorObject->json = $json;
+
+            $validator = new JSONValidator( $schema, true );
+            $validator->validate( $validatorObject );
+
+            return FiltersConfigTemplateDao::createFromJSON( $validatorObject->decoded, $this->getUser()->uid );
+        } elseif ( empty( $filters_extraction_parameters_template_id ) ) {
             return null;
         }
 
