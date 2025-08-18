72e4a37f194d2cc438cbf9b5f539a28aab08e0f3
domenico <domenico@translated.net>
2025-08-18 16:17:37 +0200

    Fixed "Could not call class constructor"

--- lib/Model/Segments/SegmentOriginalDataStruct.php
+++ lib/Model/Segments/SegmentOriginalDataStruct.php
@@ -7,7 +7,24 @@ use Model\DataAccess\IDaoStruct;
 
 class SegmentOriginalDataStruct extends AbstractDaoSilentStruct implements IDaoStruct {
 
-    public ?int  $id = null;
-    public int   $id_segment;
-    public array $map;
+    public ?int      $id          = null;
+    public int       $id_segment;
+    protected string $map;
+    protected array  $decoded_map = [];
+
+    public function setMap( array $map ): SegmentOriginalDataStruct {
+        $this->decoded_map = $map;
+        $this->map         = json_encode( $map );
+
+        return $this;
+    }
+
+    public function getMap(): array {
+        if ( empty( $this->decoded_map ) ) {
+            $this->decoded_map = json_decode( $this->map, true ) ?: [];
+        }
+
+        return $this->decoded_map;
+    }
+
 }
