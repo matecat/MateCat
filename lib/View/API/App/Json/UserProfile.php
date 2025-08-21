2d08055afdbb052ebf4a4eec03fe93de7adc5da8
domenico <domenico@translated.net>
2025-08-21 12:49:46 +0200

    Fix crash on teams/members api

--- lib/View/API/App/Json/UserProfile.php
+++ lib/View/API/App/Json/UserProfile.php
@@ -9,9 +9,11 @@
 
 namespace View\API\App\Json;
 
+use Defuse\Crypto\Exception\EnvironmentIsBrokenException;
 use Model\ConnectedServices\ConnectedServiceStruct;
 use Model\Teams\TeamStruct;
 use Model\Users\UserStruct;
+use ReflectionException;
 use View\API\V2\Json\Team;
 use View\API\V2\Json\User;
 
@@ -24,8 +26,10 @@ class UserProfile {
      * @param array                         $userMetadata
      *
      * @return array
+     * @throws ReflectionException
+     * @throws EnvironmentIsBrokenException
      */
-    public function renderItem( UserStruct $user, array $teams, array $servicesStruct = null, array $userMetadata = [] ) {
+    public function renderItem( UserStruct $user, array $teams, array $servicesStruct = null, array $userMetadata = [] ): array {
 
         return [
                 'user'               => User::renderItem( $user ),
