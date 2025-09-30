022bb04505fc36ca9366ffa4111fa51829a6fd8d
domenico <domenico@translated.net>
2025-09-30 17:48:01 +0200

    Updated migration file

--- migrations/20250926143100_add_subfiltering_to_project_templates.php
+++ migrations/20250926143100_add_subfiltering_to_project_templates.php
@@ -5,7 +5,7 @@ use migrations\AbstractMatecatMigration;
 class AddSubfilteringToProjectTemplates extends AbstractMatecatMigration {
 
     public $sql_up = [
-        'ALTER TABLE `project_templates` add column `subfiltering_handlers` varchar(50) DEFAULT NULL;',
+        "ALTER TABLE `project_templates` add column `subfiltering_handlers` varchar(50) DEFAULT '[]', ALGORITHM=INPLACE, LOCK=NONE;",
     ];
 
     
