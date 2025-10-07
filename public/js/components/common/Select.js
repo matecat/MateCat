4853ae28d132016f8efeadc062df13bde9832fcf
piedicianni <53812578+piedicianni@users.noreply.github.com>
2025-10-07 15:39:11 +0200

    Settings panel configurations template (#4064)
    * Settings panel configurations template
    
    * Improvements
    
    * Fix

--- public/js/components/common/Select.js
+++ public/js/components/common/Select.js
@@ -58,6 +58,7 @@ export const Select = ({
   dropdownClassName,
   showResetButton = false,
   resetFunction = () => {},
+  tooltipContent,
   children,
 }) => {
   const dropDownRef = useRef()
@@ -327,7 +328,9 @@ export const Select = ({
         content={
           TEXT_UTILS.isContentTextEllipsis(selectedItemRef?.current?.firstChild)
             ? selectedLabel
-            : ''
+            : tooltipContent
+              ? tooltipContent
+              : ''
         }
       >
         <div ref={selectedItemRef} className="select-with-icon__wrapper">
@@ -404,5 +407,6 @@ Select.propTypes = {
   dropdownClassName: PropTypes.string,
   showResetButton: PropTypes.bool,
   resetFunction: PropTypes.func,
+  tooltipContent: PropTypes.string,
   children: PropTypes.func,
 }
--- public/js/pages/NewProject.js
+++ public/js/pages/NewProject.js
@@ -119,6 +119,35 @@ const NewProject = () => {
     XLIFF_SETTINGS_SCHEMA_KEYS,
   )
 
+  const subtemplatesNotSaved = useMemo(
+    () => [
+      ...(qualityFrameworkTemplates.templates.some(
+        ({isTemporary}) => isTemporary,
+      )
+        ? [SCHEMA_KEYS.qaModelTemplateId]
+        : []),
+      ...(analysisTemplates.templates.some(({isTemporary}) => isTemporary)
+        ? [SCHEMA_KEYS.payableRateTemplateId]
+        : []),
+      ...(fileImportFiltersParamsTemplates.templates.some(
+        ({isTemporary}) => isTemporary,
+      )
+        ? [SCHEMA_KEYS.filtersTemplateId]
+        : []),
+      ...(fileImportXliffSettingsTemplates.templates.some(
+        ({isTemporary}) => isTemporary,
+      )
+        ? [SCHEMA_KEYS.XliffConfigTemplateId]
+        : []),
+    ],
+    [
+      analysisTemplates.templates,
+      fileImportFiltersParamsTemplates.templates,
+      fileImportXliffSettingsTemplates.templates,
+      qualityFrameworkTemplates.templates,
+    ],
+  )
+
   const isDeviceCompatible = useDeviceCompatibility()
 
   const {isUserLogged, userInfo} = useContext(ApplicationWrapperContext)
@@ -939,6 +968,7 @@ const NewProject = () => {
                   projectTemplates,
                   setProjectTemplates,
                   currentProjectTemplate,
+                  subtemplatesNotSaved,
                 }}
               />
             </div>
@@ -1138,6 +1168,7 @@ const NewProject = () => {
             analysisTemplates,
             fileImportFiltersParamsTemplates,
             fileImportXliffSettingsTemplates,
+            subtemplatesNotSaved,
           }}
         />
       )}
