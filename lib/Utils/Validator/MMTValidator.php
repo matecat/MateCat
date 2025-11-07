37913bb3f1a6c7d85cf549cdce3b03dd4734596b
Mauro Cassani <maurocassani1978@gmail.com>
2025-11-07 15:33:54 +0100

    MMT glossaries fix

--- lib/Utils/Validator/MMTValidator.php
+++ lib/Utils/Validator/MMTValidator.php
@@ -7,10 +7,8 @@ use Utils\Validator\Contracts\AbstractValidator;
 use Utils\Validator\Contracts\ValidatorObject;
 
 class MMTValidator extends AbstractValidator {
+
     /**
-     * Example:
-     * {"glossaries": [1, 2, 3, 4], "ignore_glossary_case": true }
-     *
      * @param ValidatorObject $object
      *
      * @return ValidatorObject|null
@@ -24,19 +22,7 @@ class MMTValidator extends AbstractValidator {
             throw new Exception( "mmt_glossaries is not a valid JSON" );
         }
 
-        if ( !isset( $mmtGlossariesArray[ 'ignore_glossary_case' ] ) ) {
-            throw new Exception( "`ignore_glossary_case` key not found in `mmt_glossaries` JSON" );
-        }
-
-        if ( !is_bool( $mmtGlossariesArray[ 'ignore_glossary_case' ] ) ) {
-            throw new Exception( "`ignore_glossary_case` is not boolean in `mmt_glossaries` JSON" );
-        }
-
-        if ( !is_array( $mmtGlossariesArray[ 'glossaries' ] ) ) {
-            throw new Exception( "`glossaries` is not an array in `mmt_glossaries` JSON" );
-        }
-
-        foreach ( $mmtGlossariesArray[ 'glossaries' ] as $glossaryId ) {
+        foreach ( $mmtGlossariesArray as $glossaryId ) {
             if ( !is_int( $glossaryId ) ) {
                 throw new Exception( "`glossaries` array contains a non integer value in `mmt_glossaries` JSON" );
             }
