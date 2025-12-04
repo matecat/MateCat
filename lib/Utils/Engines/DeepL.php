d8e3dbff9c34d885635e3a1ec8bc92a864f9ace6
domenico <domenico@translated.net>
2025-12-04 13:38:16 +0100

    Fixed DeepL key validation bug

--- lib/Utils/Engines/DeepL.php
+++ lib/Utils/Engines/DeepL.php
@@ -39,106 +39,120 @@ class DeepL extends AbstractEngine {
     }
 
     /**
-     * @param       $rawValue
+     * @param mixed $rawValue
      * @param array $parameters
-     * @param null  $function
+     * @param null $function
      *
      * @return MTResponse[]
      * @throws Exception
      */
-    protected function _decode( $rawValue, array $parameters = [], $function = null ): array {
-        $rawValue    = json_decode( $rawValue, true );
-        $translation = $rawValue[ 'translations' ][ 0 ][ 'text' ];
-        $translation = html_entity_decode( $translation, ENT_QUOTES | 16 );
-        $source      = $parameters[ 'source_lang' ];
-        $target      = $parameters[ 'target_lang' ];
-        $segment     = $parameters[ 'text' ][ 0 ];
+    protected function _decode($rawValue, array $parameters = [], $function = null): array
+    {
+        $rawValue = json_decode($rawValue, true);
 
-        return ( new Matches( [
-                'source'          => $source,
-                'target'          => $target,
-                'raw_segment'     => $segment,
-                'raw_translation' => $translation,
-                'match'           => "85%",
-                'created-by'      => $this->getMTName(),
-                'create-date'     => date( "Y-m-d" ),
-        ] ) )->getMatches( 1 );
+        if (($rawValue['responseStatus'] ?? 200) == 403) {
+            /*
+            [
+                'error' =>
+                    [
+                        'code' => 0,
+                        'message' => '  - Server Error (http status 403)',
+                        'response' => '{"message":"This account is not allowed to access the API. You can find more info in our docs: https://developers.deepl.com/docs/getting-started/auth"}',
+                    ],
+                'responseStatus' => 403,
+            ];
+            */
+            $error = json_decode($rawValue['error']['response'], true);
+            throw new Exception($error['message']);
+        }
+
+        $translation = $rawValue['translations'][0]['text'];
+        $translation = html_entity_decode($translation, ENT_QUOTES | 16);
+        $source = $parameters['source_lang'];
+        $target = $parameters['target_lang'];
+        $segment = $parameters['text'][0];
+
+        return (new Matches([
+            'source' => $source,
+            'target' => $target,
+            'raw_segment' => $segment,
+            'raw_translation' => $translation,
+            'match' => "85%",
+            'created-by' => $this->getMTName(),
+            'create-date' => date("Y-m-d"),
+        ]))->getMatches(1);
     }
 
     /**
      * @inheritDoc
      * @throws Exception
      */
-    public function get( array $_config ) {
+    public function get(array $_config) {
 
-        try {
-            $source = explode( "-", $_config[ 'source' ] );
-            $target = explode( "-", $_config[ 'target' ] );
+        $source = explode("-", $_config['source']);
+        $target = explode("-", $_config['target']);
 
-            $extraParams = $this->getEngineRecord()->extra_parameters;
+        $extraParams = $this->getEngineRecord()->extra_parameters;
 
-            if ( !isset( $extraParams[ 'DeepL-Auth-Key' ] ) ) {
-                throw new Exception( "DeepL API key not set" );
-            }
-
-            // glossaries (only for DeepL)
-            $metadataDao     = new MetadataDao();
-            // null coalescing operator is used to avoid errors when validating the engine for the first time
-            $deepLFormality  = $metadataDao->get( $_config[ 'pid' ], 'deepl_formality', 86400 );
-            $deepLIdGlossary = $metadataDao->get( $_config[ 'pid' ], 'deepl_id_glossary', 86400 );
-            $deepLEngineType = $metadataDao->get( $_config[ 'pid' ], 'deepl_engine_type', 86400 );
-
-            if ( $deepLEngineType !== null and in_array( $deepLEngineType->value, self::ALLOWED_MODEL_TYPES ) ) {
-                $_config[ 'model_type' ] = $deepLEngineType->value;
-            }
-
-            if ( $deepLFormality !== null ) {
-                $_config[ 'formality' ] = $deepLFormality->value;
-            }
-
-            if ( $deepLIdGlossary !== null ) {
-                $_config[ 'idGlossary' ] = $deepLIdGlossary->value;
-            }
-            // glossaries (only for DeepL)
-
-            $parameters = [
-                    'text'        => [
-                            $_config[ 'segment' ],
-                    ],
-                    'source_lang' => $source[ 0 ],
-                    'target_lang' => $target[ 0 ],
-                    'formality'   => ( $_config[ 'formality' ] ?: null ),
-                    'glossary_id' => ( $_config[ 'idGlossary' ] ?: null )
-            ];
-
-            if ( !empty( $_config[ 'model_type' ] ) ) {
-                $parameters[ 'model_type' ] = $_config[ 'model_type' ];
-            }
-
-            $headers = [
-                    'Authorization: DeepL-Auth-Key ' . $extraParams[ 'DeepL-Auth-Key' ],
-                    'Content-Type: application/json'
-            ];
-
-            $this->_setAdditionalCurlParams(
-                    [
-                            CURLOPT_POST           => true,
-                            CURLOPT_POSTFIELDS     => json_encode( $parameters ),
-                            CURLOPT_HTTPHEADER     => $headers,
-                            CURLOPT_RETURNTRANSFER => true,
-                            CURLOPT_HEADER         => false,
-                            CURLOPT_SSL_VERIFYPEER => true,
-                            CURLOPT_SSL_VERIFYHOST => 2
-                    ]
-            );
-
-            $this->call( "translate_relative_url", $parameters, true );
-
-            return $this->result;
-
-        } catch ( Exception $e ) {
-            return $this->GoogleTranslateFallback( $_config );
+        if (!isset($extraParams['DeepL-Auth-Key'])) {
+            throw new Exception("DeepL API key not set");
         }
+
+        // glossaries (only for DeepL)
+        $metadataDao = new MetadataDao();
+        // null coalescing operator is used to avoid errors when validating the engine for the first time
+        $deepLFormality = $metadataDao->get($_config['pid'], 'deepl_formality', 86400);
+        $deepLIdGlossary = $metadataDao->get($_config['pid'], 'deepl_id_glossary', 86400);
+        $deepLEngineType = $metadataDao->get($_config['pid'], 'deepl_engine_type', 86400);
+
+        if ($deepLEngineType !== null and in_array($deepLEngineType->value, self::ALLOWED_MODEL_TYPES)) {
+            $_config['model_type'] = $deepLEngineType->value;
+        }
+
+        if ($deepLFormality !== null) {
+            $_config['formality'] = $deepLFormality->value;
+        }
+
+        if ($deepLIdGlossary !== null) {
+            $_config['idGlossary'] = $deepLIdGlossary->value;
+        }
+        // glossaries (only for DeepL)
+
+        $parameters = [
+            'text' => [
+                $_config['segment'],
+            ],
+            'source_lang' => $source[0],
+            'target_lang' => $target[0],
+            'formality' => ($_config['formality'] ?: null),
+            'glossary_id' => ($_config['idGlossary'] ?: null)
+        ];
+
+        if (!empty($_config['model_type'])) {
+            $parameters['model_type'] = $_config['model_type'];
+        }
+
+        $headers = [
+            'Authorization: DeepL-Auth-Key ' . $extraParams['DeepL-Auth-Key'],
+            'Content-Type: application/json'
+        ];
+
+        $this->_setAdditionalCurlParams(
+            [
+                CURLOPT_POST => true,
+                CURLOPT_POSTFIELDS => json_encode($parameters),
+                CURLOPT_HTTPHEADER => $headers,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HEADER => false,
+                CURLOPT_SSL_VERIFYPEER => true,
+                CURLOPT_SSL_VERIFYHOST => 2
+            ]
+        );
+
+        $this->call("translate_relative_url", $parameters, true);
+
+        return $this->result;
+
     }
 
     /**
