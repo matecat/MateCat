ca7499b7a37e742c71b42cb836c5d1d999e24f7a
riccio82 <riccio82@gmail.com>
2025-12-15 17:59:02 +0100

    Lara-think: ui request

--- public/js/api/laraTranslate/laraTranslate.js
+++ public/js/api/laraTranslate/laraTranslate.js
@@ -1,6 +1,6 @@
 import {Translator} from '@translated/lara'
 
 export const laraTranslate = async ({token, source}) => {
-  const lara = new Translator({token: token})
+  const lara = new Translator({accessKey: token})
   return await lara.translate(source, config.target_rfc, config.source_rfc)
 }
