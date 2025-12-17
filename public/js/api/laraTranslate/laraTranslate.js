d7c0d800737bb06b97c8eaf120cb958dd0dbb7f7
domenico <domenico@translated.net>
2025-12-17 10:47:02 +0100

    Fixed lara call and Updated js dependency

--- public/js/api/laraTranslate/laraTranslate.js
+++ public/js/api/laraTranslate/laraTranslate.js
@@ -1,30 +1,38 @@
 import {AuthToken, Translator} from '@translated/lara'
 
 export const laraTranslate = async ({
-  token,
-  source,
-  contextListBefore,
-  contextListAfter,
-  sid,
-  jobId,
-  glossaries,
-}) => {
-  const credentials = new AuthToken(token, null)
+        token,
+        source,
+        contextListBefore,
+        contextListAfter,
+        sid,
+        jobId,
+        glossaries,
+    }) => {
+    const credentials = new AuthToken(token, null)
 
-  const lara = new Translator(credentials, {
-    multiline: false,
-    contentType: 'application/xliff+xml',
-    headers: {'X-Lara-Engine-Tuid': `${jobId}:${sid}`},
-    glossaries: glossaries,
-  })
-  let textBlocks = [
-    ...contextListBefore.map((item) => {
-      return {text: item, translatable: false}
-    }),
-    {text: source, translatable: true},
-    ...contextListAfter.map((item) => {
-      return {text: item, translatable: false}
-    }),
-  ]
-  return await lara.translate(textBlocks, config.source_rfc, config.target_rfc)
+    const lara = new Translator(credentials, {
+        connectionTimeoutMs: 10000
+    })
+    let textBlocks = [
+        ...contextListBefore.map((item) => {
+            return {text: item, translatable: false}
+        }),
+        {text: source, translatable: true},
+        ...contextListAfter.map((item) => {
+            return {text: item, translatable: false}
+        }),
+    ]
+    return await lara.translate(
+        textBlocks,
+        config.source_rfc,
+        config.target_rfc,
+        {
+            multiline: false,
+            contentType: 'application/xliff+xml',
+            headers: {'X-Lara-Engine-Tuid': `${jobId}:${sid}`},
+            glossaries: glossaries,
+            reasoning: true,
+        }
+    )
 }
