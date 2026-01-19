6f96eb990852b3d936cacb894d10b6e332044cbb
Mauro Cassani <maurocassani1978@gmail.com>
2026-01-19 14:57:59 +0100

    Edit issues (#4273)
    * Edit issue
    
    * Check user Id
    
    * storage files
    
    * missing htaccess
    
    * missing files
    
    * missing files
    
    * missing files
    
    * FE integration
    
    * Some improvements
    
    * plugins
    
    * plugins
    
    * Plugins
    
    * plugins
    
    * plugins
    
    ---------
    
    Co-authored-by: pierluigi.dicianni <pierluigi.dicianni@translated.net>

--- lib/Controller/API/V2/SegmentTranslationIssueController.php
+++ lib/Controller/API/V2/SegmentTranslationIssueController.php
@@ -3,11 +3,13 @@
 namespace Controller\API\V2;
 
 use Controller\Abstracts\AbstractStatefulKleinController;
+use Controller\API\Commons\Exceptions\AuthorizationError;
 use Controller\API\Commons\Validators\ChunkPasswordValidator;
 use Controller\API\Commons\Validators\LoginValidator;
 use Controller\API\Commons\Validators\SegmentTranslationIssueValidator;
 use Exception;
 use Model\DataAccess\Database;
+use Model\Exceptions\NotFoundException;
 use Model\Exceptions\ValidationError;
 use Model\LQA\EntryCommentDao;
 use Model\LQA\EntryDao as EntryDao;
@@ -17,16 +19,14 @@ use Plugins\Features\ReviewExtended\TranslationIssueModel;
 use View\API\V2\Json\SegmentTranslationIssue as TranslationIssueFormatter;
 use View\API\V2\Json\TranslationIssueComment;
 
-class SegmentTranslationIssueController extends AbstractStatefulKleinController
-{
+class SegmentTranslationIssueController extends AbstractStatefulKleinController {
 
     /**
      * @var SegmentTranslationIssueValidator
      */
     private SegmentTranslationIssueValidator $validator;
 
-    public function index(): void
-    {
+    public function index(): void {
         $result = EntryDao::findAllByTranslationVersion(
             $this->validator->translation->id_segment,
             $this->validator->translation->id_job,
@@ -34,85 +34,126 @@ class SegmentTranslationIssueController extends AbstractStatefulKleinController
         );
 
         $json = new TranslationIssueFormatter();
-        $rendered = $json->render($result);
+        $rendered = $json->render( $result );
 
-        $this->response->json(['issues' => $rendered]);
+        $this->response->json( [ 'issues' => $rendered ] );
     }
 
     /**
      * @throws ValidationError
      */
-    public function create(): void
-    {
+    public function create(): void {
         $data = [
-            'id_segment' => $this->request->param('id_segment'),
-            'id_job' => $this->request->param('id_job'),
-            'id_category' => $this->request->param('id_category'),
-            'severity' => $this->request->param('severity'),
+            'id_segment' => $this->request->param( 'id_segment' ),
+            'id_job' => $this->request->param( 'id_job' ),
+            'id_category' => $this->request->param( 'id_category' ),
+            'severity' => $this->request->param( 'severity' ),
             'translation_version' => $this->validator->translation->version_number,
-            'target_text' => $this->request->param('target_text'),
-            'start_node' => $this->request->param('start_node'),
-            'start_offset' => $this->request->param('start_offset'),
-            'end_node' => $this->request->param('end_node'),
-            'end_offset' => $this->request->param('end_offset'),
+            'target_text' => $this->request->param( 'target_text' ),
+            'start_node' => $this->request->param( 'start_node' ),
+            'start_offset' => $this->request->param( 'start_offset' ),
+            'end_node' => $this->request->param( 'end_node' ),
+            'end_offset' => $this->request->param( 'end_offset' ),
             'is_full_segment' => false,
-            'comment' => $this->request->param('comment'),
+            'comment' => $this->request->param( 'comment' ),
             'uid' => $this->user->uid ?? null,
-            'source_page' => ReviewUtils::revisionNumberToSourcePage($this->request->param('revision_number')),
+            'source_page' => ReviewUtils::revisionNumberToSourcePage( $this->request->param( 'revision_number' ) ),
         ];
 
         Database::obtain()->begin();
 
-        $struct = new EntryStruct($data);
+        $struct = new EntryStruct( $data );
 
         $model = $this->_getSegmentTranslationIssueModel(
-            $this->request->param('id_job'),
-            $this->request->param('password'),
+            $this->request->param( 'id_job' ),
+            $this->request->param( 'password' ),
             $struct
         );
-
-        if ($this->request->param('diff')) {
-            $model->setDiff((array)$this->request->param('diff'));
-        }
 
         $struct = $model->save();
 
         Database::obtain()->commit();
 
-        $json = new TranslationIssueFormatter();
-        $rendered = $json->renderItem($struct);
+        $json     = new TranslationIssueFormatter();
+        $rendered = $json->renderItem( $struct );
 
-        $this->response->json(['issue' => $rendered]);
-    }
-
-    /**
-     * This method does nothing
-     * @return void
-     */
-    public function update(): void
-    {
-        $this->response->json(['issue' => 'ok']);
+        $this->response->json( [ 'issue' => $rendered ] );
     }
 
     /**
      * @throws Exception
      */
-    public function delete(): void
-    {
+    public function update(): void {
+        $data = [
+                'id_issue'            => $this->request->param( 'id_issue' ),
+                'id_segment'          => $this->request->param( 'id_segment' ),
+                'id_job'              => $this->request->param( 'id_job' ),
+                'id_category'         => $this->request->param( 'id_category' ),
+                'severity'            => $this->request->param( 'severity' ),
+                'translation_version' => $this->validator->translation->version_number,
+                'target_text'         => $this->request->param( 'target_text' ),
+                'start_node'          => $this->request->param( 'start_node' ),
+                'start_offset'        => $this->request->param( 'start_offset' ),
+                'end_node'            => $this->request->param( 'end_node' ),
+                'end_offset'          => $this->request->param( 'end_offset' ),
+                'is_full_segment'     => false,
+                'comment'             => $this->request->param( 'comment' ),
+                'uid'                 => $this->user->uid ?? null,
+                'source_page'         => ReviewUtils::revisionNumberToSourcePage( $this->request->param( 'revision_number' ) ),
+        ];
+
+        Database::obtain()->begin();
+
+        $oldStruct = EntryDao::findById( $data[ 'id_issue' ] );
+
+        if ( empty( $oldStruct ) ) {
+            throw new NotFoundException( "Issue not found", 404 );
+        }
+
+        $this->checkUserId($oldStruct->uid);
+
+        $oldStruct->setDefaults();
+
+        $newStruct     = new EntryStruct( $data );
+        $newStruct->id = $data[ 'id_issue' ];
+        $newStruct->setDefaults();
+
+        $model = $this->_getSegmentTranslationIssueModel(
+                $this->request->param( 'id_job' ),
+                $this->request->param( 'password' ),
+                $newStruct
+        );
+
+        $struct = $model->editFrom( $oldStruct );
+
+        Database::obtain()->commit();
+
+        $json = new TranslationIssueFormatter();
+        $rendered = $json->renderItem( $struct );
+
+        $this->response->json( [ 'issue' => $rendered ] );
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function delete(): void {
         Database::obtain()->begin();
         $model = $this->_getSegmentTranslationIssueModel(
-            $this->request->param('id_job'),
-            $this->request->param('password'),
+            $this->request->param( 'id_job' ),
+            $this->request->param( 'password' ),
             $this->validator->issue
         );
+
+        $this->checkUserId($this->validator->issue->uid);
+
         $model->delete();
         Database::obtain()->commit();
 
-        $this->response->code(200);
+        $this->response->code( 200 );
     }
 
-    public function getComments(): void
-    {
+    public function getComments(): void {
         $dao = new EntryCommentDao();
 
         $comments = $dao->findByIssueId(
@@ -120,32 +161,39 @@ class SegmentTranslationIssueController extends AbstractStatefulKleinController
         );
 
         $json = new TranslationIssueComment();
-        $rendered = $json->render($comments);
-        $this->response->json(['comments' => $rendered]);
+        $rendered = $json->render( $comments );
+        $this->response->json( [ 'comments' => $rendered ] );
     }
 
     /**
+     * @throws AuthorizationError
+     * @throws NotFoundException
      */
-    public function createComment(): void
-    {
+    public function createComment(): void {
         $data = [
-            'comment' => $this->request->param('message'),
+            'comment' => $this->request->param( 'message' ),
             'id_qa_entry' => $this->validator->issue->id,
-            'source_page' => $this->request->param('source_page'),
+            'source_page' => $this->request->param( 'source_page' ),
             'uid' => $this->user->uid
         ];
 
         $dao = new EntryCommentDao();
-        $entry = EntryDao::findById($this->validator->issue->id);
+        $entry = EntryDao::findById( $this->validator->issue->id );
 
-        $dao->createComment($data);
+        if ( empty( $entry ) ) {
+            throw new NotFoundException( "Issue not found", 404 );
+        }
+
+        $this->checkUserId($this->validator->issue->uid);
+
+        $dao->createComment( $data );
 
         $json = new TranslationIssueFormatter();
-        $rendered = $json->renderItem($entry);
+        $rendered = $json->renderItem( $entry );
 
-        $response = ['comment' => $rendered];
+        $response = [ 'comment' => $rendered ];
 
-        $this->response->json($response);
+        $this->response->json( $response );
     }
 
     /**
@@ -155,30 +203,37 @@ class SegmentTranslationIssueController extends AbstractStatefulKleinController
      *
      * @return TranslationIssueModel
      */
-    protected function _getSegmentTranslationIssueModel(int $id_job, string $password, EntryStruct $issue): TranslationIssueModel
-    {
-        return new TranslationIssueModel($id_job, $password, $issue);
+    protected function _getSegmentTranslationIssueModel( int $id_job, string $password, EntryStruct $issue ): TranslationIssueModel {
+        return new TranslationIssueModel( $id_job, $password, $issue );
     }
 
-    protected function afterConstruct(): void
-    {
-        $this->appendValidator(new LoginValidator($this));
-        $jobValidator = new ChunkPasswordValidator($this);
-        $jobValidator->onSuccess(function () use ($jobValidator) {
+    protected function afterConstruct(): void {
+        $this->appendValidator( new LoginValidator( $this ) );
+        $jobValidator = new ChunkPasswordValidator( $this );
+        $jobValidator->onSuccess( function () use ( $jobValidator ) {
             //enable dynamic loading (Factory) by callback hook on revision features
-            $this->validator = (new SegmentTranslationIssueValidator($this))->setChunkReview($jobValidator->getChunkReview());
+            $this->validator = ( new SegmentTranslationIssueValidator( $this ) )->setChunkReview( $jobValidator->getChunkReview() );
             $this->validator->validate();
-        });
-        $this->appendValidator($jobValidator);
+        } );
+        $this->appendValidator( $jobValidator );
     }
 
-    private function getVersionNumber(): int
-    {
-        if (null !== $this->request->param('version_number')) {
-            return (int)$this->request->param('version_number');
+    private function getVersionNumber(): int {
+        if ( null !== $this->request->param( 'version_number' ) ) {
+            return (int)$this->request->param( 'version_number' );
         }
 
         return (int)$this->validator->translation->version_number;
     }
 
+    /**
+     * @param $uid
+     *
+     * @throws AuthorizationError
+     */
+    private function checkUserId( $uid ) {
+        if ( $this->user->uid !== $uid ) {
+            throw new AuthorizationError( "Not Authorized", 401 );
+        }
+    }
 }
