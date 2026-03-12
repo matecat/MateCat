95d9e7720c69bc7bef5a95eea713255bab376aa6
pierluigi.dicianni <pierluigi.dicianni@translated.net>
2026-03-12 15:45:05 +0100

    Project page  manage teams and members

--- public/js/components/projects2.0/ProjectContainer.js
+++ public/js/components/projects2.0/ProjectContainer.js
@@ -1,5 +1,11 @@
 import PropTypes from 'prop-types'
-import React, {useContext, useRef, useState} from 'react'
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
 import {ProjectsBulkActionsContext} from '../projects/ProjectsBulkActions/ProjectsBulkActionsContext'
 import {Checkbox, CHECKBOX_STATE} from '../common/Checkbox'
 import {Controller, useForm} from 'react-hook-form'
@@ -18,6 +24,23 @@ import IconClose from '../icons/IconClose'
 import {JobContainer} from './JobContainer'
 import {getLastProjectActivityLogAction} from '../../api/getLastProjectActivityLogAction'
 import {isUndefined} from 'lodash'
+import {
+  DROPDOWN_MENU_ALIGN,
+  DropdownMenu,
+} from '../common/DropdownMenu/DropdownMenu'
+import UserActions from '../../actions/UserActions'
+import {UserProjectDropdown} from '../projects/UserProjectDropdown'
+import FileLog from '../../../img/icons/FileLog'
+import Archive from '../../../img/icons/Archive'
+import Trash from '../../../img/icons/Trash'
+import FlipBackward from '../icons/FlipBackward'
+import DotsHorizontal from '../../../img/icons/DotsHorizontal'
+import ModalsActions from '../../actions/ModalsActions'
+import ConfirmMessageModal from '../modals/ConfirmMessageModal'
+import IconDown from '../icons/IconDown'
+import ManageConstants from '../../constants/ManageConstants'
+import UserStore from '../../stores/UserStore'
+import ProjectsStore from '../../stores/ProjectsStore'
 
 export const ProjectContainer = ({
   project,
@@ -33,18 +56,84 @@ export const ProjectContainer = ({
 
   const {handleSubmit, control, reset} = useForm()
 
+  const idTeamProject = project.get('id_team')
+
   const [shouldShowMoreActions, setShouldShowMoreActions] = useState(false)
   const [isEditingName, setIsEditingName] = useState(false)
   const [lastAction, setLastAction] = useState()
   const [jobsActions, setJobsActions] = useState()
+  const [idTeamSelected, setIdTeamSelected] = useState(idTeamProject)
+
+  const projectRef = useRef()
+  const projectTeam = useRef()
+  projectTeam.current = teams.find(
+    (team) => team.get('id') === project.get('id_team'),
+  )
+
+  const hideProjectAfterChangeAssignee = useCallback(
+    (projectCompare, user) => {
+      if (project.get('id') === projectCompare.get('id')) {
+        const uid = user ? user.get('uid') : -1
+        if (
+          (uid !== selectedUser &&
+            selectedUser !== ManageConstants.ALL_MEMBERS_FILTER) ||
+          (team.get('type') == 'personal' &&
+            uid !== UserStore.getUser().user.uid)
+        ) {
+          setTimeout(() => {
+            projectRef.current.style.transition = 'transform 0.5s ease-in-out'
+            projectRef.current.style.transform = 'translateX(-2000px)'
+          }, 500)
+          setTimeout(() => {
+            ManageActions.removeProject(project)
+          }, 1000)
+        }
+      }
+    },
+    [project, selectedUser, team],
+  )
+
+  useEffect(() => {
+    getLastAction.current()
+
+    const hideProject = (projectCompare) => {
+      if (project.get('id') === projectCompare.get('id')) {
+        projectRef.current.style.transition = 'transform 0.5s ease-in-out'
+        projectRef.current.style.transform = 'translateX(-2000px)'
+      }
+    }
+
+    ProjectsStore.addListener(ManageConstants.HIDE_PROJECT, hideProject)
+    ProjectsStore.addListener(
+      ManageConstants.CHANGE_PROJECT_ASSIGNEE,
+      hideProjectAfterChangeAssignee,
+    )
+
+    return () => {
+      ProjectsStore.removeListener(ManageConstants.HIDE_PROJECT, hideProject)
+      ProjectsStore.removeListener(
+        ManageConstants.CHANGE_PROJECT_ASSIGNEE,
+        hideProjectAfterChangeAssignee,
+      )
+    }
+  }, [project, hideProjectAfterChangeAssignee])
+
+  useEffect(() => {
+    setIdTeamSelected(idTeamProject)
+  }, [idTeamProject])
+
+  const changeNameFormId = `project-change-name-${project.get('id')}`
+
+  const jobsBulkForCurrentProject = project
+    .get('jobs')
+    .toJS()
+    .filter(({id}) => jobsBulk.some((value) => value === id))
 
   const handleFormSubmit = (formData) => {
     const {name} = formData
     ManageActions.changeProjectName(project, name)
     setIsEditingName(false)
   }
-
-  const changeNameFormId = `project-change-name-${project.get('id')}`
 
   const changeNameForm = (
     <form
@@ -75,13 +164,6 @@ export const ProjectContainer = ({
       </fieldset>
     </form>
   )
-
-  const idTeamProject = project.get('id_team')
-
-  const jobsBulkForCurrentProject = project
-    .get('jobs')
-    .toJS()
-    .filter(({id}) => jobsBulk.some((value) => value === id))
 
   const projectNameElements = (
     <div className="project-container-header-name">
@@ -119,7 +201,7 @@ export const ProjectContainer = ({
           <Button
             className="project-container-button-edit-name"
             mode={BUTTON_MODE.GHOST}
-            size={BUTTON_SIZE.ICON_XSMALL}
+            size={BUTTON_SIZE.ICON_SMALL}
             onClick={() => setIsEditingName(true)}
           >
             <IconEdit size={16} />
@@ -208,26 +290,253 @@ export const ProjectContainer = ({
     })
   }
 
+  const changeTeam = (value) => {
+    if (project.get('id_team') !== parseInt(value)) {
+      ManageActions.changeProjectTeam(value, project)
+      projectTeam.current = teams.find(
+        (team) => parseInt(team.get('id')) === parseInt(value),
+      )
+    }
+  }
+
+  const getDropDownTeams = () => {
+    const teamsCollections = teams?.toJS() ?? []
+    const items = teamsCollections.map((team) => ({
+      label: team.name,
+      selected: team.id === idTeamSelected,
+      onClick: () => {
+        changeTeam(team.id)
+        setIdTeamSelected(team.id)
+      },
+    }))
+
+    return (
+      <DropdownMenu
+        className="project-team-dropdown"
+        align={DROPDOWN_MENU_ALIGN.RIGHT}
+        toggleButtonProps={{
+          mode: BUTTON_MODE.BASIC,
+          size: BUTTON_SIZE.SMALL,
+          children: (
+            <>
+              {teamsCollections.find(({id}) => id === idTeamSelected)?.name}
+              <IconDown size={16} />
+            </>
+          ),
+          testId: 'teams-dropdown',
+        }}
+        items={items}
+      />
+    )
+  }
+
+  const changeUser = (value) => {
+    let user
+    const idUser = parseInt(value)
+    const team = projectTeam.current
+    if (idUser !== -1) {
+      const newUser = team.get('members').find(function (member) {
+        const user = member.get('user')
+        if (user.get('uid') === idUser) {
+          return true
+        }
+      })
+      if (!newUser) {
+        return
+      }
+      user = newUser.get('user')
+    }
+    if (
+      (!project.get('id_assignee') && idUser !== -1) ||
+      (project.get('id_assignee') && idUser != project.get('id_assignee'))
+    ) {
+      ManageActions.changeProjectAssignee(team, project, user)
+    }
+  }
+
+  const openAddMember = () =>
+    ManageActions.openAddTeamMemberModal(projectTeam.current.toJS())
+
+  const createUserDropDown = (users) => {
+    return (
+      <UserProjectDropdown
+        {...{
+          users: users.toJS(),
+          project: project.toJS(),
+          openAddMember: openAddMember,
+          changeUser: changeUser,
+          idAssignee: project.get('id_assignee'),
+        }}
+      />
+    )
+  }
+
+  const getDropDownUsers = () => {
+    let result = ''
+    if (team.get('type') == 'personal') {
+      if (teams) {
+        if (projectTeam.current && projectTeam.current.get('members')) {
+          result = createUserDropDown(projectTeam.current.get('members'))
+        } else {
+          UserActions.getAllTeams()
+        }
+      }
+    } else if (team.get('members')) {
+      result = createUserDropDown(team.get('members'))
+    }
+    return result
+  }
+
+  const removeProject = () =>
+    ManageActions.updateStatusProject(project, 'cancel')
+
+  const archiveProject = () =>
+    ManageActions.updateStatusProject(project, 'archive')
+
+  const activateProject = () =>
+    ManageActions.updateStatusProject(project, 'active')
+
+  const deleteProject = () => {
+    const props = {
+      text:
+        'You are about to delete this project permanently. This action cannot be undone.' +
+        ' Are you sure you want to proceed?',
+      successText: 'Yes, delete it',
+      successCallback: () => {
+        ManageActions.updateStatusProject(project, 'delete')
+      },
+      cancelCallback: () => {},
+    }
+    ModalsActions.showModalComponent(
+      ConfirmMessageModal,
+      props,
+      'Confirmation required',
+    )
+  }
+
+  const getDropdownProjectMenu = (activityLogUrl) => {
+    const isArchived = project.get('is_archived')
+    const isCancelled = project.get('is_cancelled')
+
+    const items = [
+      {
+        label: (
+          <>
+            <FileLog size={18} />
+            Activity Log
+          </>
+        ),
+        onClick: () => window.open(activityLogUrl, '_blank'),
+      },
+      ...(!isArchived && !isCancelled
+        ? [
+            {
+              label: (
+                <>
+                  <Archive size={18} />
+                  Archive project
+                </>
+              ),
+              onClick: archiveProject,
+            },
+            {
+              label: (
+                <>
+                  <Trash size={18} />
+                  Cancel project
+                </>
+              ),
+              onClick: removeProject,
+            },
+          ]
+        : []),
+      ...(isArchived
+        ? [
+            {
+              label: (
+                <>
+                  <FlipBackward size={18} />
+                  Unarchive project
+                </>
+              ),
+              onClick: activateProject,
+            },
+            {
+              label: (
+                <>
+                  <Trash size={18} />
+                  Cancel project
+                </>
+              ),
+              onClick: removeProject,
+            },
+          ]
+        : []),
+      ...(isCancelled
+        ? [
+            {
+              label: (
+                <>
+                  <FlipBackward size={18} />
+                  Resume Project
+                </>
+              ),
+              onClick: activateProject,
+            },
+            {
+              label: (
+                <>
+                  <Trash size={18} />
+                  Delete project permanently
+                </>
+              ),
+              onClick: deleteProject,
+            },
+          ]
+        : []),
+    ]
+
+    return (
+      <DropdownMenu
+        className="project-menu-dropdown"
+        toggleButtonProps={{
+          children: <DotsHorizontal size={16} />,
+          testId: 'project-menu-dropdown',
+        }}
+        align={DROPDOWN_MENU_ALIGN.RIGHT}
+        items={items}
+      />
+    )
+  }
+
   return (
-    <div className="project-container">
+    <div ref={projectRef} className="project-container">
       <div className="project-container-header">
-        <Checkbox
-          className="project-checkbox"
-          onChange={() => onCheckedProject(project.get('id'))}
-          value={
-            jobsBulkForCurrentProject.length === 0
-              ? CHECKBOX_STATE.UNCHECKED
-              : jobsBulkForCurrentProject.length === project.get('jobs').size
-                ? CHECKBOX_STATE.CHECKED
-                : CHECKBOX_STATE.INDETERMINATE
-          }
-        />
-        <div>
-          {projectNameElements}
-          <span title="Project id">ID: {project.get('id')}</span>
+        <div className="project-container-header-sx">
+          <Checkbox
+            className="project-checkbox"
+            onChange={() => onCheckedProject(project.get('id'))}
+            value={
+              jobsBulkForCurrentProject.length === 0
+                ? CHECKBOX_STATE.UNCHECKED
+                : jobsBulkForCurrentProject.length === project.get('jobs').size
+                  ? CHECKBOX_STATE.CHECKED
+                  : CHECKBOX_STATE.INDETERMINATE
+            }
+          />
+          <div>
+            {projectNameElements}
+            <span title="Project id">ID: {project.get('id')}</span>
+          </div>
+        </div>
+        <div className="project-container-header-dx">
+          {getDropDownTeams()}
+          {getDropDownUsers()}
+          {getDropdownProjectMenu(getActivityLogUrl())}
         </div>
       </div>
       {getJobContainer()}
+      <div className="project-container-footer">Footer</div>
     </div>
   )
 }
--- public/js/components/projects2.0/ProjectsContainer.js
+++ public/js/components/projects2.0/ProjectsContainer.js
@@ -5,6 +5,8 @@ import ProjectsStore from '../../stores/ProjectsStore'
 import ManageConstants from '../../constants/ManageConstants'
 import {ProjectsBulkActions} from '../projects/ProjectsBulkActions'
 import {ProjectContainer} from './ProjectContainer'
+import UserConstants from '../../constants/UserConstants'
+import UserStore from '../../stores/UserStore'
 
 export const ProjectsContainer = ({
   team,
@@ -21,14 +23,47 @@ export const ProjectsContainer = ({
     const renderProjects = (projects, team, teams, hideSpinner, filtering) => {
       setProjects(projects)
     }
+    const updateProjects = (projects) => setProjects(projects)
+    const updateTeams = (teams) => setTeamsState(teams)
+    const updateTeam = (team) => {
+      if (team.get('id') === teamState.get('id')) {
+        setTeamState(team)
+      }
+    }
+    const hideSpinner = () => {}
+    const showProjectsReloadSpinner = () => {}
 
     ProjectsStore.addListener(ManageConstants.RENDER_PROJECTS, renderProjects)
+    ProjectsStore.addListener(ManageConstants.UPDATE_PROJECTS, updateProjects)
+    ProjectsStore.addListener(ManageConstants.NO_MORE_PROJECTS, hideSpinner)
+    ProjectsStore.addListener(
+      ManageConstants.SHOW_RELOAD_SPINNER,
+      showProjectsReloadSpinner,
+    )
+    UserStore.addListener(UserConstants.UPDATE_TEAM, updateTeam)
+    UserStore.addListener(UserConstants.UPDATE_TEAMS, updateTeams)
+    UserStore.addListener(UserConstants.RENDER_TEAMS, updateTeams)
 
     return () => {
       ProjectsStore.removeListener(
         ManageConstants.RENDER_PROJECTS,
         renderProjects,
       )
+      ProjectsStore.removeListener(
+        ManageConstants.UPDATE_PROJECTS,
+        updateProjects,
+      )
+      ProjectsStore.removeListener(
+        ManageConstants.NO_MORE_PROJECTS,
+        hideSpinner,
+      )
+      ProjectsStore.removeListener(
+        ManageConstants.SHOW_RELOAD_SPINNER,
+        showProjectsReloadSpinner,
+      )
+      UserStore.removeListener(UserConstants.UPDATE_TEAM, updateTeam)
+      UserStore.removeListener(UserConstants.UPDATE_TEAMS, updateTeams)
+      UserStore.removeListener(UserConstants.RENDER_TEAMS, updateTeams)
     }
   }, [])
 
