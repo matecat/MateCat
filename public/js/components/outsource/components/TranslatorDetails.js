b8945137c62034f1c97f3a267e6610d25a1345ce
riccio82 <riccio82@gmail.com>
2026-03-12 17:19:39 +0100

    Outsource refactoring: wip

--- public/js/components/outsource/components/TranslatorDetails.js
+++ public/js/components/outsource/components/TranslatorDetails.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {numberWithCommas, formatPriceWithCommas} from '../outsourceConstants'
+import ChevronRight from '../../../../img/icons/ChevronRight'
 
 const TranslatorDetails = ({
   chunkQuote,
@@ -12,18 +13,20 @@ const TranslatorDetails = ({
   <div className="translator-job-details">
     {chunkQuote.get('t_name') !== '' ? (
       <>
-        <div>Best identified translator for this job:</div>
         <div className="translator-details-box">
-          <div className="translator-avatar">
-            {chunkQuote.get('t_name').charAt(0)}
-          </div>
-          <div>
-            <div>
-              <b>{chunkQuote.get('t_name')}</b> by Translated
+          <div>Best identified translator for this job:</div>
+          <div className="translator-details">
+            <div className="translator-avatar">
+              {chunkQuote.get('t_name').charAt(0)}
             </div>
-            <div>{translatedWords} words translated last 12 months</div>
-            <div>
-              {chunkQuote.get('t_experience_years')} years of experience
+            <div className="translator-info">
+              <div>
+                <b>{chunkQuote.get('t_name')}</b> by Translated
+              </div>
+              <div>{translatedWords} words translated last 12 months</div>
+              <div>
+                {chunkQuote.get('t_experience_years')} years of experience
+              </div>
             </div>
           </div>
         </div>
@@ -41,19 +44,18 @@ const TranslatorDetails = ({
 
     <div className="job-details-box">
       <div className="source-target-outsource st-details">
-        <div className="source-box">{job.get('sourceTxt')}</div>
+        <div className="source-box">{job.get('source')}</div>
         <div className="in-to">
-          <i className="icon-chevron-right icon" />
+          <ChevronRight size={16} />
         </div>
-        <div className="target-box">{job.get('targetTxt')}</div>
-      </div>
-      <div className="job-payment">
-        <div className="payable">
-          {numberWithCommas(chunkQuote.get('words'))} words
-        </div>
+        <div className="target-box">{job.get('target')}</div>
       </div>
     </div>
-
+    <div className="job-payment">
+      <div className="payable">
+        {numberWithCommas(chunkQuote.get('words'))} words
+      </div>
+    </div>
     {!outsourceConfirmed && (
       <div className="job-price">
         {priceCurrencySymbol}{' '}
