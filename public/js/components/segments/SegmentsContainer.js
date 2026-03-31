8c45df5dd9073d713a86ef18a8975b5ffc3a04c1
riccio82 <riccio82@gmail.com>
2026-03-31 16:41:46 +0200

    feat(context-review): include resname/restype/context_url in segments channel message (SegmentsContainer)

--- public/js/components/segments/SegmentsContainer.js
+++ public/js/components/segments/SegmentsContainer.js
@@ -449,10 +449,21 @@ function SegmentsContainer({isReview, startSegmentId, firstJobSegment}) {
     const segmentsList = []
     for (let i = 0; i < segments.size; i++) {
       const seg = segments.get(i)
+      const rawMetadata = seg.get('metadata')
+      const metadataArr = rawMetadata
+        ? typeof rawMetadata.toJS === 'function'
+          ? rawMetadata.toJS()
+          : rawMetadata
+        : []
+      const findMeta = (key) =>
+        metadataArr.find((m) => m.meta_key === key)?.meta_value ?? null
       segmentsList.push({
         sid: seg.get('sid'),
         source: seg.get('segment'),
         target: seg.get('translation'),
+        context_url: seg.get('context_url') ?? null,
+        resname: findMeta('resname'),
+        restype: findMeta('restype'),
       })
     }
     ContextReviewChannel.sendMessage({type: 'segments', segments: segmentsList})
