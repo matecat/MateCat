d6e86b0a16538aff56678f21027e406cd6c77598
domenico <domenico@translated.net>
2026-04-17 12:50:55 +0200

    🔧 chore(ci): update AWS credentials and CI configurations
    - add AWS credentials to load services and build test node in aws_dev.yml
    - uncomment and enable Amazon ECR login in aws_dev.yml
    - replace environment variable condition from TRAVIS/GITHUB_ACTION to CI_ENV in test_helper.php
    
    Signed-off-by: domenico <domenico@translated.net>

--- tests/test_helper.php
+++ tests/test_helper.php
@@ -12,11 +12,9 @@ define('TEST_DIR', realpath(dirname(__FILE__)));
 set_include_path(get_include_path() . PATH_SEPARATOR . TEST_DIR);
 require_once(PROJECT_ROOT . 'lib/Bootstrap.php');
 
-var_dump(getenv());
-
 if (getenv('USE_LOCAL_DEVELOPMENT_ENV')) {
     Bootstrap::start();
-} elseif (getenv('TRAVIS') || getenv('GITHUB_ACTION')) {
+} elseif (getenv('CI_ENV')) {
     Bootstrap::start(new SplFileInfo(TEST_DIR . '/inc/config.travis.ini'), new SplFileInfo(TEST_DIR . '/inc/task_manager_config.ini'));
 } else {
     Bootstrap::start(new SplFileInfo(TEST_DIR . '/inc/config.local.ini'), new SplFileInfo(TEST_DIR . '/inc/task_manager_config.ini'));
