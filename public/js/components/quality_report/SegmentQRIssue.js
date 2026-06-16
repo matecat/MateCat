7d8488c73cad6f1cbe2059c5e1355c32c0d486fb
Federico Ricciuti <riccio82@gmail.com>
2026-06-16 17:21:32 +0200

    ♻️ refactor(quality-report): flatten SCSS + convert class components
    - flatten QualityReportPage.scss from 18-level nesting to max 3 levels
      (149 nesting violations → 0), preserving all selectors and specificity
    - convert RevisionFeedback from class to functional component + PropTypes
    - convert SegmentQRIssue from class to functional, simplify comment
      generation with role/class maps instead of if/else chain
    - global lint warnings: 950 → 801

--- public/js/components/quality_report/SegmentQRIssue.js
+++ public/js/components/quality_report/SegmentQRIssue.js
@@ -1,113 +1,91 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import moment from 'moment'
 import {isUndefined} from 'lodash'
 import {Popup} from 'semantic-ui-react'
 import {Button, BUTTON_MODE, BUTTON_SIZE} from '../common/Button/Button'
 import CommentsSquareIconFilled from '../../../img/icons/CommentsSquareIconFilled'
 
-class SegmentQRIssue extends React.Component {
-  generateHtmlCommentLines(issue) {
-    let array = []
-    if (issue.get('comments')) {
-      let comments = issue.get('comments').toJS(),
-        comment_date
-      for (let n in comments) {
-        let comment = comments[n]
-        comment_date = moment(comment.create_date).format('lll')
-
-        if (comment.source_page == 1) {
-          array.push(
-            <p key={comment.id} className="re-comment">
-              <span className="re-translator">Translator </span>
-              <span className="re-comment-date">
-                <i>({comment_date}): </i>
-              </span>
-              {comment.comment}
-            </p>,
-          )
-        } else if (comment.source_page == 2) {
-          array.push(
-            <p key={comment.id} className="re-comment">
-              <span className="re-revisor">Reviewer </span>
-              <span className="re-comment-date">
-                <i>({comment_date}): </i>
-              </span>
-              {comment.comment}
-            </p>,
-          )
-        } else if (comment.source_page == 3) {
-          array.push(
-            <p key={comment.id} className="re-comment">
-              <span className="re-revisor2">Reviewer </span>
-              <span className="re-comment-date">
-                <i>({comment_date}): </i>
-              </span>
-              {comment.comment}
-            </p>,
-          )
-        }
+const generateCommentLines = (issue) => {
+  const lines = []
+  if (issue.get('comments')) {
+    const comments = issue.get('comments').toJS()
+    for (const n in comments) {
+      const comment = comments[n]
+      const date = moment(comment.create_date).format('lll')
+      const roleMap = {1: 'Translator', 2: 'Reviewer', 3: 'Reviewer'}
+      const classMap = {1: 're-translator', 2: 're-revisor', 3: 're-revisor2'}
+      const role = roleMap[comment.source_page]
+      const cls = classMap[comment.source_page]
+      if (role) {
+        lines.push(
+          <p key={comment.id} className="re-comment">
+            <span className={cls}>{role} </span>
+            <span className="re-comment-date"><i>({date}): </i></span>
+            {comment.comment}
+          </p>,
+        )
       }
     }
-    if (issue.get('target_text')) {
-      array.push(
-        <p key={issue.get('issue_id')} className="re-comment">
-          <span className="re-selected-text">
-            <b>Selected text: </b>
-          </span>
-          {issue.get('target_text')}
-        </p>,
-      )
-    }
-    if (array.length > 0) {
-      array = array.reverse()
-    }
-    return array
   }
-  render() {
-    let index = this.props.index
-    let issue = this.props.issue
-    return (
-      <div className="qr-issue human critical" key={'qr-issue' + index}>
-        <div className="qr-error" key={'error-qr' + index}>
-          {issue.get('issue_category')}:{' '}
-        </div>
-        <div className="qr-severity" key={'severity-qr' + index}>
-          <b key={'sev' + index}>[{issue.get('issue_severity')}]</b>
-        </div>
-        {(!isUndefined(issue.get('comments')) &&
-          issue.get('comments').size > 0) ||
-        issue.get('target_text') ? (
-          <React.Fragment>
-            <Popup
-              hoverable
-              position="bottom right"
-              trigger={
-                <Button
-                  size={BUTTON_SIZE.ICON_XSMALL}
-                  title="Comments"
-                  mode={BUTTON_MODE.OUTLINE}
-                >
-                  <CommentsSquareIconFilled size={18} />
-                </Button>
-              }
-            >
-              <div
-                style={{
-                  top: '0px',
-                  right: 'auto',
-                  left: '616.766px',
-                  bottom: 'auto',
-                  width: '368.594px',
-                }}
-              >
-                {this.generateHtmlCommentLines(issue)}
-              </div>
-            </Popup>
-          </React.Fragment>
-        ) : null}
-      </div>
+  if (issue.get('target_text')) {
+    lines.push(
+      <p key={issue.get('issue_id')} className="re-comment">
+        <span className="re-selected-text"><b>Selected text: </b></span>
+        {issue.get('target_text')}
+      </p>,
     )
   }
+  return lines.length > 0 ? lines.reverse() : lines
+}
+
+const SegmentQRIssue = ({issue, index}) => {
+  const hasComments =
+    (!isUndefined(issue.get('comments')) && issue.get('comments').size > 0) ||
+    issue.get('target_text')
+
+  return (
+    <div className="qr-issue human critical" key={'qr-issue' + index}>
+      <div className="qr-error" key={'error-qr' + index}>
+        {issue.get('issue_category')}:{' '}
+      </div>
+      <div className="qr-severity" key={'severity-qr' + index}>
+        <b key={'sev' + index}>[{issue.get('issue_severity')}]</b>
+      </div>
+      {hasComments && (
+        <Popup
+          hoverable
+          position="bottom right"
+          trigger={
+            <Button
+              size={BUTTON_SIZE.ICON_XSMALL}
+              title="Comments"
+              mode={BUTTON_MODE.OUTLINE}
+            >
+              <CommentsSquareIconFilled size={18} />
+            </Button>
+          }
+        >
+          <div
+            style={{
+              top: '0px',
+              right: 'auto',
+              left: '616.766px',
+              bottom: 'auto',
+              width: '368.594px',
+            }}
+          >
+            {generateCommentLines(issue)}
+          </div>
+        </Popup>
+      )}
+    </div>
+  )
+}
+
+SegmentQRIssue.propTypes = {
+  issue: PropTypes.object.isRequired,
+  index: PropTypes.number.isRequired,
 }
 
 export default SegmentQRIssue
