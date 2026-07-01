ff3a33f1d82915129746a56fd9fa5a76bc647fe1
Federico Ricciuti <riccio82@gmail.com>
2026-07-01 16:25:19 +0200

    ♻️ refactor(icons): add SVG replacements for removed icomoon icons

---
+++ public/js/components/icons/IconInfo.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+const IconInfo = ({size = 24}) => (
+  <svg width={size} height={size} viewBox="0 0 24 24">
+    <path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z" />
+  </svg>
+)
+
+export default IconInfo
---
+++ public/js/components/icons/IconSortUp.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+const IconSortUp = ({size = 24}) => (
+  <svg width={size} height={size} viewBox="0 0 24 24">
+    <path fill="currentColor" d="M7 14l5-5 5 5z" />
+  </svg>
+)
+
+export default IconSortUp
