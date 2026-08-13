2f756eab36d58dfc1e4dccd03c86982e43eae443
Federico Ricciuti <riccio82@gmail.com>
2026-08-13 17:20:40 +0200

    ♻️ refactor(review): convert TranslationIssuesSideButton to a function component

--- public/js/components/review/TranslationIssuesSideButton.test.js
+++ public/js/components/review/TranslationIssuesSideButton.test.js
@@ -1,16 +1,12 @@
-import {render} from '@testing-library/react'
 import React from 'react'
+import {render, screen} from '@testing-library/react'
 import TranslationIssuesSideButton from './TranslationIssuesSideButton'
 
-jest.mock(
-  '../review_extended/ReviewExtendedTranslationIssuesSideButton',
-  () =>
-    function MockReviewSideButton(props) {
-      return <div data-testid="review-side-button" {...props} />
-    },
-)
+jest.mock('../review_extended/ReviewExtendedTranslationIssuesSideButton', () => (props) => (
+  <div data-testid="inner" data-count={props.count} />
+))
 
-test('renders ReviewExtendedTranslationIssuesSideButton with forwarded props', () => {
-  const {getByTestId} = render(<TranslationIssuesSideButton foo="bar" />)
-  expect(getByTestId('review-side-button')).toBeInTheDocument()
+test('forwards all props to ReviewExtendedTranslationIssuesSideButton', () => {
+  render(<TranslationIssuesSideButton count={3} />)
+  expect(screen.getByTestId('inner')).toHaveAttribute('data-count', '3')
 })
