d6064e50879bd36ee663d8073615ba7943660532
Federico Ricciuti <riccio82@gmail.com>
2026-08-13 17:21:42 +0200

    ♻️ refactor(projects): convert Header to a function component

--- public/js/components/projects/Header.js
+++ public/js/components/projects/Header.js
@@ -1,10 +1,8 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
-class DasboardHeader extends React.Component {
-  render() {
-    const headerMountPoint = document.getElementsByTagName('header')[0]
-    return ReactDOM.createPortal(this.props.children, headerMountPoint)
-  }
+
+const Header = ({children}) => {
+  const headerMountPoint = document.getElementsByTagName('header')[0]
+  return ReactDOM.createPortal(children, headerMountPoint)
 }
 
-export default DasboardHeader
+export default Header
