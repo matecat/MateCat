3f0aa43d6440cdb6176e8574d9a85cd99bcb4fe3
Federico Ricciuti <riccio82@gmail.com>
2026-08-13 17:01:49 +0200

    ♻️ refactor(modals): convert AlertModal to a function component

--- public/js/components/modals/AlertModal.js
+++ public/js/components/modals/AlertModal.js
@@ -2,43 +2,50 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import {Button, BUTTON_TYPE} from '../common/Button/Button'
 
-class AlertModal extends React.Component {
-  allowHTML(string) {
-    return {__html: string}
+const allowHTML = (string) => {
+  return {__html: string}
+}
+
+const AlertModal = ({
+  text,
+  successCallback,
+  closeOnSuccess,
+  onClose,
+  buttonText,
+}) => {
+  const closeModal = () => {
+    successCallback?.()
+    if (closeOnSuccess) onClose()
   }
-  closeModal() {
-    this.props.successCallback?.()
-    if (this.props.closeOnSuccess) this.props.onClose()
-  }
-  render() {
-    return (
-      <div className="message-modal">
-        <div className="matecat-modal-middle">
-          <div className={'modal-grid alert_modal'}>
-            <div
-              className="matecat-modal-body"
-              style={{fontSize: '18px'}}
+
+  return (
+    <div className="message-modal">
+      <div className="matecat-modal-middle">
+        <div className={'modal-grid alert_modal'}>
+          <div
+            className="matecat-modal-body"
+            style={{fontSize: '18px'}}
+          >
+            {typeof text === 'string' ? (
+              <p dangerouslySetInnerHTML={allowHTML(text)} />
+            ) : (
+              text
+            )}
+          </div>
+          <div className="modal-grid__footer">
+            <Button
+              type={BUTTON_TYPE.PRIMARY}
+              onClick={() => closeModal()}
             >
-              {typeof this.props.text === 'string' ? (
-                <p dangerouslySetInnerHTML={this.allowHTML(this.props.text)} />
-              ) : (
-                this.props.text
-              )}
-            </div>
-            <div className="modal-grid__footer">
-              <Button
-                type={BUTTON_TYPE.PRIMARY}
-                onClick={() => this.closeModal()}
-              >
-                {this.props.buttonText ? this.props.buttonText : 'Ok'}
-              </Button>
-            </div>
+              {buttonText ? buttonText : 'Ok'}
+            </Button>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
 AlertModal.propTypes = {
   text: PropTypes.node,
 }
