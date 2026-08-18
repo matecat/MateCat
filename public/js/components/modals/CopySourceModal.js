4acdb4f01c24dcf39d11482e3352aeefa25a19e2
Federico Ricciuti <riccio82@gmail.com>
2026-08-18 09:40:37 +0200

    ♻️ refactor(modals): convert CopySourceModal to a function component

--- public/js/components/modals/CopySourceModal.js
+++ public/js/components/modals/CopySourceModal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import Cookies from 'js-cookie'
 import ModalsActions from '../../actions/ModalsActions'
 import {
@@ -9,25 +9,11 @@ import {
 } from '../common/Button/Button'
 import {COPY_SOURCE_COOKIE} from '../../constants/ModalKeys'
 
-class CopySourceModal extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const CopySourceModal = ({confirmCopyAllSources, abortCopyAllSources}) => {
+  const checkboxRef = useRef(null)
 
-  copyAllSources() {
-    this.props.confirmCopyAllSources()
-    this.checkCheckbox()
-    ModalsActions.onCloseModal()
-  }
-
-  copySegmentOnly() {
-    this.props.abortCopyAllSources()
-    this.checkCheckbox()
-    ModalsActions.onCloseModal()
-  }
-
-  checkCheckbox() {
-    const checked = this.checkbox.checked
+  const checkCheckbox = () => {
+    const checked = checkboxRef.current.checked
     if (checked) {
       sessionStorage.setItem(COPY_SOURCE_COOKIE, 0)
       Cookies.set(
@@ -41,48 +27,58 @@ class CopySourceModal extends React.Component {
     }
   }
 
-  render() {
-    return (
-      <div className="copy-source-modal">
-        <h3 className="text-container-top">
-          Do you really want to copy source to target for all new segments?
-          <br />
-          This action cannot be undone.
-        </h3>
-
-        <div className="buttons-popup-container">
-          <label>Copy source to target for:</label>
-          <Button
-            mode={BUTTON_MODE.OUTLINE}
-            size={BUTTON_SIZE.BIG}
-            onClick={this.copyAllSources.bind(this)}
-          >
-            ALL new segments
-          </Button>
-          <Button
-            type={BUTTON_TYPE.PRIMARY}
-            size={BUTTON_SIZE.BIG}
-            className="btn-ok"
-            onClick={this.copySegmentOnly.bind(this)}
-          >
-            This segment only
-          </Button>
-          <div className="notes-action"></div>
-        </div>
-        <div className="boxed">
-          <input
-            id="copy_s2t_dont_show"
-            type="checkbox"
-            className="dont_show"
-            ref={(checkbox) => (this.checkbox = checkbox)}
-          />
-          <label htmlFor="copy_s2t_dont_show">
-            {` Don't show this dialog again for the current job`}
-          </label>
-        </div>
-      </div>
-    )
+  const copyAllSources = () => {
+    confirmCopyAllSources()
+    checkCheckbox()
+    ModalsActions.onCloseModal()
   }
+
+  const copySegmentOnly = () => {
+    abortCopyAllSources()
+    checkCheckbox()
+    ModalsActions.onCloseModal()
+  }
+
+  return (
+    <div className="copy-source-modal">
+      <h3 className="text-container-top">
+        Do you really want to copy source to target for all new segments?
+        <br />
+        This action cannot be undone.
+      </h3>
+
+      <div className="buttons-popup-container">
+        <label>Copy source to target for:</label>
+        <Button
+          mode={BUTTON_MODE.OUTLINE}
+          size={BUTTON_SIZE.BIG}
+          onClick={copyAllSources}
+        >
+          ALL new segments
+        </Button>
+        <Button
+          type={BUTTON_TYPE.PRIMARY}
+          size={BUTTON_SIZE.BIG}
+          className="btn-ok"
+          onClick={copySegmentOnly}
+        >
+          This segment only
+        </Button>
+        <div className="notes-action"></div>
+      </div>
+      <div className="boxed">
+        <input
+          id="copy_s2t_dont_show"
+          type="checkbox"
+          className="dont_show"
+          ref={checkboxRef}
+        />
+        <label htmlFor="copy_s2t_dont_show">
+          {` Don't show this dialog again for the current job`}
+        </label>
+      </div>
+    </div>
+  )
 }
 
 export default CopySourceModal
