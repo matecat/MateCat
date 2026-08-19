39eeab406cd109cd21450efe5d3ed961d42b686d
Federico Ricciuti <riccio82@gmail.com>
2026-08-19 10:22:23 +0200

    ♻️ refactor(segments): convert SegmentFooterMultiMatches to a function component

--- public/js/components/segments/SegmentFooterMultiMatches.js
+++ public/js/components/segments/SegmentFooterMultiMatches.js
@@ -1,4 +1,4 @@
-import React, {createRef} from 'react'
+import React, {memo, createRef, useContext} from 'react'
 import {fromJS} from 'immutable'
 import {isUndefined} from 'lodash'
 import $ from 'jquery'
@@ -10,182 +10,140 @@ import {SegmentContext} from './SegmentContext'
 import {SegmentFooterTabError} from './SegmentFooterTabError'
 import DraftMatecatUtils from './utils/DraftMatecatUtils'
 
-class SegmentFooterMultiMatches extends React.Component {
-  static contextType = SegmentContext
+const SegmentFooterMultiMatches = memo(
+  (props) => {
+    const {clientConnected} = useContext(SegmentContext)
 
-  constructor(props) {
-    super(props)
-    // this.state = {
-    //     matches: (this.props.segment.cl_contributions) ? this.processContributions(this.props.segment.cl_contributions) : undefined
-    // };
-    this.parseMatches = this.parseMatches.bind(this)
-  }
+    const processContributions = (matches) => {
+      var matchesProcessed = []
+      // SegmentActions.createFooter(props.segment.sid);
+      $.each(matches, function () {
+        if (
+          isUndefined(this.segment) ||
+          this.segment === '' ||
+          this.translation === ''
+        )
+          return true
+        var item = {...this}
+        item.id = this.id
+        item.disabled = this.id == '0' ? true : false
+        item.cb = this.created_by
+        item.segment = this.segment
+        if (
+          'sentence_confidence' in this &&
+          this.sentence_confidence !== '' &&
+          this.sentence_confidence !== 0 &&
+          this.sentence_confidence != '0' &&
+          this.sentence_confidence !== null &&
+          this.sentence_confidence !== false &&
+          typeof this.sentence_confidence != 'undefined'
+        ) {
+          item.suggestion_info =
+            'Quality: <b>' + this.sentence_confidence + '</b>'
+        } else if (this.match != 'MT') {
+          item.suggestion_info = this.last_update_date
+        } else {
+          item.suggestion_info = ''
+        }
 
-  parseMatches(sid, fid, matches) {
-    if (this.props.segment.sid === sid) {
-      var matchesProcessed = this.processContributions(matches)
-      if (this._isMounted) {
-        this.setState({
-          matches: matchesProcessed,
-        })
-      }
-    }
-  }
+        item.percentText = TranslationMatches.getPercentTextForMatch(this)
+        item.percentClass = TranslationMatches.getPercentageClass(this)
 
-  processContributions(matches) {
-    var self = this
-    var matchesProcessed = []
-    // SegmentActions.createFooter(this.props.segment.sid);
-    $.each(matches, function () {
-      if (
-        isUndefined(this.segment) ||
-        this.segment === '' ||
-        this.translation === ''
-      )
-        return true
-      var item = {...this}
-      item.id = this.id
-      item.disabled = this.id == '0' ? true : false
-      item.cb = this.created_by
-      item.segment = this.segment
-      if (
-        'sentence_confidence' in this &&
-        this.sentence_confidence !== '' &&
-        this.sentence_confidence !== 0 &&
-        this.sentence_confidence != '0' &&
-        this.sentence_confidence !== null &&
-        this.sentence_confidence !== false &&
-        typeof this.sentence_confidence != 'undefined'
-      ) {
-        item.suggestion_info =
-          'Quality: <b>' + this.sentence_confidence + '</b>'
-      } else if (this.match != 'MT') {
-        item.suggestion_info = this.last_update_date
-      } else {
-        item.suggestion_info = ''
-      }
-
-      item.percentText = TranslationMatches.getPercentTextForMatch(this)
-      item.percentClass = TranslationMatches.getPercentageClass(this)
-
-      // Attention Bug: We are mixing the view mode and the raw data mode.
-      // before doing a enhanced  view you will need to add a data-original tag
-      //
-      item.suggestionDecodedHtml = DraftMatecatUtils.transformTagsToHtml(
-        this.segment,
-        config.isTargetRTL,
-      )
-      item.translationDecodedHtml = DraftMatecatUtils.transformTagsToHtml(
-        this.translation,
-        config.isTargetRTL,
-      )
-      item.translation = this.translation
-      item.sourceDiff = item.suggestionDecodedHtml
-
-      if (
-        this.match !== 'MT' &&
-        parseInt(this.match) > 74 &&
-        parseInt(this.match) < 100
-      ) {
-        item.sourceDiff = TextUtils.getDiffHtml(
+        // Attention Bug: We are mixing the view mode and the raw data mode.
+        // before doing a enhanced  view you will need to add a data-original tag
+        //
+        item.suggestionDecodedHtml = DraftMatecatUtils.transformTagsToHtml(
           this.segment,
-          self.props.segment.segment,
+          config.isTargetRTL,
         )
-
-        item.sourceDiff = DraftMatecatUtils.transformTagsToHtml(
-          item.sourceDiff,
-          config.isSourceRTL,
+        item.translationDecodedHtml = DraftMatecatUtils.transformTagsToHtml(
+          this.translation,
+          config.isTargetRTL,
         )
-      }
-      if (!isUndefined(this.tm_properties)) {
-        item.tm_properties = this.tm_properties
-      }
+        item.translation = this.translation
+        item.sourceDiff = item.suggestionDecodedHtml
 
-      matchesProcessed.push(item)
-    })
-    return matchesProcessed
-  }
+        if (
+          this.match !== 'MT' &&
+          parseInt(this.match) > 74 &&
+          parseInt(this.match) < 100
+        ) {
+          item.sourceDiff = TextUtils.getDiffHtml(
+            this.segment,
+            props.segment.segment,
+          )
 
-  getMatchInfo(match) {
-    return (
-      <ul className="graysmall-details">
-        <li className="graydesc">
-          Origin:
-          <span className="bold"> {match.cb}</span>
-        </li>
-        <li>{match.suggestion_info}</li>
-        <li
-          ref={createRef()}
-          className={`percent  ${
-            match.source !== config.source_rfc
-              ? 'per-yellow-variant'
-              : 'per-green'
-          }`}
-        >
-          {match.source} {'>'} {match.target}{' '}
-          {match.source !== config.source_rfc ? '(-1%)' : ''}
-        </li>
-        <li className={'percent ' + match.percentClass}>{match.percentText}</li>
-        {/*<li className="graydesc">
+          item.sourceDiff = DraftMatecatUtils.transformTagsToHtml(
+            item.sourceDiff,
+            config.isSourceRTL,
+          )
+        }
+        if (!isUndefined(this.tm_properties)) {
+          item.tm_properties = this.tm_properties
+        }
+
+        matchesProcessed.push(item)
+      })
+      return matchesProcessed
+    }
+
+    const getMatchInfo = (match) => {
+      return (
+        <ul className="graysmall-details">
+          <li className="graydesc">
+            Origin:
+            <span className="bold"> {match.cb}</span>
+          </li>
+          <li>{match.suggestion_info}</li>
+          <li
+            ref={createRef()}
+            className={`percent  ${
+              match.source !== config.source_rfc
+                ? 'per-yellow-variant'
+                : 'per-green'
+            }`}
+          >
+            {match.source} {'>'} {match.target}{' '}
+            {match.source !== config.source_rfc ? '(-1%)' : ''}
+          </li>
+          <li className={'percent ' + match.percentClass}>
+            {match.percentText}
+          </li>
+          {/*<li className="graydesc">
           Target:
           <span className="bold" style={{fontSize: '14px'}}>
             {' '}
             {match.target}
           </span>
         </li>*/}
-      </ul>
-    )
-  }
-
-  suggestionDblClick(match) {
-    SegmentActions.setFocusOnEditArea()
-    SegmentActions.disableTPOnSegment(this.props.segment)
-    setTimeout(() => {
-      SegmentActions.replaceEditAreaTextContent(
-        this.props.segment.sid,
-        match.translation,
+        </ul>
       )
-    }, 200)
-  }
+    }
 
-  componentDidMount() {
-    this._isMounted = true
-  }
+    const suggestionDblClick = (match) => {
+      SegmentActions.setFocusOnEditArea()
+      SegmentActions.disableTPOnSegment(props.segment)
+      setTimeout(() => {
+        SegmentActions.replaceEditAreaTextContent(
+          props.segment.sid,
+          match.translation,
+        )
+      }, 200)
+    }
 
-  componentWillUnmount() {
-    this._isMounted = false
-  }
+    const allowHTML = (string) => {
+      return {__html: string}
+    }
 
-  allowHTML(string) {
-    return {__html: string}
-  }
-
-  shouldComponentUpdate(nextProps) {
-    return (
-      ((!isUndefined(nextProps.segment.cl_contributions) ||
-        !isUndefined(this.props.segment.cl_contributions)) &&
-        ((!isUndefined(nextProps.segment.cl_contributions) &&
-          isUndefined(this.props.segment.cl_contributions)) ||
-          !fromJS(this.props.segment.cl_contributions).equals(
-            fromJS(nextProps.segment.cl_contributions),
-          ))) ||
-      this.props.active_class !== nextProps.active_class ||
-      this.props.tab_class !== nextProps.tab_class
-    )
-  }
-
-  render() {
-    const {clientConnected} = this.context
     var matches = []
     if (
-      this.props.segment.cl_contributions &&
-      this.props.segment.cl_contributions.matches &&
-      this.props.segment.cl_contributions.matches.length > 0
+      props.segment.cl_contributions &&
+      props.segment.cl_contributions.matches &&
+      props.segment.cl_contributions.matches.length > 0
     ) {
-      let tpmMatches = this.processContributions(
-        this.props.segment.cl_contributions.matches,
+      let tpmMatches = processContributions(
+        props.segment.cl_contributions.matches,
       )
-      var self = this
       tpmMatches.forEach(function (match, index) {
         var item = (
           <ul
@@ -194,33 +152,33 @@ class SegmentFooterMultiMatches extends React.Component {
             data-item={index + 1}
             data-id={match.id}
             data-original={match.segment}
-            onDoubleClick={self.suggestionDblClick.bind(self, match, index + 1)}
+            onDoubleClick={suggestionDblClick.bind(null, match, index + 1)}
           >
             <li className="sugg-source">
               <span
-                id={self.props.segment.sid + '-tm-' + match.id + '-source'}
+                id={props.segment.sid + '-tm-' + match.id + '-source'}
                 className="suggestion_source"
-                dangerouslySetInnerHTML={self.allowHTML(match.sourceDiff)}
+                dangerouslySetInnerHTML={allowHTML(match.sourceDiff)}
               ></span>
             </li>
             <li className="b sugg-target">
               <span
-                id={self.props.segment.sid + '-tm-' + match.id + '-translation'}
+                id={props.segment.sid + '-tm-' + match.id + '-translation'}
                 className="translation"
-                dangerouslySetInnerHTML={self.allowHTML(
+                dangerouslySetInnerHTML={allowHTML(
                   match.translationDecodedHtml,
                 )}
               ></span>
             </li>
-            {self.getMatchInfo(match)}
+            {getMatchInfo(match)}
           </ul>
         )
         matches.push(item)
       })
     } else if (
-      this.props.segment.cl_contributions &&
-      this.props.segment.cl_contributions.matches &&
-      this.props.segment.cl_contributions.matches.length === 0
+      props.segment.cl_contributions &&
+      props.segment.cl_contributions.matches &&
+      props.segment.cl_contributions.matches.length === 0
     ) {
       if (config.mt_enabled) {
         matches.push(
@@ -246,14 +204,11 @@ class SegmentFooterMultiMatches extends React.Component {
     }
     return (
       <div
-        key={'container_' + this.props.code}
+        key={'container_' + props.code}
         className={
-          'tab sub-editor ' +
-          this.props.active_class +
-          ' ' +
-          this.props.tab_class
+          'tab sub-editor ' + props.active_class + ' ' + props.tab_class
         }
-        id={'segment-' + this.props.segment.sid + '-' + this.props.tab_class}
+        id={'segment-' + props.segment.sid + '-' + props.tab_class}
       >
         {clientConnected ? (
           <div className="overflow">
@@ -268,7 +223,21 @@ class SegmentFooterMultiMatches extends React.Component {
         )}
       </div>
     )
-  }
-}
+  },
+  (prevProps, nextProps) =>
+    !(
+      ((!isUndefined(nextProps.segment.cl_contributions) ||
+        !isUndefined(prevProps.segment.cl_contributions)) &&
+        ((!isUndefined(nextProps.segment.cl_contributions) &&
+          isUndefined(prevProps.segment.cl_contributions)) ||
+          !fromJS(prevProps.segment.cl_contributions).equals(
+            fromJS(nextProps.segment.cl_contributions),
+          ))) ||
+      prevProps.active_class !== nextProps.active_class ||
+      prevProps.tab_class !== nextProps.tab_class
+    ),
+)
+
+SegmentFooterMultiMatches.displayName = 'SegmentFooterMultiMatches'
 
 export default SegmentFooterMultiMatches
